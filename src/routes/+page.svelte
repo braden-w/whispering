fa5ef262722f952b32207c4ed97c5741fe4457f1
Braden Wong <bmw02002turbo@gmail.com>
2023-03-16 00:12:50 -0700

    feat: scaffold record and whisper

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { startRecording, stopRecording } from '$lib/recorder';
+	import { sendAudioToWhisper } from '$lib/whisper';
+
+	let isRecording = false;
+	let micIcon = '🎙️';
+
+	async function toggleRecording() {
+		isRecording = !isRecording;
+		micIcon = isRecording ? '🟥' : '🎙️';
+
+		if (isRecording) {
+			await startRecording();
+		} else {
+			const audioBlob = await stopRecording();
+			const text = await sendAudioToWhisper(audioBlob);
+			navigator.clipboard.writeText('Hello, World!');
+		}
+	}
+</script>
+
+<div class="flex items-center justify-center min-h-screen">
+	<button class="text-6xl focus:outline-none" on:click={toggleRecording}>{micIcon}</button>
+</div>
