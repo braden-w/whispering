aad9526cf355d333e0a793c884e1381e038f84f5
Braden Wong <bmw02002turbo@gmail.com>
2023-03-17 09:00:54 -0700

    Feat: Title called Whispering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,6 +42,7 @@
 </script>
 
 <div class="flex flex-col items-center justify-center min-h-screen space-y-4">
+	<h1 class="text-4xl font-semibold text-gray-700">Whispering</h1>
 	<button class="text-6xl focus:outline-none" on:click={toggleRecording}>{micIcon}</button>
 	<input
 		class="w-64 px-3 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 focus:outline-none"
