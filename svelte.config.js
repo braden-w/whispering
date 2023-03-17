f829b47cd54e6c7d1343c122bdaabd0a729071ac
Braden Wong <bmw02002turbo@gmail.com>
2023-03-17 03:07:36 -0700

    feat: cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static'; // This was changed from adapter-auto
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
