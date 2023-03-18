41874ead04c61d30d2783f6330d3d4cdb7d2b74f
Braden Wong <bmw02002turbo@gmail.com>
2023-03-18 14:57:13 -0700

    refactor: use form

--- src/routes/setup/+page.svelte
+++ src/routes/setup/+page.svelte
@@ -19,32 +19,34 @@
 
 <div class="flex flex-col items-center justify-center min-h-screen space-y-4">
 	<h1 class="text-4xl font-semibold text-gray-700">Enter OpenAI API Key</h1>
-	<div class="flex items-center space-x-2">
-		<input
-			class="w-64 px-3 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-gray-400 focus:ring-2 focus:ring-gray-200 focus:outline-none"
-			placeholder="API Key"
-			bind:value={inputApiKey}
-		/>
-		<button
-			class="px-4 py-2 text-white bg-gray-600 border border-gray-600 rounded-md hover:bg-gray-700 focus:border-gray-700 focus:ring-2 focus:ring-gray-200 focus:outline-none"
-			on:click={submitApiKey}
-		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="w-6 h-6"
+	<form on:submit|preventDefault={submitApiKey}>
+		<div class="flex items-center space-x-2">
+			<input
+				class="w-64 px-3 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-gray-400 focus:ring-2 focus:ring-gray-200 focus:outline-none"
+				placeholder="API Key"
+				bind:value={inputApiKey}
+			/>
+			<button
+				class="px-4 py-2 text-white bg-gray-600 border border-gray-600 rounded-md hover:bg-gray-700 focus:border-gray-700 focus:ring-2 focus:ring-gray-200 focus:outline-none"
+				type="submit"
 			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-				/>
-			</svg>
-		</button>
-	</div>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+					/>
+				</svg>
+			</button>
+		</div>
+	</form>
 	<p class="text-xs text-gray-600">
 		You can find your OpenAI API key in your
 		<a
