841bfe86abeefa23808f68e189df7fb6c9fcb4fe
Braden Wong <bmw02002turbo@gmail.com>
2023-03-18 14:29:26 -0700

    refactor: ApiKeyPrompt to separate route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,6 @@
 	import { sendAudioToWhisper } from '$lib/whisper';
 	import { onDestroy, onMount } from 'svelte';
 	import toast from 'svelte-french-toast';
-	import ApiKeyPrompt from '../components/ApiKeyPrompt.svelte';
 
 	let isRecording = false;
 	let micIcon = '🎙️';
@@ -64,42 +63,38 @@
 </script>
 
 <div class="flex flex-col items-center justify-center min-h-screen space-y-4">
-	{#if !$apiKey}
-		<ApiKeyPrompt />
-	{:else}
-		<h1 class="text-4xl font-semibold text-gray-700">Whispering</h1>
-		<button class="text-6xl focus:outline-none" on:click={toggleRecording}>{micIcon}</button>
-		<div class="flex items-center space-x-2">
-			<input
-				class="w-64 px-3 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-gray-400 focus:ring-2 focus:ring-gray-200 focus:outline-none"
-				placeholder="Output text will appear here..."
-				bind:value={outputText}
-			/>
+	<h1 class="text-4xl font-semibold text-gray-700">Whispering</h1>
+	<button class="text-6xl focus:outline-none" on:click={toggleRecording}>{micIcon}</button>
+	<div class="flex items-center space-x-2">
+		<input
+			class="w-64 px-3 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-gray-400 focus:ring-2 focus:ring-gray-200 focus:outline-none"
+			placeholder="Output text will appear here..."
+			bind:value={outputText}
+		/>
 
-			<button
-				class="px-4 py-2 text-white bg-gray-600 border border-gray-600 rounded-md hover:bg-gray-700 focus:border-gray-700 focus:ring-2 focus:ring-gray-200 focus:outline-none"
-				on:click={copyOutputText}
+		<button
+			class="px-4 py-2 text-white bg-gray-600 border border-gray-600 rounded-md hover:bg-gray-700 focus:border-gray-700 focus:ring-2 focus:ring-gray-200 focus:outline-none"
+			on:click={copyOutputText}
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6"
 			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-6 h-6"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
-					/>
-				</svg>
-			</button>
-		</div>
-		<p class="text-xs text-gray-600">
-			Click the microphone or press <kbd>space</kbd> to start recording.
-		</p>
-	{/if}
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+				/>
+			</svg>
+		</button>
+	</div>
+	<p class="text-xs text-gray-600">
+		Click the microphone or press <kbd>space</kbd> to start recording.
+	</p>
 
 	<div class="fixed bottom-4 right-4">
 		<a
