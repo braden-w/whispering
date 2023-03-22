06027f34683ba44921aced01cb82a757228ab823
Braden Wong <bmw02002turbo@gmail.com>
2023-03-21 19:09:40 -0700

    fix: shortcuts only executes if Tauri
    Former-commit-id: edbe75665e639cc66903a46125c111cb81e61546 [formerly e4aeafac52c4f68914a6bff465236592b1d5103c]
    Former-commit-id: 9e3bfd0f9962073fac8c39da669949b94a49f744

--- src/lib/shorcuts.ts
+++ src/lib/shorcuts.ts
@@ -1,6 +1,6 @@
-import { register, unregisterAll } from '@tauri-apps/api/globalShortcut';
-
 export async function registerShortcut(currentShortcut: string, command: () => Promise<void>) {
+	if (!window.__TAURI__) return;
+	const { register, unregisterAll } = await import('@tauri-apps/api/globalShortcut');
 	await unregisterAll();
 	await register(currentShortcut, command);
 }
