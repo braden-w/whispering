e160e27cef2991c510f56ea892c710b6fc1046f3
Braden Wong <bmw02002turbo@gmail.com>
2023-03-23 10:22:07 -0700

    feat: use new timeout logic
    Former-commit-id: b41717f8379b6990c8ca34b9a76c893c1d92b597 [formerly 94d745f2bdd1f7cad57ecc364e87c8127376080f]
    Former-commit-id: 76b4463c45220d040b6b9e8b4a003c11920fd234

--- src/lib/system-apis/shorcuts.ts
+++ src/lib/system-apis/shorcuts.ts
@@ -3,12 +3,18 @@
  * @param currentShortcut The shortcut to be registered.
  * @param command The command to be executed when the shortcut is triggered.
  */
+
 export async function registerShortcut(currentShortcut: string, command: () => Promise<void>) {
 	if (!window.__TAURI__) return;
-	if (!isValidAccelerator(currentShortcut)) throw new Error('Invalid shortcut');
 	const { register, unregisterAll } = await import('@tauri-apps/api/globalShortcut');
 	await unregisterAll();
-	await register(currentShortcut, command);
+	// Register the new shortcut. If it takes more than 1 second, assume an error and reject the promise.
+	const timeoutPromise = new Promise<void>((_, reject) => {
+		setTimeout(() => {
+			reject(new Error('Timeout: operation took more than 1 second'));
+		}, 1000);
+	});
+	await Promise.race([register(currentShortcut, command), timeoutPromise]);
 }
 
 /**
@@ -18,27 +24,4 @@ export async function unregisterAllShortcuts() {
 	if (!window.__TAURI__) return;
 	const { unregisterAll } = await import('@tauri-apps/api/globalShortcut');
 	await unregisterAll();
-}
-
-// From https://github.com/brrd/electron-is-accelerator/blob/master/index.js
-const modifiers =
-	/^(Command|Cmd|Control|Ctrl|CommandOrControl|CmdOrCtrl|Alt|Option|AltGr|Shift|Super|Meta)$/;
-const keyCodes =
-	/^([0-9A-Z)!@#$%^&*(:+<_>?~{|}";=,\-./`[\\\]']|F1*[1-9]|F10|F2[0-4]|Plus|Space|Tab|Backspace|Delete|Insert|Return|Enter|Up|Down|Left|Right|Home|End|PageUp|PageDown|Escape|Esc|VolumeUp|VolumeDown|VolumeMute|MediaNextTrack|MediaPreviousTrack|MediaStop|MediaPlayPause|PrintScreen)$/;
-
-function isValidAccelerator(accelerator: string) {
-	const parts = accelerator.split('+');
-	let keyFound = false;
-	return parts.every((val, index) => {
-		const isKey = keyCodes.test(val);
-		const isModifier = modifiers.test(val);
-		if (isKey) {
-			// Key must be unique
-			if (keyFound) return false;
-			keyFound = true;
-		}
-		// Key is required
-		if (index === parts.length - 1 && !keyFound) return false;
-		return isKey || isModifier;
-	});
 }
