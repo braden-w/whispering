9544ff2f819e519192bd634aac5f8da30f928863
Braden Wong <bmw02002turbo@gmail.com>
2023-03-23 10:03:00 -0700

    feat: check keycodes before registering
    Former-commit-id: 888678ca5836bcc8d70fcff2571e413c55be2b3f [formerly c31af72c30978f3ed536c23470563b11a8567880]
    Former-commit-id: 2919cab858a04b591e41b1100f1f0baf6750cd9c

--- src/lib/system-apis/shorcuts.ts
+++ src/lib/system-apis/shorcuts.ts
@@ -5,6 +5,7 @@
  */
 export async function registerShortcut(currentShortcut: string, command: () => Promise<void>) {
 	if (!window.__TAURI__) return;
+	if (!isValidAccelerator(currentShortcut)) throw new Error('Invalid shortcut');
 	const { register, unregisterAll } = await import('@tauri-apps/api/globalShortcut');
 	await unregisterAll();
 	await register(currentShortcut, command);
@@ -17,4 +18,27 @@ export async function unregisterAllShortcuts() {
 	if (!window.__TAURI__) return;
 	const { unregisterAll } = await import('@tauri-apps/api/globalShortcut');
 	await unregisterAll();
+}
+
+// From https://github.com/brrd/electron-is-accelerator/blob/master/index.js
+const modifiers =
+	/^(Command|Cmd|Control|Ctrl|CommandOrControl|CmdOrCtrl|Alt|Option|AltGr|Shift|Super)$/;
+const keyCodes =
+	/^([0-9A-Z)!@#$%^&*(:+<_>?~{|}";=,\-./`[\\\]']|F1*[1-9]|F10|F2[0-4]|Plus|Space|Tab|Backspace|Delete|Insert|Return|Enter|Up|Down|Left|Right|Home|End|PageUp|PageDown|Escape|Esc|VolumeUp|VolumeDown|VolumeMute|MediaNextTrack|MediaPreviousTrack|MediaStop|MediaPlayPause|PrintScreen)$/;
+
+function isValidAccelerator(accelerator: string) {
+	const parts = accelerator.split('+');
+	let keyFound = false;
+	return parts.every((val, index) => {
+		const isKey = keyCodes.test(val);
+		const isModifier = modifiers.test(val);
+		if (isKey) {
+			// Key must be unique
+			if (keyFound) return false;
+			keyFound = true;
+		}
+		// Key is required
+		if (index === parts.length - 1 && !keyFound) return false;
+		return isKey || isModifier;
+	});
 }
