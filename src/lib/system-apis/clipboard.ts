d7851d2eaf8a7baccbe954d190dcba1b04ef4922
Braden Wong <bmw02002turbo@gmail.com>
2023-03-23 00:46:06 -0700

    docs: system-api functions
    Former-commit-id: bdd3824dcb6708b5b468a45b06c485b1fe61c303 [formerly ac0560de83820fd3504a3195f4732ae57194e5d1]
    Former-commit-id: 6f43ba77a9974749612877f044e96673aa3fcc33

--- src/lib/system-apis/clipboard.ts
+++ src/lib/system-apis/clipboard.ts
@@ -1,3 +1,7 @@
+/**
+ * Writes text to the user's clipboard.
+ * @param text The text to write to the clipboard.
+ */
 export async function writeText(text: string): Promise<void> {
 	if (window.__TAURI__) {
 		const { writeText } = await import('@tauri-apps/api/clipboard');
