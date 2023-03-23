b61970b87c0ef21c74b169c3c4e0c33301052d5a
Braden Wong <bmw02002turbo@gmail.com>
2023-03-23 10:24:07 -0700

    refactor: registerWithTimeout
    Former-commit-id: 2bdb30628c009ef505854a25fd6c9d70608f8579 [formerly 3248d6de5933b0994aab18f253489fc3a7b2a9ef]
    Former-commit-id: 6dd9ff50fa8be998c23d6d4dbe119acec298d4b8

--- src/lib/system-apis/shorcuts.ts
+++ src/lib/system-apis/shorcuts.ts
@@ -8,13 +8,7 @@ export async function registerShortcut(currentShortcut: string, command: () => P
 	if (!window.__TAURI__) return;
 	const { register, unregisterAll } = await import('@tauri-apps/api/globalShortcut');
 	await unregisterAll();
-	// Register the new shortcut. If it takes more than 1 second, assume an error and reject the promise.
-	const timeoutPromise = new Promise<void>((_, reject) => {
-		setTimeout(() => {
-			reject(new Error('Timeout: operation took more than 1 second'));
-		}, 1000);
-	});
-	await Promise.race([register(currentShortcut, command), timeoutPromise]);
+	await registerWithTimeout(currentShortcut, command, register, 1000);
 }
 
 /**
@@ -24,4 +18,18 @@ export async function unregisterAllShortcuts() {
 	if (!window.__TAURI__) return;
 	const { unregisterAll } = await import('@tauri-apps/api/globalShortcut');
 	await unregisterAll();
+}
+
+async function registerWithTimeout(
+	currentShortcut: string,
+	command: () => Promise<void>,
+	registerFn: (shortcut: string, command: () => Promise<void>) => Promise<void>,
+	timeout: number
+) {
+	const timeoutPromise = new Promise<void>((_, reject) => {
+		setTimeout(() => {
+			reject(new Error(`Timeout: operation took more than ${timeout} milliseconds`));
+		}, timeout);
+	});
+	await Promise.race([registerFn(currentShortcut, command), timeoutPromise]);
 }
