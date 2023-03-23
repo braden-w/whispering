c9992f4e2feacbe8109461a40da5a84bfcf51a51
Braden Wong <bmw02002turbo@gmail.com>
2023-03-22 17:27:18 -0700

    refactor: move desktop back into root
    Former-commit-id: 8fa0b033c9e117d3b1a7a5dc1936c0ea088ffd9c [formerly 740074b0482b28b4cab074af0c0de4377d62c55c]
    Former-commit-id: 8113bf0501ddf941db99fd63d11391066c006e55

--- web/src/lib/stores/apiKey.ts
+++
@@ -1,3 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const apiKey = writable(localStorage.getItem('openai-api-key') || '');
--- web/src/lib/toasts/SomethingWentWrongToast.svelte
+++
@@ -1,8 +0,0 @@
-<span>
-	Something went wrong. Please verify that your <a
-		href="/setup"
-		class="text-gray-600 underline hover:text-indigo-900"
-	>
-		API Key in settings
-	</a> is correct.
-</span>
