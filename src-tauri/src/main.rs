08360b36bbf6e1423c6777fade8dbf7f8a6a832d
Braden Wong <bmw02002turbo@gmail.com>
2023-05-22 16:44:40 -0700

    feat: convert_to_mp3 file
    Former-commit-id: eb893f188b798e999057e16f34f8e6ec03dafb7f
    Former-commit-id: 51fffdbc8a5dce95c27adb1d06eee08a397ed12c

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,23 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![convert_to_mp3])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+use std::process::Command;
+
+#[tauri::command]
+fn convert_to_mp3(input: &str, output: &str) -> Result<String, String> {
+    let status = Command::new("lame").arg(input).arg(output).status();
+
+    match status {
+        Ok(_) => Ok(output.into()),
+        Err(e) => {
+            eprintln!("Failed to execute the lame command: {}", e);
+            Err(format!("Failed to execute the lame command: {}", e).into())
+        }
+    }
 }
--- src/lib/recorder/recordRtcRecorder.ts
+++ src/lib/recorder/recordRtcRecorder.ts
@@ -45,8 +45,15 @@ export async function stopRecording(): Promise<Blob> {
 async function saveAudioFile(blob: Blob) {
 	if (!window.__TAURI__) return;
 	const { writeBinaryFile, BaseDirectory } = await import('@tauri-apps/api/fs');
+	const { invoke } = await import('@tauri-apps/api');
+	const { appDataDir } = await import('@tauri-apps/api/path');
 	const buffer = await blob.arrayBuffer();
 	const uint8Array = new Uint8Array(buffer);
-	const filename = new Date().toISOString() + '.wav'; // get the current timestamp as ISO string
-	await writeBinaryFile(filename, uint8Array, { dir: BaseDirectory.AppData });
+	const timestampIsoString = new Date().toISOString().replace(/[-:.]/g, '');
+	await writeBinaryFile(`${timestampIsoString}.wav`, uint8Array, { dir: BaseDirectory.AppData });
+	const appDataDirPath = await appDataDir();
+	const r = await invoke('convert_to_mp3', {
+		input: `${appDataDirPath}${timestampIsoString}.wav`,
+		output: `${appDataDirPath}${timestampIsoString}.mp3`
+	});
 }
