96a88daf6ae980700b094e71d16a97bc72f3cd2f
Braden Wong <bmw02002turbo@gmail.com>
2023-05-23 22:16:17 -0700

    feat: Anchor.svelte

---
+++ src/lib/Anchor.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { Route } from "~lib/stores/currentRoute"
+
+  export let href: Route
+
+  let className: string = ""
+  export { className as class }
+</script>
+
+<style>
+</style>
