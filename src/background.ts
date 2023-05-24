d51f6b2c0cb4c8abf54ce1c15e7a632512ef38dd
Braden Wong <bmw02002turbo@gmail.com>
2023-05-24 08:15:21 -0700

    feat: toggle icon with Ctrl+Shift+I

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,15 @@
+// import toast from "svelte-french-toast/dist/core/toast"
+import octagonalSign from "data-base64:~assets/octagonal_sign.png"
+import studioMicrophone from "data-base64:~assets/studio_microphone.png"
+
+import { Storage } from "@plasmohq/storage/dist"
+
+import { writeText } from "~lib/apis/clipboard"
+import { startRecording, stopRecording } from "~lib/recorder/mediaRecorder"
+// import PleaseEnterAPIKeyToast from "~lib/toasts/PleaseEnterAPIKeyToast.svelte"
+// import SomethingWentWrongToast from "~lib/toasts/SomethingWentWrongToast.svelte"
+import { transcribeAudioWithWhisperApi } from "~lib/transcribeAudioWithWhisperApi"
+
 export {}
 
 chrome.runtime.onInstalled.addListener((details) => {
@@ -5,3 +17,31 @@ chrome.runtime.onInstalled.addListener((details) => {
     chrome.runtime.openOptionsPage()
   }
 })
+
+let isRecording = false
+chrome.commands.onCommand.addListener(function (command) {
+  if (command === "toggle-recording") {
+    toggleRecording()
+  }
+})
+
+async function toggleRecording() {
+  const storage = new Storage()
+  const apiKey = await storage.get("openai-api-key")
+  if (!apiKey) {
+    // toast.error(PleaseEnterAPIKeyToast)
+    return
+  }
+
+  if (!isRecording) {
+    // await startRecording()
+    isRecording = !isRecording
+    chrome.action.setIcon({ path: octagonalSign })
+  } else {
+    // const audioBlob = await stopRecording()
+    isRecording = !isRecording
+    chrome.action.setIcon({ path: studioMicrophone })
+    // const text = await transcribeAudioWithWhisperApi(audioBlob, get(apiKey))
+    // writeText(text)
+  }
+}
