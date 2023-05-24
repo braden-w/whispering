5e642ba79801f62f731e3096410caff10ad076dc
Braden Wong <bmw02002turbo@gmail.com>
2023-05-23 23:58:49 -0700

    chore: setApiKey is not async
    Former-commit-id: f99f2d31ae981e1e765170e24ce8f77f8ffa3ed2
    Former-commit-id: d74b00e0160e16c2735766929c0946b931228c56

--- src/lib/stores/apiKey.ts
+++ src/lib/stores/apiKey.ts
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store';
 
 export const apiKey = writable(localStorage.getItem('openai-api-key') || '');
 
-export async function setApiKey(inputApiKey: string) {
+export function setApiKey(inputApiKey: string) {
 	localStorage.setItem('openai-api-key', inputApiKey);
 	apiKey.set(inputApiKey);
 }
