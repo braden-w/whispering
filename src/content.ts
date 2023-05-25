15265ee38b443232fb249d1bd723e496ad64d46d
Braden Wong <bmw02002turbo@gmail.com>
2023-05-25 09:09:30 -0700

    chore: cleanup addListener

--- src/content.ts
+++ src/content.ts
@@ -7,17 +7,14 @@ export {};
 console.log('🚀 ~ file: content.ts:7 ~ export');
 
 chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
-	console.log('🚀 ~ file: content.ts:11 ~ request:', request);
 	if (request.name === 'startRecording') {
-		console.log('🚀 ~ file: content.ts:14 ~ startRecording:');
 		await startRecording();
-		sendResponse();
 	} else if (request.name === 'stopRecording') {
-		console.log('🚀 ~ file: content.ts:18 ~ stopRecording:');
 		const audioBlob = await stopRecording();
 		const apiKey = await getApiKey();
 		const text = await transcribeAudioWithWhisperApi(audioBlob, apiKey);
 		writeText(text);
-		sendResponse({ audioBlob });
+		sendResponse({ text });
 	}
+	return true;
 });
