d0826058b1cd89329571ec9a11349f16026167af
Braden Wong <bmw02002turbo@gmail.com>
2023-05-25 12:37:56 -0700

    feat: microphone icon

--- src/contents/chatGptButton.ts
+++ src/contents/chatGptButton.ts
@@ -14,7 +14,6 @@ window.onload = function () {
 		const buttonHTML = /*html*/ `
 <button
 	class="absolute p-1 rounded-md text-gray-500 bottom-1.5 md:bottom-2.5 hover:bg-gray-100 enabled:dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent right-1 md:right-2 disabled:opacity-40"
-	disabled=""
 >
 	<svg
 		stroke="currentColor"
@@ -28,6 +27,12 @@ window.onload = function () {
 		width="1em"
 		xmlns="http://www.w3.org/2000/svg"
 	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z"
+		/>
+	</svg>	
 		<line x1="22" y1="2" x2="11" y2="13"></line>
 		<polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
 	</svg>
