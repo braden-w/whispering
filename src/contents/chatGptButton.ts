5e53d5f828d62e6f2faaead43e2c81256c83814a
Braden Wong <bmw02002turbo@gmail.com>
2023-05-25 13:11:59 -0700

    fix: switch to stop sign

--- src/contents/chatGptButton.ts
+++ src/contents/chatGptButton.ts
@@ -67,7 +67,7 @@ window.onload = function () {
 					<path
 						stroke-linecap="round"
 						stroke-linejoin="round"
-						d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z"
+						d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z"
 					/>
 				`;
 					isRecording = true;
