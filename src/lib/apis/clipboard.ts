9956025786586372248ea1db81b92d7992a70fc8
Braden Wong <bmw02002turbo@gmail.com>
2023-05-25 15:39:52 -0700

    chore: tidy up clipboard

--- src/lib/apis/clipboard.ts
+++ src/lib/apis/clipboard.ts
@@ -1,40 +1,29 @@
 /**
- * Writes text to the user's clipboard without removing focus from the current input element.
- * @param text The text to write to the clipboard.
+ * Writes the specified text to the user's clipboard without removing focus from the current input element.
+ * @param text - The text to write to the clipboard.
  */
-export function writeText(text: string) {
-	// Store the currently focused element
+export function writeTextToClipboard(text: string) {
 	const previousFocus = document.activeElement as HTMLElement;
 
-	// Create a temporary textarea element
 	const textarea = document.createElement('textarea');
-
-	// Set the value of the textarea to the desired text
 	textarea.value = text;
-
-	// Make the textarea hidden
 	textarea.style.position = 'fixed';
 	textarea.style.opacity = '0';
 
-	// Append the textarea to the document
 	document.body.appendChild(textarea);
 
-	// Select the text within the textarea
 	textarea.select();
 
 	try {
-		// Copy the selected text to the clipboard
 		const successful = document.execCommand('copy');
 		if (!successful) {
-			throw new Error('Unable to write to clipboard.');
+			throw new Error('Failed to write text to the clipboard.');
 		}
-	} catch (err) {
-		console.error('Failed to write to clipboard:', err);
+	} catch (error) {
+		console.error('Failed to write text to the clipboard:', error);
 	} finally {
-		// Clean up by removing the temporary textarea
 		document.body.removeChild(textarea);
 
-		// Restore focus to the previously focused element
 		if (previousFocus && typeof previousFocus.focus === 'function') {
 			previousFocus.focus();
 		}
