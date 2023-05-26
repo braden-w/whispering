41e130b586f87742bfdbab311cc82b261a9635fb
Braden Wong <bmw02002turbo@gmail.com>
2023-05-26 11:09:38 -0700

    refactor: writeTextToClipboard

--- src/lib/apis/clipboard.ts
+++ src/lib/apis/clipboard.ts
@@ -1,32 +1,62 @@
 /**
  * Writes the specified text to the user's clipboard without removing focus from the current input element.
+ *
  * @param text - The text to write to the clipboard.
  */
-export function writeTextToClipboard(text: string) {
-	const previousFocus = document.activeElement as HTMLElement;
+export function writeTextToClipboard(text: string): void {
+	const previousFocus = document.activeElement as HTMLElement | null;
 
-	const textarea = document.createElement('textarea');
-	textarea.value = text;
-	textarea.style.position = 'fixed';
-	textarea.style.opacity = '0';
-
+	const textarea: HTMLTextAreaElement = createInvisibleTextArea(text);
 	document.body.appendChild(textarea);
 
-	textarea.select();
-
 	try {
-		const successful = document.execCommand('copy');
-		if (!successful) {
-			throw new Error('Failed to write text to the clipboard.');
-		}
+		copyToClipboard(textarea);
 	} catch (error) {
 		console.error('Failed to write text to the clipboard:', error);
 	} finally {
 		document.body.removeChild(textarea);
+		restorePreviousFocus(previousFocus);
+	}
+}
 
-		if (previousFocus && typeof previousFocus.focus === 'function') {
-			previousFocus.focus();
-		}
+/**
+ * Creates an invisible textarea with given text.
+ *
+ * @param {string} text - The text to be filled into the textarea.
+ * @returns {HTMLTextAreaElement} A textarea element.
+ */
+function createInvisibleTextArea(text: string): HTMLTextAreaElement {
+	const textarea: HTMLTextAreaElement = document.createElement('textarea');
+	textarea.value = text;
+	textarea.style.position = 'fixed';
+	textarea.style.opacity = '0';
+
+	return textarea;
+}
+
+/**
+ * Executes the copy command on the given textarea.
+ *
+ * @param {HTMLTextAreaElement} textarea - The textarea containing the text to be copied.
+ * @throws {Error} If failed to execute the copy command.
+ */
+function copyToClipboard(textarea: HTMLTextAreaElement): void {
+	textarea.select();
+
+	const successful = document.execCommand('copy');
+	if (!successful) {
+		throw new Error('Failed to write text to the clipboard.');
+	}
+}
+
+/**
+ * Restores focus to the previously focused element, if it exists.
+ *
+ * @param {HTMLElement | null} element - The previously focused element.
+ */
+function restorePreviousFocus(element: HTMLElement | null): void {
+	if (element && typeof element.focus === 'function') {
+		element.focus();
 	}
 }
 
