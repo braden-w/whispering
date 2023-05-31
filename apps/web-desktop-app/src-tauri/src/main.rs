18b50a62e7614dbb628e550312701122075f097c
Braden Wong <bmw02002turbo@gmail.com>
2023-05-31 16:40:15 -0700

    feat: working paste command
    Former-commit-id: 624d6a5178e456bed0bd0fa13118341395d15b4e
    Former-commit-id: 585c45f25010c8aa6230cbb2378d18097a9f596c

--- apps/web-desktop-app/src-tauri/src/main.rs
+++ apps/web-desktop-app/src-tauri/src/main.rs
@@ -3,7 +3,7 @@
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![convert_to_mp3])
+        .invoke_handler(tauri::generate_handler![convert_to_mp3, paste])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -18,4 +18,20 @@ fn convert_to_mp3(input: &str, output: &str) -> Result<String, String> {
         .status()
         .map_err(|e| format!("Failed to execute the lame command: {}", e))
         .map(|_| format!("Successfully converted {} to {}", input, output))
+}
+
+use enigo::{Enigo, Key, KeyboardControllable};
+
+#[tauri::command]
+fn paste() {
+    let mut enigo = Enigo::new();
+    if cfg!(target_os = "macos") {
+        enigo.key_down(Key::Meta);
+        enigo.key_click(Key::Layout('v'));
+        enigo.key_up(Key::Meta);
+    } else {
+        enigo.key_down(Key::Control);
+        enigo.key_click(Key::Layout('v'));
+        enigo.key_up(Key::Control);
+    }
 }
