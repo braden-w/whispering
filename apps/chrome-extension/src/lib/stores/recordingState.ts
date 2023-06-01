9cdb7514b2fd30928f524825ee31302efe613bde
Braden Wong <bmw02002turbo@gmail.com>
2023-05-31 17:17:19 -0700

    chore: extract creation of stores
    Former-commit-id: 1fad2a062291da8ae22f184d90c77846b9f3f027
    Former-commit-id: 7265b0a36fab208538fab72f85e65a91e45b9f8d

--- apps/chrome-extension/src/lib/stores/recordingState.ts
+++ apps/chrome-extension/src/lib/stores/recordingState.ts
@@ -1,29 +1,5 @@
-import { Storage } from '@plasmohq/storage/dist';
-import { writable } from 'svelte/store';
-
-export const recordingState = createRecordingStateStore();
+import { createStore } from './createStore';
 
 type RecordingState = 'idle' | 'recording' | 'transcribing';
-function createRecordingStateStore() {
-	const initialRecordingState: RecordingState = 'idle';
-	const recordingStateStore = writable<RecordingState>(initialRecordingState);
-	const { subscribe, set, update } = recordingStateStore;
-	const storage = new Storage();
-
-	async function init() {
-		const recordingStateFromStorage = await storage.get<RecordingState>('recording-state');
-		set(recordingStateFromStorage || initialRecordingState);
-	}
-
-	async function setRecordingState(value: RecordingState) {
-		await storage.set('recording-state', value);
-		set(value);
-	}
-
-	return {
-		init,
-		subscribe,
-		set: setRecordingState,
-		update
-	};
-}
+const initialRecordingState: RecordingState = 'idle';
+export const recordingState = createStore('recording-state', initialRecordingState);
