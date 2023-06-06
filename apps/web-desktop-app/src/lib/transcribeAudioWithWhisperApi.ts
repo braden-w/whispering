54d93296769065a833bf03041387f62a05750533
Braden Wong <bmw02002turbo@gmail.com>
2023-06-06 11:43:25 -0700

    feat: add multilingual support
    Former-commit-id: 7d2650581beb849136b54ed73956ada8c3117cfb
    Former-commit-id: 25c8849492e28cc8baf4c36a8e88084c46f136bd

--- apps/web-desktop-app/src/lib/transcribeAudioWithWhisperApi.ts
+++ apps/web-desktop-app/src/lib/transcribeAudioWithWhisperApi.ts
@@ -11,7 +11,6 @@ export async function transcribeAudioWithWhisperApi(
 	const formData = new FormData();
 	formData.append('file', wavFile);
 	formData.append('model', 'whisper-1');
-	formData.append('language', 'en');
 
 	const response = await fetch('https://api.openai.com/v1/audio/transcriptions', {
 		method: 'POST',
