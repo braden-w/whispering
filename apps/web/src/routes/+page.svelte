0415f13761a2d98f191082fc306402bd17f89055
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-20 17:47:04 -0800

    feat: simplfiy module resolution
    Former-commit-id: f03a5dff560ef042652176dd3bd3517d8076d00c
    Former-commit-id: 250d385fbf69b39c17760809b20166de17b37aad

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+	import { cn } from '@repo/ui/utils';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <Button variant="outline">Test</Button> -->
