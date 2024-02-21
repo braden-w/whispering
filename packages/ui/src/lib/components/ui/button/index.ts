715bb0ef31ff7e9d2531fb9e2009bcf1eaf98e5c
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-21 15:29:39 -0800

    feat: restore rounded-md
    Former-commit-id: 3bce9f49272e6da8cbfc474611acebbaf7b441d5
    Former-commit-id: 46b593d0d65d9f6e0437fc4af75e051c64735bcc

--- packages/ui/src/lib/components/ui/button/index.ts
+++ packages/ui/src/lib/components/ui/button/index.ts
@@ -3,7 +3,7 @@ import { tv, type VariantProps } from 'tailwind-variants';
 import type { Button as ButtonPrimitive } from 'bits-ui';
 
 const buttonVariants = tv({
-	base: 'inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium ring-offset-background transition duration-200 ease-in-out focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+	base: 'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition duration-200 ease-in-out focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
 	variants: {
 		variant: {
 			default: 'bg-primary text-primary-foreground hover:bg-primary/90',
@@ -15,8 +15,8 @@ const buttonVariants = tv({
 		},
 		size: {
 			default: 'h-10 px-4 py-2',
-			sm: 'h-9 rounded-lg px-3',
-			lg: 'h-11 rounded-lg px-8',
+			sm: 'h-9 rounded-md px-3',
+			lg: 'h-11 rounded-md px-8',
 			icon: 'h-10 w-10'
 		}
 	},
