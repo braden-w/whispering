b06aabe8929ea0bfd78ee326585a47470ead61db
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-20 17:03:28 -0800

    chore: install all of shadcn svelte
    Former-commit-id: 9d8b26119afdeb6efe0e6beabf7095119e2733c6
    Former-commit-id: 020fec7d7ccbc5848cb0a1e93db6c75a2d2292d1

---
+++ packages/ui/src/lib/components/ui/form/form-button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import * as Button from "$lib/components/ui/button";
+
+	type $$Props = Button.Props;
+</script>
+
+<Button.Root type="submit" {...$$restProps}>
+	<slot />
+</Button.Root>
---
+++ packages/ui/src/lib/components/ui/form/form-description.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import * as FormPrimitive from "formsnap";
+	import { cn } from "$lib/utils";
+	import type { HTMLAttributes } from "svelte/elements";
+
+	type $$Props = HTMLAttributes<HTMLSpanElement>;
+	let className: string | undefined | null = undefined;
+	export { className as class };
+</script>
+
+<FormPrimitive.Description
+	class={cn("text-sm text-muted-foreground", className)}
+	{...$$restProps}
+	let:descriptionAttrs
+>
+	<slot {descriptionAttrs} />
+</FormPrimitive.Description>
