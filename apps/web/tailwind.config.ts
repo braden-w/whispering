58b1bcf0058eaabd472919a7704a90cba09a250d
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-20 22:12:29 -0800

    feat: extend config in tailwind config ts
    Former-commit-id: 5f0866d328db2d2b9f4f0e23fe0f9c5c8aa1f013
    Former-commit-id: 6cc5a53899ef75652dda3732ca939099f7b2636f

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,1 +1,12 @@
-export * from '@repo/ui/tailwind.config';
+import base from '@repo/ui/tailwind.config';
+import type { Config } from 'tailwindcss';
+
+const config = {
+	presets: [base],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'../../packages/ui/src/lib/components/**/*.{svelte,ts}'
+	]
+} satisfies Config;
+
+export default config;
