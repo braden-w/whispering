e65f53d3afc7504084b92ac3479734cf6b8515d6
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-22 14:59:03 -0800

    feat: better  WhisperFileTooLarge error
    Former-commit-id: 1b401f70d3b44ba387c00f52b973ab0065f2da1d
    Former-commit-id: 55851a9574907c5615c70c9d5ff76f07569b32de

--- packages/recorder/src/services/transcription/whisper.ts
+++ packages/recorder/src/services/transcription/whisper.ts
@@ -1,20 +1,19 @@
 import type { Context } from 'effect';
 import { Data, Effect } from 'effect';
-import type { TranscriptionService } from '.';
+import { TranscriptionError, type TranscriptionService } from '.';
 
 function isString(input: unknown): input is string {
 	return typeof input === 'string';
 }
 
+const MAX_FILE_SIZE_MB = 25 as const;
+
 export const whisperTranscriptionService: Context.Tag.Service<TranscriptionService> = {
-	transcribe: (blob) =>
+	transcribe: (audioBlob) =>
 		Effect.gen(function* (_) {
-			if (audioBlob.size > 25 * 1024 * 1024) {
-				return yield* _(
-					new WhisperFileTooLarge({
-						message: 'The file is too large. Please upload a file smaller than 25MB.'
-					})
-				);
+			const blobSizeInMb = audioBlob.size / (1024 * 1024);
+			if (blobSizeInMb > MAX_FILE_SIZE_MB) {
+				return yield* _(new WhisperFileTooLarge(blobSizeInMb, MAX_FILE_SIZE_MB));
 			}
 			const fileName = 'recording.wav';
 			const wavFile = new File([audioBlob], fileName);
@@ -39,14 +38,22 @@ export const whisperTranscriptionService: Context.Tag.Service<TranscriptionServi
 		})
 };
 
-class WhisperFileTooLarge extends Data.TaggedError('WhisperFileTooLarge')<{
-	message: string;
-}> {}
+class WhisperFileTooLarge extends TranscriptionError {
+	constructor(fileSizeMb: number, maxFileSizeMb: number) {
+		super({
+			message: `The file size (${fileSizeMb}MB) is too large. Please upload a file smaller than ${maxFileSizeMb}MB.`
+		});
+	}
+}
 
-class WhisperFetchError extends Data.TaggedError('WhisperFetchError')<{
-	origError: unknown;
-}> {}
+class WhisperFetchError extends TranscriptionError {
+	constructor(origError: unknown) {
+		super({ origError });
+	}
+}
 
-class TranscriptionIsNotStringError extends Data.TaggedError('TranscriptionIsNotStringError')<{
-	transcription: unknown;
-}> {}
+class TranscriptionIsNotStringError extends TranscriptionError {
+	constructor(transcription: unknown) {
+		super({ origError: transcription });
+	}
+}
