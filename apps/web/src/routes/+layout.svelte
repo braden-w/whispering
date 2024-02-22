df98deb7e769a0ffdcdbedfb82df697ce676fcd0
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-22 15:06:09 -0800

    chore: layout and stuff
    Former-commit-id: fc6472512b1062ad21862392d7a4e1e636d09139
    Former-commit-id: c65fe4b924b2ada20d9d9fc81e028d29984a7b80

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-	import { toggleRecording } from '$lib/recorder/toggleRecording';
 	import { options } from '$lib/stores/options';
 	import { registerShortcut, unregisterAllShortcuts } from '$lib/system-apis/shortcuts';
 	import '@repo/ui/app.pcss';
 	import { ModeWatcher, Toaster } from '@repo/ui/components/sonner';
 	import { onDestroy, onMount } from 'svelte';
 
-	onMount(async () => await registerShortcut($options.currentGlobalShortcut, toggleRecording));
-	onDestroy(async () => await unregisterAllShortcuts());
+	// onMount(async () => await registerShortcut($options.currentGlobalShortcut, toggleRecording));
+	// onDestroy(async () => await unregisterAllShortcuts());
 </script>
 
 <svelte:head>
