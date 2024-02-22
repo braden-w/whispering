73a5dfbfca7cbeaf52664e02c399f8d1a62b42f3
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-21 17:22:26 -0800

    formatting: flatten
    Former-commit-id: fb69a75588ccf851261fdf823b8b15ce10c5dce5
    Former-commit-id: 74a6bd9c78faf6f12c1b5e73cccdb223836370d4

--- apps/web/src/routes/shortcut/+page.svelte
+++ apps/web/src/routes/shortcut/+page.svelte
@@ -5,42 +5,39 @@
 </script>
 
 <div class="flex min-h-screen items-center justify-center">
-	<section>
-		<div class="container flex flex-col items-center gap-4 px-4 text-center md:px-6">
-			<h1 class="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
-				Change Your Keyboard Shortcut
-			</h1>
-			<p class="text-muted-foreground text-xl">
-				You can access keyboard shortcuts on the extension and desktop app.
-			</p>
-
-			<div class="flex flex-col gap-2 md:flex-row">
-				<Button
-					size="lg"
-					variant="default"
-					href="https://github.com/braden-w/whispering/releases"
-					target="_blank"
-					rel="noopener noreferrer"
-					title="Check out the Chrome Extension"
-					aria-label="Check out the Chrome Extension"
-				>
-					<DesktopIcon class="mr-2 h-6 w-6" />
-					Download for Desktop
-				</Button>
-				<Button
-					size="lg"
-					variant="outline"
-					href="https://chrome.google.com/webstore/detail/whispering/oilbfihknpdbpfkcncojikmooipnlglo?hl=en&authuser=0"
-					target="_blank"
-					rel="noopener noreferrer"
-					title="Check out the desktop app"
-					aria-label="Check out the desktop app"
-				>
-					<ChromeWebstoreIcon class="mr-2 h-6 w-6" />
-					Get Chrome Extension
-				</Button>
-			</div>
-			<Button href="/" variant="link">Go back</Button>
+	<section class="container flex flex-col items-center gap-4 px-4 text-center md:px-6">
+		<h1 class="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
+			Change Your Keyboard Shortcut
+		</h1>
+		<p class="text-muted-foreground text-xl">
+			You can access keyboard shortcuts on the extension and desktop app.
+		</p>
+		<div class="flex flex-col gap-2 md:flex-row">
+			<Button
+				size="lg"
+				variant="default"
+				href="https://github.com/braden-w/whispering/releases"
+				target="_blank"
+				rel="noopener noreferrer"
+				title="Check out the Chrome Extension"
+				aria-label="Check out the Chrome Extension"
+			>
+				<DesktopIcon class="mr-2 h-6 w-6" />
+				Download for Desktop
+			</Button>
+			<Button
+				size="lg"
+				variant="outline"
+				href="https://chrome.google.com/webstore/detail/whispering/oilbfihknpdbpfkcncojikmooipnlglo?hl=en&authuser=0"
+				target="_blank"
+				rel="noopener noreferrer"
+				title="Check out the desktop app"
+				aria-label="Check out the desktop app"
+			>
+				<ChromeWebstoreIcon class="mr-2 h-6 w-6" />
+				Get Chrome Extension
+			</Button>
 		</div>
+		<Button href="/" variant="link">Go back</Button>
 	</section>
 </div>
