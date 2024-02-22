baf3f7da44594c82bf69c29ab7a10ef8c9c9c6f1
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-21 23:50:55 -0800

    feat: sync function in createRecordings
    Former-commit-id: 2f1595453d2b8c48514c67e77d1d579792ee6681
    Former-commit-id: ae97f2a813f2984defcb14670067b5f76f8bfa6f

--- apps/web/src/lib/stores/recordings/index.ts
+++ apps/web/src/lib/stores/recordings/index.ts
@@ -1,10 +1,25 @@
-import type { Recording } from '@repo/recorder';
-import { writable } from 'svelte/store';
+import { RecordingsDb, type Recording } from '@repo/recorder';
+import { Effect } from 'effect';
+import { writable, type Writable } from 'svelte/store';
+
+// readonly getAllRecordings: Effect.Effect<Recording[], GetAllRecordingsError>;
+// readonly getRecording: (id: string) => Effect.Effect<Recording | undefined, GetRecordingError>;
+// readonly addRecording: (recording: Recording) => Effect.Effect<void, AddRecordingError>;
+// readonly editRecording: (
+// 	id: string,
+// 	recording: Recording
+// ) => Effect.Effect<void, EditRecordingError>;
+// readonly deleteRecording: (id: string) => Effect.Effect<void, DeleteRecordingError>;
 
 export function createRecordings() {
-	const { subscribe, update } = writable<Recording[]>([]);
+	const { subscribe, set, update } = writable<Recording[]>([]);
 	return {
 		subscribe,
+		sync: Effect.gen(function* (_) {
+			const recordingsDb = yield* _(RecordingsDb);
+			const recordings = yield* _(recordingsDb.getAllRecordings);
+			set(recordings);
+		}),
 		addRecording: (recording: Recording) => {
 			update((recordings) => [...recordings, recording]);
 		},
