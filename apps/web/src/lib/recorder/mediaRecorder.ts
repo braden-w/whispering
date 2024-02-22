69f93763e6b16e35c55211bb961739b331ff8b13
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-21 19:42:48 -0800

    fix: return yield * _()
    Former-commit-id: 35994d023de3d2992651f9b85e074d66152d3f0c
    Former-commit-id: 69646d2d64a55fcb6d7d90242d1d6b3afde17633

--- apps/web/src/lib/recorder/mediaRecorder.ts
+++ apps/web/src/lib/recorder/mediaRecorder.ts
@@ -1,23 +1,10 @@
 import AudioRecorder from 'audio-recorder-polyfill';
 import { Data, Effect } from 'effect';
 
-let mediaRecorder: MediaRecorder | null = null;
-let recordedChunks: Blob[] = [];
-
-export const startRecording = () =>
-	Effect.gen(function* (_) {
-		const stream = yield* _(getMediaStream);
-		mediaRecorder = new AudioRecorder(stream);
-		if (!mediaRecorder) yield* _(new MediaRecorderNotInitializedError());
-		mediaRecorder.addEventListener('dataavailable', (event: BlobEvent) => {
-			recordedChunks.push(event.data);
-		});
-		mediaRecorder.start();
-	});
-
 class GetNavigatorMediaError extends Data.TaggedError('GetNavigatorMediaError')<{
 	origError: unknown;
 }> {}
+
 class MediaRecorderNotInitializedError extends Data.TaggedError(
 	'MediaRecorderNotInitializedError'
 ) {}
@@ -26,6 +13,20 @@ const getMediaStream = Effect.tryPromise({
 	try: () => navigator.mediaDevices.getUserMedia({ audio: true }),
 	catch: (error) => new GetNavigatorMediaError({ origError: error })
 });
+
+let mediaRecorder: MediaRecorder | null = null;
+let recordedChunks: Blob[] = [];
+
+export const startRecording = () =>
+	Effect.gen(function* (_) {
+		const stream = yield* _(getMediaStream);
+		mediaRecorder = new AudioRecorder(stream);
+		if (!mediaRecorder) return yield* _(new MediaRecorderNotInitializedError());
+		mediaRecorder.addEventListener('dataavailable', (event: BlobEvent) => {
+			recordedChunks.push(event.data);
+		});
+		mediaRecorder.start();
+	});
 
 export async function stopRecording(): Promise<Blob> {
 	return new Promise((resolve) => {
