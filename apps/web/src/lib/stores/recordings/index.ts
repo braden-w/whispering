5b77cd19367cae0834e3c52ba257de727a3b7b84
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-21 23:53:17 -0800

    feat: update impl of createRecordings
    Former-commit-id: 03aaae6b746fca8d886b3a73ea8f5c4edb816703
    Former-commit-id: b00c72bc781692ea6f73a6c9264fcffc8414c281

--- apps/web/src/lib/stores/recordings/index.ts
+++ apps/web/src/lib/stores/recordings/index.ts
@@ -20,35 +20,44 @@ export function createRecordings() {
 			const recordings = yield* _(recordingsDb.getAllRecordings);
 			set(recordings);
 		}),
-		addRecording: (recording: Recording) => {
-			update((recordings) => [...recordings, recording]);
-		},
-		deleteRecording: (id: string) => {
-			update((recordings) => recordings.filter((recording) => recording.id !== id));
-		},
-		setRecording: (id: string, recording: Recording) => {
-			update((recordings) => {
-				const index = recordings.findIndex((recording) => recording.id === id);
-				if (index === -1) return recordings;
-				recordings[index] = recording;
-				return recordings;
-			});
-		},
-		setRecordingState: (id: string, state: RecordingState) => {
-			update((recordings) => {
-				const index = recordings.findIndex((recording) => recording.id === id);
-				if (index === -1) return recordings;
-				recordings[index].state = state;
-				return recordings;
-			});
-		},
-		setRecordingTranscription: (id: string, transcription: string) => {
-			update((recordings) => {
-				const index = recordings.findIndex((recording) => recording.id === id);
-				if (index === -1) return recordings;
-				recordings[index].transcription = transcription;
-				return recordings;
-			});
-		}
+		addRecording: (recording: Recording) =>
+			Effect.gen(function* (_) {
+				const recordingsDb = yield* _(RecordingsDb);
+				yield* _(recordingsDb.addRecording(recording));
+				update((recordings) => [...recordings, recording]);
+			}),
+		editRecording: (id: string, recording: Recording) =>
+			Effect.gen(function* (_) {
+				const recordingsDb = yield* _(RecordingsDb);
+				yield* _(recordingsDb.editRecording(id, recording));
+				update((recordings) => {
+					const index = recordings.findIndex((recording) => recording.id === id);
+					if (index === -1) return recordings;
+					recordings[index] = recording;
+					return recordings;
+				});
+			}),
+		deleteRecording: (id: string) =>
+			Effect.gen(function* (_) {
+				const recordingsDb = yield* _(RecordingsDb);
+				yield* _(recordingsDb.deleteRecording(id));
+				update((recordings) => recordings.filter((recording) => recording.id !== id));
+			})
+		// setRecordingState: (id: string, state: RecordingState) => {
+		// 	update((recordings) => {
+		// 		const index = recordings.findIndex((recording) => recording.id === id);
+		// 		if (index === -1) return recordings;
+		// 		recordings[index].state = state;
+		// 		return recordings;
+		// 	});
+		// },
+		// setRecordingTranscription: (id: string, transcription: string) => {
+		// 	update((recordings) => {
+		// 		const index = recordings.findIndex((recording) => recording.id === id);
+		// 		if (index === -1) return recordings;
+		// 		recordings[index].transcription = transcription;
+		// 		return recordings;
+		// 	});
+		// }
 	};
 }
