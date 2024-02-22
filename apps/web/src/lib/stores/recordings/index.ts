d9265908360a81ed2bab5d61422005fbff213c2e
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-22 00:51:26 -0800

    refactor: transcribeAudioWithWhisperApi
    Former-commit-id: 5dfea0c36af244d9fa565883292c3c578c1f74be
    Former-commit-id: 47397bd85326a64833047c36adfc269258068a06

--- apps/web/src/lib/stores/recordings/index.ts
+++ apps/web/src/lib/stores/recordings/index.ts
@@ -1,4 +1,5 @@
 import { RecordingsDb, type Recording, RecordingNotFound } from '@repo/recorder';
+import { transcribeAudioWithWhisperApi } from '@repo/recorder/whisper';
 import { Data, Effect } from 'effect';
 import { get, writable } from 'svelte/store';
 import { apiKey } from '../apiKey';
@@ -49,40 +50,3 @@ export function createRecordings() {
 			})
 	};
 }
-
-const transcribeAudioWithWhisperApi = (audioBlob: Blob, WHISPER_API_KEY: string) =>
-	Effect.gen(function* (_) {
-		if (audioBlob.size > 25 * 1024 * 1024) {
-			return yield* _(
-				new WhisperFileTooLarge({
-					message: 'The file is too large. Please upload a file smaller than 25MB.'
-				})
-			);
-		}
-		const fileName = 'recording.wav';
-		const wavFile = new File([audioBlob], fileName);
-		const formData = new FormData();
-		formData.append('file', wavFile);
-		formData.append('model', 'whisper-1');
-		const data = yield* _(
-			Effect.tryPromise({
-				try: () =>
-					fetch('https://api.openai.com/v1/audio/transcriptions', {
-						method: 'POST',
-						headers: { Authorization: `Bearer ${WHISPER_API_KEY}` },
-						body: formData
-					}).then((res) => res.json()),
-				catch: (error) => new WhisperFetchError({ origError: error })
-			})
-		);
-		console.log('🚀 ~ Effect.gen ~ data:', data);
-		return data.text;
-	});
-
-class WhisperFileTooLarge extends Data.TaggedError('WhisperFileTooLarge')<{
-	message: string;
-}> {}
-
-class WhisperFetchError extends Data.TaggedError('WhisperFetchError')<{
-	origError: unknown;
-}> {}
