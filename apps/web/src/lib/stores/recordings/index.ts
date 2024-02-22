59078e93e8e23bf53e63511002c5d7c6004dfccb
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-21 23:53:23 -0800

    chore: remove dead code
    Former-commit-id: 05ef8fc74d346d05178f77e11f7ad612411e5f32
    Former-commit-id: 88563a48c09157e5d5951b374f5527c434fe09a8

--- apps/web/src/lib/stores/recordings/index.ts
+++ apps/web/src/lib/stores/recordings/index.ts
@@ -1,15 +1,6 @@
 import { RecordingsDb, type Recording } from '@repo/recorder';
 import { Effect } from 'effect';
-import { writable, type Writable } from 'svelte/store';
-
-// readonly getAllRecordings: Effect.Effect<Recording[], GetAllRecordingsError>;
-// readonly getRecording: (id: string) => Effect.Effect<Recording | undefined, GetRecordingError>;
-// readonly addRecording: (recording: Recording) => Effect.Effect<void, AddRecordingError>;
-// readonly editRecording: (
-// 	id: string,
-// 	recording: Recording
-// ) => Effect.Effect<void, EditRecordingError>;
-// readonly deleteRecording: (id: string) => Effect.Effect<void, DeleteRecordingError>;
+import { writable } from 'svelte/store';
 
 export function createRecordings() {
 	const { subscribe, set, update } = writable<Recording[]>([]);
