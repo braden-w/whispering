13d8fd62d49f4fc4dfb0707281d99a99ecabc47a
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-22 15:03:07 -0800

    fix: TranscriptionError and such
    Former-commit-id: bd0a228c15debe2be3ad896beca650d993566ffc
    Former-commit-id: 84940875250bfe6e0385035ddf24b81b199b3198

--- packages/recorder/src/services/transcription/index.ts
+++ packages/recorder/src/services/transcription/index.ts
@@ -1,57 +1,14 @@
-import { Effect } from 'effect';
+import type { Effect } from 'effect';
 import { Context, Data } from 'effect';
-
-export class CreateTranscriptionService extends Context.Tag('CreateTranscriptionService')<
-	CreateTranscriptionService,
-	{
-		readonly createTranscriptionService: () => Effect.Effect<TranscriptionService>;
-	}
->() {}
 
 export class TranscriptionService extends Context.Tag('TranscriptionService')<
 	TranscriptionService,
 	{
-		readonly transcribe: (blob: Blob) => Effect.Effect<string, TranscribeError>;
+		readonly transcribe: (blob: Blob) => Effect.Effect<string, TranscriptionError>;
 	}
 >() {}
 
-class TranscribeError extends Data.TaggedError('TranscribeError')<{
-	origError: unknown;
-}> {}
-
-export const transcribeAudioWithWhisperApi = (audioBlob: Blob, WHISPER_API_KEY: string) =>
-	Effect.gen(function* (_) {
-		if (audioBlob.size > 25 * 1024 * 1024) {
-			return yield* _(
-				new WhisperFileTooLarge({
-					message: 'The file is too large. Please upload a file smaller than 25MB.'
-				})
-			);
-		}
-		const fileName = 'recording.wav';
-		const wavFile = new File([audioBlob], fileName);
-		const formData = new FormData();
-		formData.append('file', wavFile);
-		formData.append('model', 'whisper-1');
-		const data = yield* _(
-			Effect.tryPromise({
-				try: () =>
-					fetch('https://api.openai.com/v1/audio/transcriptions', {
-						method: 'POST',
-						headers: { Authorization: `Bearer ${WHISPER_API_KEY}` },
-						body: formData
-					}).then((res) => res.json()),
-				catch: (error) => new WhisperFetchError({ origError: error })
-			})
-		);
-		console.log('🚀 ~ Effect.gen ~ data:', data);
-		return data.text;
-	});
-
-class WhisperFileTooLarge extends Data.TaggedError('WhisperFileTooLarge')<{
+export class TranscriptionError extends Data.TaggedError('TranscribeError')<{
 	message: string;
-}> {}
-
-class WhisperFetchError extends Data.TaggedError('WhisperFetchError')<{
-	origError: unknown;
+	origError?: unknown;
 }> {}
