0c4f0dad07d29b39dc9b23a04ca52c8991dfdc80
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-23 12:18:42 -0800

    feat: modify gap
    Former-commit-id: cc496613510d3a4f950873227b50d4c60de657b4
    Former-commit-id: 661d9907ff31a91333856f1dc748a4f1da2b8c76

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -93,7 +93,7 @@
 
 <svelte:window on:keydown={handleKeyDown} />
 
-<div class="flex flex-col items-center justify-center gap-8 text-center">
+<div class="flex flex-col items-center justify-center gap-6 text-center">
 	<div class="flex flex-col gap-4">
 		<h1 class="scroll-m=20 text-4xl font-bold tracking-tight lg:text-5xl">Start recording</h1>
 		<p class="text-muted-foreground">
