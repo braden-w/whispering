f222414fe2a706706191440feaa1be79fa724fb2
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-23 00:17:36 -0800

    feat: app bar
    Former-commit-id: e0444576ec169aa60530c126ddac8327ec157eea
    Former-commit-id: 379d0291e6e617479b63ffafcac938b76986e0a6

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	import { options } from '$lib/stores/options';
-	import { registerShortcut, unregisterAllShortcuts } from '$lib/system-apis/shortcuts';
 	import '@repo/ui/app.pcss';
-	import { ModeWatcher, Toaster } from '@repo/ui/components/sonner';
-	import { onDestroy, onMount } from 'svelte';
+	import { Button } from '@repo/ui/components/button';
+	import { ModeWatcher, Toaster, toggleMode } from '@repo/ui/components/sonner';
+	import MoonIcon from '~icons/lucide/moon';
+	import SunIcon from '~icons/lucide/sun';
+	import SettingsIcon from '~icons/mdi/settings';
 
 	// onMount(async () => await registerShortcut($options.currentGlobalShortcut, toggleRecording));
 	// onDestroy(async () => await unregisterAllShortcuts());
@@ -13,6 +14,36 @@
 	<title>Whispering</title>
 </svelte:head>
 
-<slot />
+<div class="relative flex min-h-screen flex-col">
+	<header
+		class="border-border/4 bg-background/95 supports-[backdrop-filter]:bg-background/6t0 sticky top-0 z-50 border-b shadow-sm backdrop-blur"
+	>
+		<div class="flex h-[50px] items-center justify-between px-4 md:px-6">
+			<div class="flex items-center gap-2 text-lg font-semibold">
+				<span class="font-bold">whispering</span>
+				<span aria-hidden="true" role="img"> 🎙️ </span>
+			</div>
+			<div class="flex items-center gap-4">
+				<Button size="icon" variant="ghost">
+					<SettingsIcon class="h-5 w-5" />
+					<span class="sr-only">Settings</span>
+				</Button>
+				<Button on:click={toggleMode} size="icon" variant="ghost">
+					<SunIcon
+						class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+					/>
+					<MoonIcon
+						class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+					/>
+					<span class="sr-only">Toggle dark mode</span>
+				</Button>
+			</div>
+		</div>
+	</header>
+	<main class="flex flex-1 items-center justify-center p-4">
+		<slot />
+	</main>
+</div>
+
 <Toaster />
 <ModeWatcher />
