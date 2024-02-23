cd3628d7ce9ebb36b5dbf1de9d8f871e76e5a44f
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-22 23:27:17 -0800

    refactor: single tap
    Former-commit-id: 889b3c0f7f0cdb6d200fa902a09f69a854bc954e
    Former-commit-id: 8d81d79cf99a87516959c35ec3ff981b21d65e32

--- apps/web/src/lib/stores/recordings/index.ts
+++ apps/web/src/lib/stores/recordings/index.ts
@@ -5,9 +5,10 @@ import { indexedDbService } from './indexed-db-service';
 import { TranscriptionService } from '@repo/recorder/services/transcription';
 import { whisperTranscriptionService } from '@repo/recorder/implementations/transcription/whisper.ts';
 
-export const recordings = createRecordings
-	.pipe(Effect.provideService(RecordingsDbService, indexedDbService))
-	.pipe(Effect.provideService(TranscriptionService, whisperTranscriptionService))
-	.pipe(Effect.runSync);
+export const recordings = createRecordings.pipe(
+	Effect.provideService(RecordingsDbService, indexedDbService),
+	Effect.provideService(TranscriptionService, whisperTranscriptionService),
+	Effect.runSync
+);
 
 recordings.sync.pipe(Effect.runPromise);
