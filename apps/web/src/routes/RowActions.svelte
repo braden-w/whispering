693b2b3b534ddd4cd47c17eb34a310c196fc12e0
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-22 22:43:06 -0800

    fix: imports
    Former-commit-id: fea837288919479a4e9cf1b28d1e22c05a747728
    Former-commit-id: 51153cfea52a6fd0fcb31bef6e773f71338409bf

--- apps/web/src/routes/RowActions.svelte
+++ apps/web/src/routes/RowActions.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import { recordings } from '$lib/stores/recordingState';
+	import { recordings } from '$lib/stores/recordings';
 	import { Button } from '@repo/ui/components/button';
 	import { Effect } from 'effect';
-	import TranscriptionIcon from '~icons/material-symbols/speech-to-text';
 	import TrashIcon from '~icons/heroicons/trash';
+	import TranscriptionIcon from '~icons/material-symbols/speech-to-text';
 
 	export let id: string;
 </script>
