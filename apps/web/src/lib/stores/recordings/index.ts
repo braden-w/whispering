4fb02973a468197a6bae7911d8fac558c8031955
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-22 20:33:05 -0800

    feat: Transcription errors and such
    Former-commit-id: 881397e1fead685c12c62cfa08017c5e7e1a1b98
    Former-commit-id: 9cf34449edc31c4b5f543ff25d784d9423b047ca

--- apps/web/src/lib/stores/recordings/index.ts
+++ apps/web/src/lib/stores/recordings/index.ts
@@ -1,50 +1,78 @@
+import type {
+	AddRecordingError,
+	DeleteRecordingError,
+	EditRecordingError,
+	GetAllRecordingsError,
+	GetRecordingError
+} from '@repo/recorder';
 import { RecordingNotFound, RecordingsDbService, type Recording } from '@repo/recorder';
+import type { TranscriptionError } from '@repo/recorder/services/transcription';
 import { TranscriptionService } from '@repo/recorder/services/transcription';
-import { Effect } from 'effect';
-import { writable } from 'svelte/store';
+import { Context, Effect } from 'effect';
+import { writable, type Readable } from 'svelte/store';
 
-export const createRecordings = () =>
-	Effect.gen(function* (_) {
-		const recordingsDb = yield* _(RecordingsDbService);
-		const allRecordings = yield* _(recordingsDb.getAllRecordings);
-		const { subscribe, set, update } = writable<Recording[]>(allRecordings);
-		const editRecording = (id: string, recording: Recording) =>
-			Effect.gen(function* (_) {
-				yield* _(recordingsDb.editRecording(id, recording));
-				update((recordings) => {
-					const index = recordings.findIndex((recording) => recording.id === id);
-					if (index === -1) return recordings;
-					recordings[index] = recording;
-					return recordings;
-				});
+class RecordingsStateService extends Context.Tag('RecordingsStateService')<
+	RecordingsStateService,
+	{
+		readonly subscribe: Readable<Recording[]>['subscribe'];
+		readonly sync: Effect.Effect<void, GetAllRecordingsError>;
+		readonly addRecording: (recording: Recording) => Effect.Effect<void, AddRecordingError>;
+		readonly editRecording: (
+			id: string,
+			recording: Recording
+		) => Effect.Effect<void, EditRecordingError>;
+		readonly deleteRecording: (id: string) => Effect.Effect<void, DeleteRecordingError>;
+		readonly transcribeRecording: (
+			id: string
+		) => Effect.Effect<
+			void,
+			EditRecordingError | GetRecordingError | RecordingNotFound | TranscriptionError,
+			TranscriptionService
+		>;
+	}
+>() {}
+
+export const recordingsStateService = Effect.gen(function* (_) {
+	const recordingsDb = yield* _(RecordingsDbService);
+	const allRecordings = yield* _(recordingsDb.getAllRecordings);
+	const { subscribe, set, update } = writable<Recording[]>(allRecordings);
+	const editRecording = (id: string, recording: Recording) =>
+		Effect.gen(function* (_) {
+			yield* _(recordingsDb.editRecording(id, recording));
+			update((recordings) => {
+				const index = recordings.findIndex((recording) => recording.id === id);
+				if (index === -1) return recordings;
+				recordings[index] = recording;
+				return recordings;
 			});
-		return {
-			subscribe,
-			sync: Effect.gen(function* (_) {
-				const recordings = yield* _(recordingsDb.getAllRecordings);
-				set(recordings);
+		});
+	return {
+		subscribe,
+		sync: Effect.gen(function* (_) {
+			const recordings = yield* _(recordingsDb.getAllRecordings);
+			set(recordings);
+		}),
+		addRecording: (recording: Recording) =>
+			Effect.gen(function* (_) {
+				yield* _(recordingsDb.addRecording(recording));
+				update((recordings) => [...recordings, recording]);
+				console.log('🚀 ~ addRecording ~ recording:', recording);
 			}),
-			addRecording: (recording: Recording) =>
-				Effect.gen(function* (_) {
-					yield* _(recordingsDb.addRecording(recording));
-					update((recordings) => [...recordings, recording]);
-					console.log('🚀 ~ addRecording ~ recording:', recording);
-				}),
-			editRecording,
-			deleteRecording: (id: string) =>
-				Effect.gen(function* (_) {
-					yield* _(recordingsDb.deleteRecording(id));
-					update((recordings) => recordings.filter((recording) => recording.id !== id));
-				}),
-			transcribeRecording: (id: string) =>
-				Effect.gen(function* (_) {
-					const recording = yield* _(recordingsDb.getRecording(id));
-					if (!recording) return yield* _(new RecordingNotFound({ id }));
-					yield* _(editRecording(id, { ...recording, state: 'TRANSCRIBING' }));
-					const transcriptionService = yield* _(TranscriptionService);
-					const transcription = yield* _(transcriptionService.transcribe(recording.blob));
-					yield* _(editRecording(id, { ...recording, state: 'DONE' }));
-					yield* _(editRecording(id, { ...recording, transcription }));
-				})
-		};
-	});
+		editRecording,
+		deleteRecording: (id: string) =>
+			Effect.gen(function* (_) {
+				yield* _(recordingsDb.deleteRecording(id));
+				update((recordings) => recordings.filter((recording) => recording.id !== id));
+			}),
+		transcribeRecording: (id: string) =>
+			Effect.gen(function* (_) {
+				const recording = yield* _(recordingsDb.getRecording(id));
+				if (!recording) return yield* _(new RecordingNotFound({ id }));
+				yield* _(editRecording(id, { ...recording, state: 'TRANSCRIBING' }));
+				const transcriptionService = yield* _(TranscriptionService);
+				const transcription = yield* _(transcriptionService.transcribe(recording.blob));
+				yield* _(editRecording(id, { ...recording, state: 'DONE' }));
+				yield* _(editRecording(id, { ...recording, transcription }));
+			})
+	} satisfies Context.Tag.Service<RecordingsStateService>;
+});
