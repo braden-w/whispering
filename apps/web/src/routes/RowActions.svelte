9d0a6a6d1aca40b0689e0c7249fef9548b1ada56
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-22 17:39:48 -0800

    feat: Add Transcription Button to RowActions
    Former-commit-id: 76e03236087c8d4dc653771ea0bec4bc16f819d7
    Former-commit-id: 76513d79cc15cdf7fae4f221f13cd7ec729bb6cb

--- apps/web/src/routes/RowActions.svelte
+++ apps/web/src/routes/RowActions.svelte
@@ -2,10 +2,19 @@
 	import { recordings } from '$lib/stores/recordingState';
 	import { Button } from '@repo/ui/components/button';
 	import { Effect } from 'effect';
+	import TranscriptionIcon from '~icons/material-symbols/speech-to-text';
 	import TrashIcon from '~icons/heroicons/trash';
 
 	export let id: string;
 </script>
+
+<Button
+	variant="ghost"
+	size="icon"
+	on:click={() => recordings.transcribeRecording(id).pipe(Effect.runPromise).catch(console.error)}
+>
+	<TranscriptionIcon />
+</Button>
 
 <Button
 	variant="ghost"
