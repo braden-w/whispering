ecb0964a6a520640764dce1119ca266dde0c70a4
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-23 16:14:51 -0800

    feat: sliding to left on transition
    Former-commit-id: b475d27762260fc068c5f9d78c302a7ed7483206
    Former-commit-id: 5b04460330f3c02aec13cc94f92bd95597ea6b3a

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -71,3 +71,27 @@
 
 <Toaster />
 <ModeWatcher />
+
+<style>
+	@keyframes slide-to-left {
+		to {
+			transform: translateX(-30px);
+		}
+	}
+
+	@keyframes slide-from-right {
+		from {
+			transform: translateX(30px);
+		}
+	}
+
+	:root::view-transition-old(root) {
+		animation: 90ms cubic-bezier(0.4, 0, 1, 1) both fade-out,
+			300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-to-left;
+	}
+
+	:root::view-transition-new(root) {
+		animation: 210ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in,
+			300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-right;
+	}
+</style>
