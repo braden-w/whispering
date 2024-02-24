833619b93313b84cb978c81ad29c53eddabf1ced
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-23 18:01:07 -0800

    chore: more docs
    Former-commit-id: 38adac7b0e2db05cc8c2a59348693e52f96d27e4
    Former-commit-id: f77ba39a98439b25840b52c51a62bdccfd00f6c7

--- packages/recorder/src/services/clipboard/index.ts
+++ packages/recorder/src/services/clipboard/index.ts
@@ -9,12 +9,22 @@ export class ClipboardError extends Data.TaggedError('TranscribeError')<{
 export class ClipboardService extends Context.Tag('ClipboardService')<
 	ClipboardService,
 	{
+		/***
+		 * Reads text from the user's clipboard.
+		 * @returns The text from the clipboard.
+		 */
 		readonly getClipboard: Effect.Effect<string, ClipboardError>;
 		/**
 		 * Writes text to the user's clipboard.
 		 * @param text The text to write to the clipboard.
 		 */
 		readonly setClipboard: (text: string) => Effect.Effect<void, ClipboardError>;
+		/**
+		 * Pastes text from the user's clipboard.
+		 * - Web: No need to implement this function.
+		 * - Desktop: This function should trigger a paste action, as if the user had pressed `Ctrl` + `V`.
+		 * - Mobile: This function should trigger a paste action, as if the user had pressed `Paste` in the context menu.
+		 */
 		readonly pasteTextFromClipboard: Effect.Effect<void, ClipboardError>;
 	}
 >() {}
