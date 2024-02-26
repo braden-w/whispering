bfe31ec873c03b5541d18e6d2b88081768c2b5de
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-26 13:25:42 -0800

    feat: layout routes
    Former-commit-id: 9071a9365a5f2694076e384744b848f96fe93894
    Former-commit-id: 76af482b0cdd2cf1c1a699a824e66c27b8cb82ed

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -15,21 +15,7 @@
 </svelte:head>
 
 <div class="relative flex min-h-screen flex-col">
-	<header
-		class="border-border/40 bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 border-b shadow-sm backdrop-blur"
-		style="view-transition-name: header"
-	>
-		<div class="container flex h-14 w-full max-w-screen-2xl items-center justify-between">
-			<a class="flex items-center gap-2" href="/">
-				<span aria-hidden="true" role="img"> 🎙️ </span>
-				<span class="text-lg font-bold">whispering</span>
-			</a>
-			<NavItems />
-		</div>
-	</header>
-	<main class="flex flex-1 justify-center p-4">
-		<slot />
-	</main>
+	<slot />
 </div>
 
 <Toaster />
