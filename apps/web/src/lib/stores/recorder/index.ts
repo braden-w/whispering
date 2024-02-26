d59474a8446f42c165c8bfc3d806687f1abd6c67
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-25 21:14:48 -0800

    feat: recorder with services
    Former-commit-id: 9d472b42c2125bf05c0b4ec80dc29f9ae89cf654
    Former-commit-id: 7a6cc4d6ae479d1e8675ad484978ada4512ca3f5

--- apps/web/src/lib/stores/recorder/index.ts
+++ apps/web/src/lib/stores/recorder/index.ts
@@ -1,3 +1,9 @@
+import { webRecorderService } from '@repo/recorder/implementations/recorder/web';
+import { RecorderService } from '@repo/recorder/services/recorder';
+import { Effect } from 'effect';
 import { createRecorder } from './create-recorder';
 
-export const recorder = createRecorder({});
+export const recorder = createRecorder({}).pipe(
+	Effect.provideService(RecorderService, webRecorderService),
+	Effect.runSync
+);
