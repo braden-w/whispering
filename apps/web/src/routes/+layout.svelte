1ac54d4df9ea3c8d8b176a070270aa2082199f5e
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-26 13:22:31 -0800

    chore: remove unused style
    Former-commit-id: 9f4132a75aa3253ca4d78747aa927af9ba1d35c1
    Former-commit-id: dc425b7d410b98f910e9d03aaec377a91bb66241

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -34,39 +34,3 @@
 
 <Toaster />
 <ModeWatcher />
-
-<style>
-	:global(::view-transition-new(root)) {
-		animation-duration: 150ms;
-	}
-
-	/* @keyframes slide-to-left {
-		to {
-			transform: translateX(-100%);
-		}
-	}
-
-	@keyframes slide-from-right {
-		from {
-			transform: translateX(100%);
-		}
-	}
-
-	:root::view-transition-old(root) {
-		animation: 90ms cubic-bezier(0.4, 0, 1, 1) both fade-out,
-			300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-to-left;
-	}
-
-	:root::view-transition-new(root) {
-		animation: 210ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in,
-			300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-right;
-	}
-
-	:root::view-transition-old(header) {
-		animation: none;
-	}
-
-	:root::view-transition-new(header) {
-		animation: none;
-	} */
-</style>
