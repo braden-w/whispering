3dc9f274782ab43c97e8ac2c58b2f5fae64c446d
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-26 13:19:36 -0800

    refactor: navItems
    Former-commit-id: 2dcf0b9bc43cfa33fd9296397266f31205c88cb2
    Former-commit-id: 458a6f98a7e3f2b8f332413155747d2e246ab9a7

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
-	import GithubIcon from '~icons/mdi/github';
-	import AdjustmentsHorizontalIcon from '~icons/heroicons/adjustments-horizontal';
 	import '@repo/ui/app.pcss';
-	import { Button } from '@repo/ui/components/button';
-	import { ModeWatcher, Toaster, toggleMode } from '@repo/ui/components/sonner';
-	import MoonIcon from '~icons/lucide/moon';
-	import SunIcon from '~icons/lucide/sun';
-	import { onNavigate } from '$app/navigation';
+	import { ModeWatcher, Toaster } from '@repo/ui/components/sonner';
 	import { setupViewTransition } from 'sveltekit-view-transition';
+	import NavItems from './NavItems.svelte';
 
 	setupViewTransition();
 
@@ -29,32 +24,7 @@
 				<span aria-hidden="true" role="img"> 🎙️ </span>
 				<span class="text-lg font-bold">whispering</span>
 			</a>
-			<nav class="flex items-center">
-				<Button href="/settings" aria-label="Settings" variant="ghost" size="icon">
-					<AdjustmentsHorizontalIcon />
-					<span class="sr-only">Settings</span>
-				</Button>
-				<Button
-					href="https://github.com/braden-w/whispering"
-					target="_blank"
-					rel="noopener noreferrer"
-					title="View project on GitHub"
-					aria-label="View project on GitHub"
-					size="icon"
-					variant="ghost"
-				>
-					<GithubIcon class="h-4 w-4" />
-				</Button>
-				<Button on:click={toggleMode} size="icon" variant="ghost">
-					<SunIcon
-						class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-					/>
-					<MoonIcon
-						class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-					/>
-					<span class="sr-only">Toggle dark mode</span>
-				</Button>
-			</nav>
+			<NavItems />
 		</div>
 	</header>
 	<main class="flex flex-1 justify-center p-4">
