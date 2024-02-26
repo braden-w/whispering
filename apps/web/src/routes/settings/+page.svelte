f8535032e7713be20910b9e82065b269e6725a85
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-25 22:28:05 -0800

    feat: select deviceId!
    Former-commit-id: 6859e78fbd918909ff4e68c82df1815235fb8379
    Former-commit-id: 633298f82d8bb304bfb24025e17438a0ad148fd8

--- apps/web/src/routes/settings/+page.svelte
+++ apps/web/src/routes/settings/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+	import { selectedAudioInputDeviceId, recorder } from '$lib/stores/recorder';
 	import { settings } from '$lib/stores/settings';
 	import { Button } from '@repo/ui/components/button';
 	import * as Card from '@repo/ui/components/card';
 	import { Checkbox } from '@repo/ui/components/checkbox';
 	import { Label } from '@repo/ui/components/label';
+	import * as Select from '@repo/ui/components/select';
+
+	const mediaDevicesPromise = recorder.getAudioInputDevices();
 </script>
 
 <div class="container flex min-h-screen items-center justify-center">
@@ -35,6 +39,25 @@
 				>
 					Paste contents from clipboard after successful transcription (experimental)
 				</Label>
+			</div>
+			<div class="flex gap-2">
+				<Label for="device" class="text-sm font-medium leading-none">Recording Device</Label>
+				<Select.Root selected={selectedAudioInputDeviceId}>
+					<Select.Trigger class="w-[180px]">
+						<Select.Value placeholder="Device" />
+					</Select.Trigger>
+					<Select.Content>
+						{#await mediaDevicesPromise}
+							<p>Loading...</p>
+						{:then mediaDevices}
+							{#each mediaDevices as device}
+								<Select.Item value={device.deviceId}>{device.label}</Select.Item>
+							{/each}
+						{:catch error}
+							<p>Error: {error.message}</p>
+						{/await}
+					</Select.Content>
+				</Select.Root>
 			</div>
 			<Button href="/" variant="link">Go back</Button>
 			<Button href="/key" variant="link">Edit key</Button>
