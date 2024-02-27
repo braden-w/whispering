dab66f98f6659bec60aecd0c5cb1dbcd9f7b6f16
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-26 21:04:51 -0800

    feat: wrap navs in tooltips
    Former-commit-id: ad6b2752381e78f3719fc213904db91acdf15a74
    Former-commit-id: d8bc1c620cc732a915252a925ea996c3f5c2a49b

--- apps/web/src/routes/NavItems.svelte
+++ apps/web/src/routes/NavItems.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '@repo/ui/app.pcss';
 	import { Button } from '@repo/ui/components/button';
+	import * as Tooltip from '@repo/ui/components/tooltip';
 	import { toggleMode } from '@repo/ui/components/sonner';
 	import AdjustmentsHorizontalIcon from '~icons/heroicons/adjustments-horizontal';
 	import ListIcon from '~icons/lucide/list';
@@ -10,38 +11,78 @@
 </script>
 
 <nav class="flex items-center">
-	<Button
-		href="/recordings"
-		variant="ghost"
-		size="icon"
-		style="view-transition-name: nav-recordings"
-	>
-		<ListIcon aria-hidden="true" />
-		<span class="sr-only">Settings</span>
-	</Button>
-	<Button href="/settings" variant="ghost" size="icon" style="view-transition-name: nav-settings">
-		<AdjustmentsHorizontalIcon aria-hidden="true" />
-		<span class="sr-only">Settings</span>
-	</Button>
-	<Button
-		href="https://github.com/braden-w/whispering"
-		target="_blank"
-		rel="noopener noreferrer"
-		title="View project on GitHub"
-		variant="ghost"
-		size="icon"
-		style="view-transition-name: nav-github"
-	>
-		<GithubIcon class="h-4 w-4" aria-hidden="true" />
-		<span class="sr-only">View project on GitHub</span>
-	</Button>
-	<Button on:click={toggleMode} variant="ghost" size="icon" style="view-transition-name: nav-mode">
-		<SunIcon
-			class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-		/>
-		<MoonIcon
-			class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-		/>
-		<span class="sr-only">Toggle dark mode</span>
-	</Button>
+	<Tooltip.Root>
+		<Tooltip.Trigger asChild let:builder>
+			<Button
+				href="/recordings"
+				variant="ghost"
+				size="icon"
+				style="view-transition-name: nav-recordings"
+			>
+				<ListIcon aria-hidden="true" />
+				<span class="sr-only">Settings</span>
+			</Button>
+		</Tooltip.Trigger>
+		<Tooltip.Content>
+			<p>Recordings</p>
+		</Tooltip.Content>
+	</Tooltip.Root>
+	<Tooltip.Root>
+		<Tooltip.Trigger asChild let:builder>
+			<Button
+				builders={[builder]}
+				href="/settings"
+				variant="ghost"
+				size="icon"
+				style="view-transition-name: nav-settings"
+			>
+				<AdjustmentsHorizontalIcon aria-hidden="true" />
+				<span class="sr-only">Settings</span>
+			</Button>
+		</Tooltip.Trigger>
+		<Tooltip.Content>
+			<p>Settings</p>
+		</Tooltip.Content>
+	</Tooltip.Root>
+	<Tooltip.Root>
+		<Tooltip.Trigger asChild let:builder>
+			<Button
+				builders={[builder]}
+				href="https://github.com/braden-w/whispering"
+				target="_blank"
+				rel="noopener noreferrer"
+				variant="ghost"
+				size="icon"
+				style="view-transition-name: nav-github"
+			>
+				<GithubIcon class="h-4 w-4" aria-hidden="true" />
+				<span class="sr-only">View project on GitHub</span>
+			</Button>
+		</Tooltip.Trigger>
+		<Tooltip.Content>
+			<p>View project on GitHub</p>
+		</Tooltip.Content>
+	</Tooltip.Root>
+	<Tooltip.Root>
+		<Tooltip.Trigger asChild let:builder>
+			<Button
+				builders={[builder]}
+				on:click={toggleMode}
+				variant="ghost"
+				size="icon"
+				style="view-transition-name: nav-mode"
+			>
+				<SunIcon
+					class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+				/>
+				<MoonIcon
+					class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+				/>
+				<span class="sr-only">Toggle dark mode</span>
+			</Button>
+		</Tooltip.Trigger>
+		<Tooltip.Content>
+			<p>Toggle dark mode</p>
+		</Tooltip.Content>
+	</Tooltip.Root>
 </nav>
