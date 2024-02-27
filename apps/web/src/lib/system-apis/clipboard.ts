ce95772c273a1a451d4f36e8fc5f8f4ce8f8a20b
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-26 16:40:07 -0800

    fix: imports in sytem-apis
    Former-commit-id: 696d2464d17da6aa9654868d114746d3d967610d
    Former-commit-id: 7018c4330a0d28dd0ff5e591bc8d014317f5f6e8

--- apps/web/src/lib/system-apis/clipboard.ts
+++ apps/web/src/lib/system-apis/clipboard.ts
@@ -1,6 +1,6 @@
-import { Effect } from 'effect';
+import { clipboardService } from '@repo/recorder/implementations/clipboard/web.ts';
 import { ClipboardService } from '@repo/recorder/services/clipboard';
-import { clipboardService } from '@repo/recorder/implementations/clipboard/web';
+import { Effect } from 'effect';
 
 export const clipboard = ClipboardService.pipe(
 	Effect.provideService(ClipboardService, clipboardService),
