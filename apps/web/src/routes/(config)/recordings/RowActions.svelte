76f4756577f2be83b2bb28eeb7fdc6904bff5b95
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-27 00:07:52 -0800

    fix: row and such
    Former-commit-id: affb67740a39a1e3ff73625f8808f435908a7041
    Former-commit-id: 93e07ba1c3a847bd9e59702ef3c682830a0cfa10

--- apps/web/src/routes/(config)/recordings/RowActions.svelte
+++ apps/web/src/routes/(config)/recordings/RowActions.svelte
@@ -10,50 +10,50 @@
 	export let id: string;
 </script>
 
-<Tooltip.Root>
-	<Tooltip.Trigger asChild let:builder>
-		<Button
-			builders={[builder]}
-			variant="ghost"
-			size="icon"
-			on:click={() => recordings.transcribeRecording(id).pipe(Effect.runPromise)}
-		>
-			<TranscriptionIcon />
-		</Button>
-	</Tooltip.Trigger>
-	<Tooltip.Content>
-		<p>Transcribe Recording</p>
-	</Tooltip.Content>
-</Tooltip.Root>
-
-<Tooltip.Root>
-	<Tooltip.Trigger asChild let:builder>
-		<Button
-			builders={[builder]}
-			variant="ghost"
-			size="icon"
-			on:click={() => recordings.deleteRecording(id).pipe(Effect.runPromise)}
-		>
-			<EditIcon />
-		</Button>
-	</Tooltip.Trigger>
-	<Tooltip.Content>
-		<p>Edit Recording</p>
-	</Tooltip.Content>
-</Tooltip.Root>
-
-<Tooltip.Root>
-	<Tooltip.Trigger asChild let:builder>
-		<Button
-			builders={[builder]}
-			variant="ghost"
-			size="icon"
-			on:click={() => recordings.deleteRecording(id).pipe(Effect.runPromise)}
-		>
-			<TrashIcon />
-		</Button>
-	</Tooltip.Trigger>
-	<Tooltip.Content>
-		<p>Delete Recording</p>
-	</Tooltip.Content>
-</Tooltip.Root>
+<div class="flex items-center">
+	<Tooltip.Root>
+		<Tooltip.Trigger asChild let:builder>
+			<Button
+				builders={[builder]}
+				variant="ghost"
+				size="icon"
+				on:click={() => recordings.transcribeRecording(id).pipe(Effect.runPromise)}
+			>
+				<TranscriptionIcon />
+			</Button>
+		</Tooltip.Trigger>
+		<Tooltip.Content>
+			<p>Transcribe Recording</p>
+		</Tooltip.Content>
+	</Tooltip.Root>
+	<Tooltip.Root>
+		<Tooltip.Trigger asChild let:builder>
+			<Button
+				builders={[builder]}
+				variant="ghost"
+				size="icon"
+				on:click={() => recordings.deleteRecording(id).pipe(Effect.runPromise)}
+			>
+				<EditIcon />
+			</Button>
+		</Tooltip.Trigger>
+		<Tooltip.Content>
+			<p>Edit Recording</p>
+		</Tooltip.Content>
+	</Tooltip.Root>
+	<Tooltip.Root>
+		<Tooltip.Trigger asChild let:builder>
+			<Button
+				builders={[builder]}
+				variant="ghost"
+				size="icon"
+				on:click={() => recordings.deleteRecording(id).pipe(Effect.runPromise)}
+			>
+				<TrashIcon />
+			</Button>
+		</Tooltip.Trigger>
+		<Tooltip.Content>
+			<p>Delete Recording</p>
+		</Tooltip.Content>
+	</Tooltip.Root>
+</div>
