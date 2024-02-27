ae89f0f4666ee82b745de6362625b5150ce571b3
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-27 01:15:19 -0800

    fix: Edit Recording popup
    Former-commit-id: 4ef33c013ad7cc2ab05793e343c1543007ddb9a5
    Former-commit-id: 126c64132d125906aa354272f5cae5be6b12d8d0

--- apps/web/src/routes/(config)/recordings/RowActions.svelte
+++ apps/web/src/routes/(config)/recordings/RowActions.svelte
@@ -4,6 +4,9 @@
 	import { copyRecordingText } from '$lib/system-apis/clipboard';
 	import type { Recording } from '@repo/recorder/services/recordings-db';
 	import { Button } from '@repo/ui/components/button';
+	import * as Dialog from '@repo/ui/components/dialog';
+	import { Input } from '@repo/ui/components/input';
+	import { Label } from '@repo/ui/components/label';
 	import * as Tooltip from '@repo/ui/components/tooltip';
 	import { Effect } from 'effect';
 	import ClipboardIcon from '~icons/heroicons/clipboard';
@@ -30,21 +33,41 @@
 			<p>Transcribe Recording</p>
 		</Tooltip.Content>
 	</Tooltip.Root>
-	<Tooltip.Root>
-		<Tooltip.Trigger asChild let:builder>
-			<Button
-				builders={[builder]}
-				variant="ghost"
-				size="icon"
-				on:click={() => recordings.deleteRecording(recording.id).pipe(Effect.runPromise)}
-			>
-				<EditIcon />
-			</Button>
-		</Tooltip.Trigger>
-		<Tooltip.Content>
-			<p>Edit Recording</p>
-		</Tooltip.Content>
-	</Tooltip.Root>
+	<Dialog.Root>
+		<Dialog.Trigger>
+			<Tooltip.Root>
+				<Tooltip.Trigger asChild let:builder>
+					<Button builders={[builder]} variant="ghost" size="icon">
+						<EditIcon />
+					</Button>
+				</Tooltip.Trigger>
+				<Tooltip.Content>
+					<p>Edit Recording</p>
+				</Tooltip.Content>
+			</Tooltip.Root>
+		</Dialog.Trigger>
+		<Dialog.Content class="sm:max-w-[425px]">
+			<Dialog.Header>
+				<Dialog.Title>Edit recording</Dialog.Title>
+				<Dialog.Description>
+					Make changes to your recording here. Click save when you're done.
+				</Dialog.Description>
+			</Dialog.Header>
+			<div class="grid gap-4 py-4">
+				<div class="grid grid-cols-4 items-center gap-4">
+					<Label class="text-right">Name</Label>
+					<Input id="name" value="Pedro Duarte" class="col-span-3" />
+				</div>
+				<div class="grid grid-cols-4 items-center gap-4">
+					<Label class="text-right">Username</Label>
+					<Input id="username" value="@peduarte" class="col-span-3" />
+				</div>
+			</div>
+			<Dialog.Footer>
+				<Button type="submit">Save changes</Button>
+			</Dialog.Footer>
+		</Dialog.Content>
+	</Dialog.Root>
 	<Tooltip.Root>
 		<Tooltip.Trigger asChild let:builder>
 			<Button
