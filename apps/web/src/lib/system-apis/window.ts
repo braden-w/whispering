2ff4bba64ea472e534b2834f157afe852d56bea7
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-26 16:41:21 -0800

    chore: remove setAlwaysOnTop
    Former-commit-id: da9e20bd6b7cf3e34dcc7988c2007365bc3157e1
    Former-commit-id: 43d4a670bb5358b7d659d6cdb5979919e2603b1d

--- apps/web/src/lib/system-apis/window.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Sets whether the window is always on top of other windows.
- * @param alwaysOnTop Whether the window should be always on top of other windows.
- */
-export async function setAlwaysOnTop(alwaysOnTop: boolean): Promise<void> {
-	return;
-}
