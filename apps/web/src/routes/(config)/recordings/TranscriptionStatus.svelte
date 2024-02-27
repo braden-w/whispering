01f9b61cfbe0c2f09ff5e6a1a9e56e7dee8c061d
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-27 01:45:43 -0800

    fix: ToolTip Trigger and such
    Former-commit-id: 4256c83ed962a3c7f404561843c0ff9c364fb9d0
    Former-commit-id: fe2218c7757dd4d7b4aacdfba79f3a336c9d450a

---
+++ apps/web/src/routes/(config)/recordings/TranscriptionStatus.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { Recording } from '@repo/recorder/services/recordings-db';
+	import * as Tooltip from '@repo/ui/components/tooltip';
+	import RepeatIcon from '~icons/lucide/repeat';
+	import PlayIcon from '~icons/heroicons/play';
+	import ProcessedIcon from '~icons/heroicons/check-circle';
+
+	export let state: Recording['state'];
+</script>
+
+<span>
+	{#if state === 'UNPROCESSED'}
+		<Tooltip.Root>
+			<Tooltip.Trigger>️<PlayIcon /></Tooltip.Trigger>
+			<Tooltip.Content>
+				<p>Recording has not been processed. Press the <RepeatIcon /> icon to transcribe it.</p>
+			</Tooltip.Content>
+		</Tooltip.Root>
+	{:else if state === 'TRANSCRIBING'}
+		<Tooltip.Root>
+			<Tooltip.Trigger><RepeatIcon /></Tooltip.Trigger>
+			<Tooltip.Content>
+				<p>Transcribing</p>
+			</Tooltip.Content>
+		</Tooltip.Root>
+	{:else if state === 'DONE'}
+		<Tooltip.Root>
+			<Tooltip.Trigger>
+				<ProcessedIcon />
+			</Tooltip.Trigger>
+			<Tooltip.Content>Recording is done transcribing</Tooltip.Content>
+		</Tooltip.Root>
+	{/if}
+</span>
