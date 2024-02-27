2273c00045b535e6d7814cda5a5fbb1e54c5327a
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-26 22:43:14 -0800

    fix: import
    Former-commit-id: 3837b1834013546106ea3834360868731a56b198
    Former-commit-id: e3587616734419da8b7e925251fdb4bf8a51993e

--- apps/web/src/lib/stores/recorder/index.ts
+++ apps/web/src/lib/stores/recorder/index.ts
@@ -1,4 +1,4 @@
-import { webRecorderService } from '@repo/recorder/implementations/recorder/web';
+import { webRecorderService } from '@repo/recorder/implementations/recorder/web.ts';
 import { RecorderService } from '@repo/recorder/services/recorder';
 import { Effect } from 'effect';
 import { createRecorder } from './create-recorder';
