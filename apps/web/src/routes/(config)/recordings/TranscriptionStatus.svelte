09e724d2710a56e7e066ae7d46777898fdd1c1ab
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-27 01:46:10 -0800

    chore: remove span
    Former-commit-id: 065adf529e866d3175cf76b0e32830a8a95a867b
    Former-commit-id: 498e8f457618391653cb1b59027bd68d98efebb9

--- apps/web/src/routes/(config)/recordings/TranscriptionStatus.svelte
+++ apps/web/src/routes/(config)/recordings/TranscriptionStatus.svelte
@@ -8,27 +8,25 @@
 	export let state: Recording['state'];
 </script>
 
-<span>
-	{#if state === 'UNPROCESSED'}
-		<Tooltip.Root>
-			<Tooltip.Trigger>️<PlayIcon /></Tooltip.Trigger>
-			<Tooltip.Content>
-				<p>Recording has not been processed. Press the <RepeatIcon /> icon to transcribe it.</p>
-			</Tooltip.Content>
-		</Tooltip.Root>
-	{:else if state === 'TRANSCRIBING'}
-		<Tooltip.Root>
-			<Tooltip.Trigger><RepeatIcon /></Tooltip.Trigger>
-			<Tooltip.Content>
-				<p>Transcribing</p>
-			</Tooltip.Content>
-		</Tooltip.Root>
-	{:else if state === 'DONE'}
-		<Tooltip.Root>
-			<Tooltip.Trigger>
-				<ProcessedIcon />
-			</Tooltip.Trigger>
-			<Tooltip.Content>Recording is done transcribing</Tooltip.Content>
-		</Tooltip.Root>
-	{/if}
-</span>
+{#if state === 'UNPROCESSED'}
+	<Tooltip.Root>
+		<Tooltip.Trigger>️<PlayIcon /></Tooltip.Trigger>
+		<Tooltip.Content>
+			<p>Recording has not been processed. Press the <RepeatIcon /> icon to transcribe it.</p>
+		</Tooltip.Content>
+	</Tooltip.Root>
+{:else if state === 'TRANSCRIBING'}
+	<Tooltip.Root>
+		<Tooltip.Trigger><RepeatIcon /></Tooltip.Trigger>
+		<Tooltip.Content>
+			<p>Transcribing</p>
+		</Tooltip.Content>
+	</Tooltip.Root>
+{:else if state === 'DONE'}
+	<Tooltip.Root>
+		<Tooltip.Trigger>
+			<ProcessedIcon />
+		</Tooltip.Trigger>
+		<Tooltip.Content>Recording is done transcribing</Tooltip.Content>
+	</Tooltip.Root>
+{/if}
