4197424d51c3e9f926762379094948e5e8deb85d
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-28 15:55:24 -0800

    fix: space-y-4
    Former-commit-id: 670585ea75ebe6557d9d4d0caaaa71425542a892
    Former-commit-id: 1398ee30401618d7518959b890275db7e41f36d4

--- apps/web/src/routes/(config)/recordings/+page.svelte
+++ apps/web/src/routes/(config)/recordings/+page.svelte
@@ -154,7 +154,7 @@
 <div class="container flex flex-col gap-2">
 	<h1 class="scroll-m=20 text-4xl font-bold tracking-tight lg:text-5xl">Recordings</h1>
 	<p class="text-muted-foreground">Your latest recordings</p>
-	<div class="rounded-md border p-6">
+	<div class="space-y-4 rounded-md border p-6">
 		<div class="flex items-center gap-2">
 			<Input
 				class="max-w-sm"
