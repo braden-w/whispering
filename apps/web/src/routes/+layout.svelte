b7050ec3c9c4b9bc2426e773efbda5a42a79f5a9
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-28 16:58:42 -0800

    chore: use my own view transitions impl
    Former-commit-id: 5825b9f571cacd70e6588b2a0c0bd9458d73b3dd
    Former-commit-id: c6dba9d70f63187c7c36f4f0879909c4ea434e41

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -2,9 +2,18 @@
 	import '@repo/ui/app.pcss';
 	import { ModeWatcher } from 'mode-watcher';
 	import { Toaster } from 'svelte-french-toast';
-	import { setupViewTransition } from 'sveltekit-view-transition';
+	import { onNavigate } from '$app/navigation';
 
-	setupViewTransition();
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <svelte:head>
