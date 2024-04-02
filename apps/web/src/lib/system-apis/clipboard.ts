ef9d36e1263f8cd1b270406d482c7c7d71dcf24a
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-04-02 15:04:40 -0400

    refactor: clipboardService and such
    Former-commit-id: 997ad4321ddd1a536d982c0fcea0abf4cf310cc4
    Former-commit-id: c1c6f1cef4be0733ad1eda777bd31ea733a989ba

--- apps/web/src/lib/system-apis/clipboard.ts
+++ apps/web/src/lib/system-apis/clipboard.ts
@@ -1,17 +1,13 @@
-import { clipboardService } from '@repo/recorder/implementations/clipboard/web.ts';
+import { ClipboardServiceLive } from '@repo/recorder/implementations/clipboard/web.js';
 import { ClipboardService } from '@repo/recorder/services/clipboard';
 import type { Recording } from '@repo/recorder/services/recordings-db';
-import { toast } from 'svelte-french-toast';
 import { Effect } from 'effect';
-
-export const clipboard = ClipboardService.pipe(
-	Effect.provideService(ClipboardService, clipboardService),
-	Effect.runSync
-);
+import { toast } from 'svelte-french-toast';
 
 export const copyRecordingText = (recording: Recording) =>
 	Effect.gen(function* (_) {
 		if (!recording.transcribedText) return;
-		yield* _(clipboard.setClipboardText(recording.transcribedText));
+		const clipboardService = yield* _(ClipboardService);
+		yield* _(clipboardService.setClipboardText(recording.transcribedText));
 		toast.success('Copied to clipboard!');
-	}).pipe(Effect.runPromise);
+	}).pipe(Effect.provide(ClipboardServiceLive), Effect.runPromise);
--- packages/recorder/src/implementations/clipboard/web.ts
+++ packages/recorder/src/implementations/clipboard/web.ts
@@ -1,25 +1,26 @@
-import type { Context } from 'effect';
-import { Effect } from 'effect';
-import type { ClipboardService } from '../../services/clipboard';
-import { ClipboardError } from '../../services/clipboard';
+import { Effect, Layer } from 'effect';
+import { ClipboardError, ClipboardService } from '../../services/clipboard';
 
-export const clipboardService: Context.Tag.Service<ClipboardService> = {
-	getClipboardText: Effect.tryPromise({
-		try: () => navigator.clipboard.readText(),
-		catch: (error) =>
-			new ClipboardError({ message: 'Failed to read from clipboard', origError: error })
-	}),
-	setClipboardText: (text) =>
-		Effect.tryPromise({
-			try: () => navigator.clipboard.writeText(text),
+export const ClipboardServiceLive = Layer.succeed(
+	ClipboardService,
+	ClipboardService.of({
+		getClipboardText: Effect.tryPromise({
+			try: () => navigator.clipboard.readText(),
 			catch: (error) =>
-				new ClipboardError({ message: 'Failed to write to clipboard', origError: error })
+				new ClipboardError({ message: 'Failed to read from clipboard', origError: error })
 		}),
-	pasteTextFromClipboard: Effect.try({
-		try: () => {
-			return;
-		},
-		catch: (error) =>
-			new ClipboardError({ message: 'Failed to paste from clipboard', origError: error })
+		setClipboardText: (text) =>
+			Effect.tryPromise({
+				try: () => navigator.clipboard.writeText(text),
+				catch: (error) =>
+					new ClipboardError({ message: 'Failed to write to clipboard', origError: error })
+			}),
+		pasteTextFromClipboard: Effect.try({
+			try: () => {
+				return;
+			},
+			catch: (error) =>
+				new ClipboardError({ message: 'Failed to paste from clipboard', origError: error })
+		})
 	})
-};
+);
