cbc34b828269dc6aceba909b6e78f456f6b9a452
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-04-02 15:56:16 -0400

    refactor: indexed-db implementation
    Former-commit-id: dc26143fe5ccb60364588f46af9cc81f31881f3e
    Former-commit-id: 470dd3d46a3a5d2f61a14a6f332991c0ca3d5c69

--- apps/web/src/lib/stores/recordings/index.ts
+++ apps/web/src/lib/stores/recordings/index.ts
@@ -1,12 +1,11 @@
-import { indexedDbService } from '@repo/recorder/implementations/recordings-db/indexed-db.ts';
+import { RecordingsDbServiceLiveIndexedDb } from '@repo/recorder/implementations/recordings-db/indexed-db.ts';
 import { whisperTranscriptionService } from '@repo/recorder/implementations/transcription/whisper.ts';
-import { RecordingsDbService } from '@repo/recorder/services/recordings-db';
 import { TranscriptionService } from '@repo/recorder/services/transcription';
 import { Effect } from 'effect';
 import { createRecordings } from './create-recordings';
 
 export const recordings = createRecordings.pipe(
-	Effect.provideService(RecordingsDbService, indexedDbService),
+	Effect.provide(RecordingsDbServiceLiveIndexedDb),
 	Effect.provideService(TranscriptionService, whisperTranscriptionService),
 	Effect.runSync
 );
