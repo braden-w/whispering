acec53f53a30020153be56efa8e74b30964277a4
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-04-02 15:57:43 -0400

    refactor TranscriptionServiceLiveWhisper
    Former-commit-id: a4b4549011c664dde1daba21acb39dbe858f4992
    Former-commit-id: 18e864baa54b5e1d63b20dfb2f7a5fc7ed5a86c2

--- apps/web/src/lib/stores/recordings/index.ts
+++ apps/web/src/lib/stores/recordings/index.ts
@@ -1,12 +1,11 @@
 import { RecordingsDbServiceLiveIndexedDb } from '@repo/recorder/implementations/recordings-db/indexed-db.ts';
-import { whisperTranscriptionService } from '@repo/recorder/implementations/transcription/whisper.ts';
-import { TranscriptionService } from '@repo/recorder/services/transcription';
+import { TranscriptionServiceLiveWhisper } from '@repo/recorder/implementations/transcription/whisper.ts';
 import { Effect } from 'effect';
 import { createRecordings } from './create-recordings';
 
 export const recordings = createRecordings.pipe(
 	Effect.provide(RecordingsDbServiceLiveIndexedDb),
-	Effect.provideService(TranscriptionService, whisperTranscriptionService),
+	Effect.provide(TranscriptionServiceLiveWhisper),
 	Effect.runSync
 );
 
--- packages/recorder/src/implementations/transcription/whisper.ts
+++ packages/recorder/src/implementations/transcription/whisper.ts
@@ -1,10 +1,9 @@
-import type { Context } from 'effect';
-import { Effect } from 'effect';
+import { Effect, Layer } from 'effect';
 import {
 	InvalidApiKeyError,
 	PleaseEnterApiKeyError,
 	TranscriptionError,
-	type TranscriptionService
+	TranscriptionService
 } from '../../services/transcription';
 
 class WhisperFileTooLarge extends TranscriptionError {
@@ -46,47 +45,50 @@ function isString(input: unknown): input is string {
 
 const MAX_FILE_SIZE_MB = 25 as const;
 
-export const whisperTranscriptionService: Context.Tag.Service<TranscriptionService> = {
-	transcribe: (audioBlob, { apiKey }: { apiKey: string }) =>
-		Effect.gen(function* (_) {
-			if (!apiKey) {
-				return yield* _(new PleaseEnterApiKeyError());
-			}
-			if (!apiKey.startsWith('sk-')) {
-				return yield* _(new InvalidApiKeyError());
-			}
-			const blobSizeInMb = audioBlob.size / (1024 * 1024);
-			if (blobSizeInMb > MAX_FILE_SIZE_MB) {
-				return yield* _(new WhisperFileTooLarge(blobSizeInMb, MAX_FILE_SIZE_MB));
-			}
-			const fileName = 'recording.wav';
-			const wavFile = new File([audioBlob], fileName);
-			const formData = new FormData();
-			formData.append('file', wavFile);
-			formData.append('model', 'whisper-1');
-			const data = yield* _(
-				Effect.tryPromise({
-					try: () =>
-						fetch('https://api.openai.com/v1/audio/transcriptions', {
-							method: 'POST',
-							headers: { Authorization: `Bearer ${apiKey}` },
-							body: formData
-						}).then((res) => res.json()),
-					catch: (error) => new WhisperFetchError({ fetchError: error })
-				})
-			);
-			if (data?.error?.message) {
-				return yield* _(
-					new WhisperServerError({
-						message: data.error.message,
-						code: data.error.code,
-						type: data.error.type
+export const TranscriptionServiceLiveWhisper = Layer.succeed(
+	TranscriptionService,
+	TranscriptionService.of({
+		transcribe: (audioBlob, { apiKey }: { apiKey: string }) =>
+			Effect.gen(function* (_) {
+				if (!apiKey) {
+					return yield* _(new PleaseEnterApiKeyError());
+				}
+				if (!apiKey.startsWith('sk-')) {
+					return yield* _(new InvalidApiKeyError());
+				}
+				const blobSizeInMb = audioBlob.size / (1024 * 1024);
+				if (blobSizeInMb > MAX_FILE_SIZE_MB) {
+					return yield* _(new WhisperFileTooLarge(blobSizeInMb, MAX_FILE_SIZE_MB));
+				}
+				const fileName = 'recording.wav';
+				const wavFile = new File([audioBlob], fileName);
+				const formData = new FormData();
+				formData.append('file', wavFile);
+				formData.append('model', 'whisper-1');
+				const data = yield* _(
+					Effect.tryPromise({
+						try: () =>
+							fetch('https://api.openai.com/v1/audio/transcriptions', {
+								method: 'POST',
+								headers: { Authorization: `Bearer ${apiKey}` },
+								body: formData
+							}).then((res) => res.json()),
+						catch: (error) => new WhisperFetchError({ fetchError: error })
 					})
 				);
-			}
-			if (!isString(data.text)) {
-				return yield* _(new TranscriptionIsNotStringError());
-			}
-			return data.text;
-		})
-};
+				if (data?.error?.message) {
+					return yield* _(
+						new WhisperServerError({
+							message: data.error.message,
+							code: data.error.code,
+							type: data.error.type
+						})
+					);
+				}
+				if (!isString(data.text)) {
+					return yield* _(new TranscriptionIsNotStringError());
+				}
+				return data.text;
+			})
+	})
+);
