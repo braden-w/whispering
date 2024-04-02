cfa708ff95c1bb0ea651b67afe015290ed544a8a
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-04-02 16:02:11 -0400

    refactor: RecorderServiceLiveWeb
    Former-commit-id: 9e083c8b67f1e82aeda559b1ab4b723ca030b673
    Former-commit-id: 25e543adfed7b67e7053ba0defca335a58f815cf

--- packages/recorder/src/implementations/recorder/web.ts
+++ packages/recorder/src/implementations/recorder/web.ts
@@ -1,7 +1,6 @@
 import AudioRecorder from 'audio-recorder-polyfill';
-import type { Context } from 'effect';
-import { Effect } from 'effect';
-import { RecorderError, type RecorderService } from '../../services/recorder';
+import { Effect, Layer } from 'effect';
+import { RecorderError, RecorderService } from '../../services/recorder';
 
 class GetNavigatorMediaError extends RecorderError {
 	constructor({ message, origError }: { message: string; origError?: unknown }) {
@@ -41,57 +40,60 @@ const resetRecorder = () => {
 	stream.getTracks().forEach((track) => track.stop());
 };
 
-export const webRecorderService: Context.Tag.Service<RecorderService> = {
-	startRecording: (recordingDeviceId) =>
-		Effect.gen(function* (_) {
-			stream = yield* _(getMediaStream(recordingDeviceId));
-			recordedChunks.length = 0;
-			mediaRecorder = new AudioRecorder(stream);
-			mediaRecorder.addEventListener(
-				'dataavailable',
-				(event: BlobEvent) => {
-					if (!event.data.size) return;
-					recordedChunks.push(event.data);
-				},
-				{ once: true }
-			);
-			mediaRecorder.start();
-		}),
-	stopRecording: Effect.tryPromise({
-		try: () =>
-			new Promise<Blob>((resolve) => {
+export const RecorderServiceLiveWeb = Layer.succeed(
+	RecorderService,
+	RecorderService.of({
+		startRecording: (recordingDeviceId) =>
+			Effect.gen(function* (_) {
+				stream = yield* _(getMediaStream(recordingDeviceId));
+				recordedChunks.length = 0;
+				mediaRecorder = new AudioRecorder(stream);
 				mediaRecorder.addEventListener(
-					'stop',
-					() => {
-						const audioBlob = new Blob(recordedChunks, { type: 'audio/wav' });
-						resolve(audioBlob);
-						resetRecorder();
+					'dataavailable',
+					(event: BlobEvent) => {
+						if (!event.data.size) return;
+						recordedChunks.push(event.data);
 					},
 					{ once: true }
 				);
-				mediaRecorder.stop();
+				mediaRecorder.start();
 			}),
-		catch: (error) =>
-			new StopMediaRecorderError({
-				message: 'Error stopping media recorder and getting audio blob',
-				origError: error
+		stopRecording: Effect.tryPromise({
+			try: () =>
+				new Promise<Blob>((resolve) => {
+					mediaRecorder.addEventListener(
+						'stop',
+						() => {
+							const audioBlob = new Blob(recordedChunks, { type: 'audio/wav' });
+							resolve(audioBlob);
+							resetRecorder();
+						},
+						{ once: true }
+					);
+					mediaRecorder.stop();
+				}),
+			catch: (error) =>
+				new StopMediaRecorderError({
+					message: 'Error stopping media recorder and getting audio blob',
+					origError: error
+				})
+		}).pipe(
+			Effect.catchAll((error) => {
+				resetRecorder();
+				return error;
 			})
-	}).pipe(
-		Effect.catchAll((error) => {
-			resetRecorder();
-			return error;
+		),
+		enumerateRecordingDevices: Effect.tryPromise({
+			try: async () => {
+				const devices = await navigator.mediaDevices.enumerateDevices();
+				const audioInputDevices = devices.filter((device) => device.kind === 'audioinput');
+				return audioInputDevices;
+			},
+			catch: (error) =>
+				new EnumerateRecordingDevicesError({
+					message: 'Error enumerating recording devices',
+					origError: error
+				})
 		})
-	),
-	enumerateRecordingDevices: Effect.tryPromise({
-		try: async () => {
-			const devices = await navigator.mediaDevices.enumerateDevices();
-			const audioInputDevices = devices.filter((device) => device.kind === 'audioinput');
-			return audioInputDevices;
-		},
-		catch: (error) =>
-			new EnumerateRecordingDevicesError({
-				message: 'Error enumerating recording devices',
-				origError: error
-			})
 	})
-};
+);
