d7e9ace76a7d2ef07cf50eb195584afd042d34a0
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-05-23 15:47:00 -0400

    refactor: combine web and desktop into app
    Former-commit-id: 0da29aa8b04151fae21f60d5033a95fb0a421f13
    Former-commit-id: bccbea8bebcfee11e32e17a68dbb5f4839b29eb4

--- apps/web/src/lib/toasts/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as PleaseEnterAPIKeyToast } from './PleaseEnterAPIKeyToast.svelte';
-export { default as SomethingWentWrongToast } from './SomethingWentWrongToast.svelte';
-export { default as TranscriptionComplete } from './TranscriptionComplete.svelte';
--- apps/web/src/routes/(app)/+layout.svelte
+++
@@ -1,3 +0,0 @@
-<main class="flex flex-1 justify-center p-4">
-	<slot />
-</main>
