7ef1c1f4db8ecf2c2c3179bdb97c1e2bdb8d18d7
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-05-23 17:55:44 -0400

    fix: convert createRecorder to runes
    Former-commit-id: 9b41250a9baa5f02f1b751e2ffff62ef851aefac
    Former-commit-id: f0b85b1521cdec28b16886fa9c448b8c43568610

--- apps/app/src/lib/stores/recorder/create-recorder.svelte.ts
+++ apps/app/src/lib/stores/recorder/create-recorder.svelte.ts
@@ -1,9 +1,9 @@
+import { createPersistedState } from '$lib/createPersistedState.svelte';
 import {
 	PleaseEnterAPIKeyToast,
 	SomethingWentWrongToast,
 	TranscriptionComplete
 } from '$lib/toasts';
-import persistedWritable from '@epicenterhq/svelte-persisted-writable';
 import { ClipboardServiceLive } from '@repo/services/implementations/clipboard/web.js';
 import { ClipboardService } from '@repo/services/services/clipboard';
 import { RecorderService } from '@repo/services/services/recorder';
@@ -11,7 +11,6 @@ import type { Recording } from '@repo/services/services/recordings-db';
 import { Effect } from 'effect';
 import { nanoid } from 'nanoid';
 import { toast } from 'svelte-sonner';
-import { get, writable } from 'svelte/store';
 import { z } from 'zod';
 import { recordings } from '../recordings';
 import type { createRecordings } from '../recordings/create-recordings.svelte';
@@ -27,16 +26,18 @@ const INITIAL_STATE = 'IDLE' satisfies RecorderState;
 export const createRecorder = () =>
 	Effect.gen(function* (_) {
 		const recorderService = yield* _(RecorderService);
-		const recorderState = writable<RecorderState>(INITIAL_STATE);
+		let recorderState = $state<RecorderState>(INITIAL_STATE);
 
-		const selectedAudioInputDeviceId = persistedWritable({
+		const selectedAudioInputDeviceId = createPersistedState({
 			key: 'selected-audio-input-device-id',
 			schema: z.string(),
-			initialValue: ''
+			defaultValue: ''
 		});
 
 		return {
-			recorderState,
+			get recorderState() {
+				return recorderState;
+			},
 			selectedAudioInputDeviceId,
 			getAudioInputDevices: recorderService.enumerateRecordingDevices.pipe(
 				Effect.catchAll((error) => {
@@ -46,13 +47,13 @@ export const createRecorder = () =>
 			),
 			refreshDefaultAudioInput: Effect.gen(function* (_) {
 				const recordingDevices = yield* _(recorderService.enumerateRecordingDevices);
-				const $selectedAudioInput = get(selectedAudioInputDeviceId);
+				const $selectedAudioInput = selectedAudioInputDeviceId.value;
 				const isSelectedExists = recordingDevices.some(
 					({ deviceId }) => deviceId === $selectedAudioInput
 				);
 				if (!isSelectedExists) {
 					const firstAudioInput = recordingDevices[0].deviceId;
-					selectedAudioInputDeviceId.set(firstAudioInput);
+					selectedAudioInputDeviceId.value = firstAudioInput;
 				}
 			}).pipe(
 				Effect.catchAll((error) => {
@@ -61,13 +62,12 @@ export const createRecorder = () =>
 				})
 			),
 			toggleRecording: Effect.gen(function* (_) {
-				const $recorderState = get(recorderState);
-				const $selectedAudioInput = get(selectedAudioInputDeviceId);
-				switch ($recorderState) {
+				const $selectedAudioInput = selectedAudioInputDeviceId.value;
+				switch (recorderState) {
 					case 'IDLE': {
 						yield* _(recorderService.startRecording($selectedAudioInput));
 						yield* _(Effect.logInfo('Recording started'));
-						recorderState.set('RECORDING');
+						recorderState = 'RECORDING';
 						break;
 					}
 					case 'RECORDING': {
@@ -82,15 +82,14 @@ export const createRecorder = () =>
 							blob: audioBlob,
 							transcriptionStatus: 'UNPROCESSED'
 						};
-						recorderState.set('IDLE');
+						recorderState = 'IDLE';
 						yield* _(recordings.addRecording(newRecording));
 						const transcribeAndCopyPromise = Effect.gen(function* (_) {
 							const clipboardService = yield* _(ClipboardService);
 							const transcription = yield* _(recordings.transcribeRecording(newRecording.id));
-							const $settings = get(settings);
-							if ($settings.isCopyToClipboardEnabled && transcription)
+							if (settings.value.isCopyToClipboardEnabled && transcription)
 								yield* _(clipboardService.setClipboardText(transcription));
-							if ($settings.isPasteContentsOnSuccessEnabled && transcription)
+							if (settings.value.isPasteContentsOnSuccessEnabled && transcription)
 								yield* _(clipboardService.pasteTextFromClipboard);
 						}).pipe(Effect.provide(ClipboardServiceLive), Effect.runPromise);
 						toast.promise(transcribeAndCopyPromise, {
