8c2ce7e8e9884b71395b5a190907a04a1942cda0
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-05-24 16:54:45 -0400

    feat: DataTableHeaders
    Former-commit-id: 57b00eff584bc027351b03dfe02236a44628ec5b
    Former-commit-id: e9202ab81a19eaeaecca6d5dee23263bf25f7046

---
+++ apps/app/src/routes/(config)/recordings/DataTableHeader.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { Recording } from '@repo/services/services/recordings-db';
+	import { Button } from '@repo/ui/components/button';
+	import type { HeaderContext } from '@tanstack/table-core';
+	import ArrowDown from '~icons/lucide/arrow-down';
+	import ArrowUp from '~icons/lucide/arrow-up';
+	import ArrowUpDown from '~icons/lucide/arrow-up-down';
+
+	let { column }: HeaderContext<Recording, unknown> = $props();
+	let headerText = $derived(column.columnDef.meta?.headerText);
+</script>
+
+<Button
+	variant="ghost"
+	onclick={() => {
+		column.toggleSorting();
+	}}
+>
+	{#if column.getIsSorted() === 'asc'}
+		<ArrowUp class="ml-2 h-4 w-4" />
+	{:else if column.getIsSorted() === 'desc'}
+		<ArrowDown class="ml-2 h-4 w-4" />
+	{:else}
+		<ArrowUpDown class="ml-2 h-4 w-4" />
+	{/if}
+	{headerText}
+</Button>
