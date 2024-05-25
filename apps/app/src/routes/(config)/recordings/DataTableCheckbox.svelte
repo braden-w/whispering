1f53f30bfb9a91822fdce5a7cf2ce216b0ee36fa
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-05-25 10:37:30 -0400

    chore: remove data table checkbox
    Former-commit-id: 55040895caae7d8e5177c4a09df2101f60d812eb
    Former-commit-id: 781ea6fb03bae0a2e5bbf06e273f7094b5b2643c

--- apps/app/src/routes/(config)/recordings/DataTableCheckbox.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import type { Writable } from 'svelte/store';
-	import { Checkbox } from '@repo/ui/components/checkbox';
-
-	export let checked: Writable<boolean>;
-</script>
-
-<Checkbox bind:checked={$checked} />
