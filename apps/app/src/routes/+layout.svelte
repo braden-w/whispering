2bfacd54d58fbea98f84f28adde6ce92ac993a76
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-05-27 19:08:28 -0400

    fix: remove listener logic in favor of logic placed nearby
    Former-commit-id: bf7769a429df63b71c17dca5eede3a5023f47703
    Former-commit-id: 6fda6bda34e4b354ee2be1ccbd203509073b93fc

--- apps/app/src/routes/+layout.svelte
+++ apps/app/src/routes/+layout.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
-	import '@repo/ui/app.pcss';
-	import { ModeWatcher } from 'mode-watcher';
-	import { Toaster } from '@repo/ui/components/sonner';
 	import { onNavigate } from '$app/navigation';
-	import { recorder } from '$lib/stores/recorder';
-	import { onMount } from 'svelte';
-	import { settings } from '$lib/stores/settings.svelte';
-	import { Effect } from 'effect';
-	import { unregisterAll } from '@tauri-apps/api/globalShortcut';
-	import { register } from '@tauri-apps/api/globalShortcut';
+	import '@repo/ui/app.pcss';
+	import { Toaster } from '@repo/ui/components/sonner';
+	import { ModeWatcher } from 'mode-watcher';
 
 	onNavigate((navigation) => {
 		if (!document.startViewTransition) return;
@@ -19,17 +13,6 @@
 				await navigation.complete;
 			});
 		});
-	});
-
-	const registerCurrentGlobalShortcut = async () => {
-		await unregisterAll();
-		await register(settings.currentGlobalShortcut, () =>
-			recorder.toggleRecording.pipe(Effect.runPromise),
-		);
-	};
-
-	$effect(() => {
-		registerCurrentGlobalShortcut();
 	});
 </script>
 
