8ce71ef13d1683a5ae622ba7fc78aea23f9bf060
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-05-27 16:12:28 -0400

    feat: onMount for globalShortcut
    Former-commit-id: 67f96b01a8cfbdd0b3b83c1ef380383b43f92bdc
    Former-commit-id: f74c88019e2bd14d49737969fbe27a92fc57f5ba

--- apps/app/src/routes/+layout.svelte
+++ apps/app/src/routes/+layout.svelte
@@ -3,6 +3,10 @@
 	import { ModeWatcher } from 'mode-watcher';
 	import { Toaster } from '@repo/ui/components/sonner';
 	import { onNavigate } from '$app/navigation';
+	import { recorder } from '$lib/stores/recorder';
+	import { onMount } from 'svelte';
+	import { settings } from '$lib/stores/settings.svelte';
+	import { Effect } from 'effect';
 
 	onNavigate((navigation) => {
 		if (!document.startViewTransition) return;
@@ -13,6 +17,13 @@
 				await navigation.complete;
 			});
 		});
+	});
+
+	onMount(async () => {
+		const { register } = await import('@tauri-apps/api/globalShortcut');
+		await register(settings.currentGlobalShortcut, () =>
+			recorder.toggleRecording.pipe(Effect.runPromise),
+		);
 	});
 </script>
 
