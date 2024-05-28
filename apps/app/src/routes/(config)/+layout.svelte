e83ec3ea4350b4c54d3f8a6c076ef1dae3beedf9
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-05-28 10:52:57 -0400

    feat: update app bar formatting
    Former-commit-id: e3165468a3ba543c84fa346d7619060b325b6a30
    Former-commit-id: 0acc31a73283b0e8cf5b9803d4e5ad1b1037c48d

--- apps/app/src/routes/(config)/+layout.svelte
+++ apps/app/src/routes/(config)/+layout.svelte
@@ -1,37 +1,38 @@
 <script lang="ts">
 	import { recorder } from '$lib/stores/recorder';
 	import { Button } from '@repo/ui/components/button';
-	import * as Tooltip from '@repo/ui/components/tooltip';
+	import { cn } from '@repo/ui/utils';
 	import NavItems from '../NavItems.svelte';
 </script>
 
 <header
-	class="border-border/40 bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 border-b shadow-sm backdrop-blur"
+	class={cn(
+		'border-border/40 bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 border-b shadow-sm backdrop-blur ',
+		'flex h-14 w-full items-center justify-between px-4',
+	)}
 	style="view-transition-name: header"
 >
-	<div class="container flex h-14 w-full max-w-screen-2xl items-center justify-between">
-		<div class="flex gap-2">
-			<Button
-				variant="ghost"
-				size="icon"
-				aria-label="Toggle recording"
-				onclick={recorder.toggleRecording}
-				style="view-transition-name: microphone-icon"
-			>
-				{#if recorder.recorderState === 'RECORDING'}
-					🟥
-				{:else if recorder.recorderState === 'SAVING'}
-					🔄
-				{:else}
-					🎙️
-				{/if}
-			</Button>
-			<Button variant="ghost" href="/">
-				<span class="text-lg font-bold">whispering</span>
-			</Button>
-		</div>
-		<NavItems />
+	<div class="flex gap-2">
+		<Button
+			variant="ghost"
+			size="icon"
+			aria-label="Toggle recording"
+			onclick={recorder.toggleRecording}
+			style="view-transition-name: microphone-icon"
+		>
+			{#if recorder.recorderState === 'RECORDING'}
+				🟥
+			{:else if recorder.recorderState === 'SAVING'}
+				🔄
+			{:else}
+				🎙️
+			{/if}
+		</Button>
+		<Button variant="ghost" href="/">
+			<span class="text-lg font-bold">whispering</span>
+		</Button>
 	</div>
+	<NavItems />
 </header>
 <main class="flex flex-1 justify-center p-4">
 	<slot />
