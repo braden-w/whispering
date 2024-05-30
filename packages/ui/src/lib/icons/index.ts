a6178bf926f9c71e094ca3868b9063ea6f5fa9fe
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-05-30 10:02:57 -0400

    docs: explain reexporting icons
    Former-commit-id: b7c0e062f7ff5fe135d88a2950e7393891dc1df1
    Former-commit-id: 6769cbc1133d57afc1f0fd94dea897eea8403482

--- packages/ui/src/lib/icons/index.ts
+++ packages/ui/src/lib/icons/index.ts
@@ -1,4 +1,10 @@
 export * from 'lucide-svelte';
+
+/** The following are custom icons that either
+ * 1. Don't exist in lucide
+ * 2. Do exist in lucide, but we prefer to override them with our own version from a different library
+ */
+
 export { default as ChromeWebStoreIcon } from './ChromeWebStore.svelte';
 export { default as ClipboardIcon } from './Clipboard.svelte';
 export { default as GithubIcon } from './Github.svelte';
