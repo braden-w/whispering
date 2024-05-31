17a54c1405c445b1799b520204deb6d092030c92
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-05-31 10:48:31 -0400

    format: index.tsx for plasmo website
    Former-commit-id: 09765e95aa5b44e22f89d7a6877180ec43fdb1ad
    Former-commit-id: 178949f0ef798ef64685648d5ef4e5a2707d476f

--- apps/extension/contents/index.tsx
+++ apps/extension/contents/index.tsx
@@ -1,24 +1,24 @@
-import cssText from "data-text:~/contents/style.css"
-import type { PlasmoCSConfig } from "plasmo"
+import cssText from 'data-text:~/style.css';
+import type { PlasmoCSConfig } from 'plasmo';
 
 export const config: PlasmoCSConfig = {
-  matches: ["https://www.plasmo.com/*"]
-}
+	matches: ['https://www.plasmo.com/*'],
+};
 
 export const getStyle = () => {
-  const style = document.createElement("style")
-  style.textContent = cssText
-  return style
-}
+	const style = document.createElement('style');
+	style.textContent = cssText;
+	return style;
+};
 
 const PlasmoContent = () => {
-  return (
-    <div className="fixed inset-5">
-      <button className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700">
-        HELLO WORLD
-      </button>
-    </div>
-  )
-}
+	return (
+		<div className="fixed inset-5">
+			<button className="mb-2 mr-2 rounded-full border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:outline-none">
+				HELLO WORLD
+			</button>
+		</div>
+	);
+};
 
-export default PlasmoContent
+export default PlasmoContent;
