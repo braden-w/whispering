b09e629f0839d550b3d7f7b96f2f31dce509dc23
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-05-31 09:51:32 -0400

    feat: getWhisperingTab and such
    Former-commit-id: 9c188b553d1943dcca9b8fb00c68ba60c6765d80
    Former-commit-id: dba578f5c349058305f1ec39e19609d054042b21

--- apps/extension/background/index.ts
+++ apps/extension/background/index.ts
@@ -1,75 +1,72 @@
-import {
-  sendMessageToContentScript,
-  type MessageToBackgroundRequest
-} from "$lib/utils/messaging"
+// import {
+//   sendMessageToContentScript,
+//   type MessageToBackgroundRequest
+// } from "$lib/utils/messaging"
+
+import { setIcon } from './setIcon';
 
 chrome.runtime.onInstalled.addListener((details) => {
-  if (details.reason === "install") {
-    chrome.runtime.openOptionsPage()
-  }
-})
+	if (details.reason === 'install') {
+		chrome.runtime.openOptionsPage();
+	}
+});
 
 chrome.commands.onCommand.addListener(async function (command) {
-  if (command === "toggle-recording") {
-    sendMessageToContentScript({ command: "toggle-recording" })
-  }
-})
+	if (command === 'toggle-recording') {
+		// sendMessageToContentScript({ command: "toggle-recording" })
+	}
+});
 
-chrome.runtime.onMessage.addListener(function (
-  message: MessageToBackgroundRequest
-) {
-  switch (message.action) {
-    case "setExtensionIcon":
-      setIcon(message.icon)
-      break
-    case "openOptionsPage":
-      chrome.runtime.openOptionsPage()
-      break
-  }
-})
+// chrome.runtime.onMessage.addListener(function (
+//   message: MessageToBackgroundRequest
+// ) {
+//   switch (message.action) {
+//     case "setExtensionIcon":
+//       setIcon(message.icon)
+//       break
+//     case "openOptionsPage":
+//       chrome.runtime.openOptionsPage()
+//       break
+//   }
+// })
 
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-  if (
-    [
-      "getLocalStorage",
-      "setLocalStorage",
-      "getIndexedDB",
-      "setIndexedDB"
-    ].includes(message.action)
-  ) {
-    const whisperingTabId = await getOrCreateWhisperingTab()
-    chrome.scripting.executeScript(
-      {
-        target: { tabId: whisperingTabId },
-        files: ["scripts/content.js"]
-      },
-      () => {
-        chrome.tabs.sendMessage(whisperingTabId, message, sendResponse)
-      }
-    )
+	if (
+		['getLocalStorage', 'setLocalStorage', 'getIndexedDB', 'setIndexedDB'].includes(message.action)
+	) {
+		const whisperingTabId = await getOrCreateWhisperingTab();
+		chrome.scripting.executeScript(
+			{
+				target: { tabId: whisperingTabId },
+				files: ['scripts/content.js'],
+			},
+			() => {
+				chrome.tabs.sendMessage(whisperingTabId, message, sendResponse);
+			},
+		);
 
-    return true // Will respond asynchronously.
-  }
-})
+		return true; // Will respond asynchronously.
+	}
+});
 
 function getOrCreateWhisperingTab() {
-  return new Promise<number>((resolve, reject) => {
-    chrome.tabs.query({ url: "https://www.whispering.com/*" }, (tabs) => {
-      if (tabs.length > 0) {
-        for (const tab of tabs) {
-          if (tab.pinned) {
-            resolve(tab.id)
-          }
-        }
-        resolve(tabs[0].id)
-      } else {
-        chrome.tabs.create(
-          { url: "https://www.whispering.com", active: false, pinned: true },
-          (tab) => {
-            resolve(tab.id)
-          }
-        )
-      }
-    })
-  })
+	return new Promise<number>((resolve, reject) => {
+		chrome.tabs.query({ url: 'https://www.whispering.com/*' }, (tabs) => {
+			if (tabs.length > 0) {
+				for (const tab of tabs) {
+					if (tab.pinned) {
+						resolve(tab.id);
+					}
+				}
+				resolve(tabs[0].id);
+			} else {
+				chrome.tabs.create(
+					{ url: 'https://www.whispering.com', active: false, pinned: true },
+					(tab) => {
+						resolve(tab.id);
+					},
+				);
+			}
+		});
+	});
 }
