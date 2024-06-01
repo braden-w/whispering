e2bbf0e3a1abe498779302cd8149e6eafee8a853
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-01 10:43:56 -0400

    refactor: getLocalStorage
    Former-commit-id: fbb54ca4dc399a3d41769e7a0af734f67120d9fb
    Former-commit-id: 9b1e25fa6b21e2220893bf217dfceaae2f47efa7

--- apps/extension/lib/settings.ts
+++ apps/extension/lib/settings.ts
@@ -1,66 +1,56 @@
-import { Effect } from 'effect';
 import { z } from 'zod';
 import { getOrCreateWhisperingTab } from '~background';
 import { createGetLocalStorageForTab } from './utils/messaging';
 
-const createSettings = async function () {
-	const whisperingTabId = await getOrCreateWhisperingTab();
-	const getLocalStorage = createGetLocalStorageForTab(whisperingTabId);
-	const isPlaySoundEnabledPromise = getLocalStorage({
-		key: 'whispering-is-play-sound-enabled',
-		schema: z.boolean(),
-		defaultValue: true,
-	});
-	const isCopyToClipboardEnabledPromise = getLocalStorage({
-		key: 'whispering-is-copy-to-clipboard-enabled',
-		schema: z.boolean(),
-		defaultValue: true,
-	});
-	const isPasteContentsOnSuccessEnabledPromise = getLocalStorage({
-		key: 'whispering-is-paste-contents-on-success-enabled',
-		schema: z.boolean(),
-		defaultValue: true,
-	});
-	const apiKeyPromise = getLocalStorage({
-		key: 'whispering-api-key',
-		schema: z.string(),
-		defaultValue: '',
-	});
-	const outputLanguagePromise = getLocalStorage({
-		key: 'whispering-output-language',
-		schema: z.string(),
-		defaultValue: 'en',
-	});
-	const [
-		isPlaySoundEnabled,
-		isCopyToClipboardEnabled,
-		isPasteContentsOnSuccessEnabled,
-		apiKey,
-		outputLanguage,
-	] = await Promise.all([
-		isPlaySoundEnabledPromise,
-		isCopyToClipboardEnabledPromise,
-		isPasteContentsOnSuccessEnabledPromise,
-		apiKeyPromise,
-		outputLanguagePromise,
-	]);
-	return {
-		get isPlaySoundEnabled() {
-			return isPlaySoundEnabled;
-		},
-		get isCopyToClipboardEnabled() {
-			return isCopyToClipboardEnabled;
-		},
-		get isPasteContentsOnSuccessEnabled() {
-			return isPasteContentsOnSuccessEnabled;
-		},
-		get apiKey() {
-			return apiKey;
-		},
-		get outputLanguage() {
-			return outputLanguage;
-		},
-	};
+export const settings = {
+	async getIsPlaySoundEnabled() {
+		const whisperingTabId = await getOrCreateWhisperingTab();
+		const getLocalStorage = createGetLocalStorageForTab(whisperingTabId);
+		const isPlaySoundEnabled = await getLocalStorage({
+			key: 'whispering-is-play-sound-enabled',
+			schema: z.boolean(),
+			defaultValue: true,
+		});
+		return isPlaySoundEnabled;
+	},
+	async getIsCopyToClipboardEnabled() {
+		const whisperingTabId = await getOrCreateWhisperingTab();
+		const getLocalStorage = createGetLocalStorageForTab(whisperingTabId);
+		const isCopyToClipboardEnabled = await getLocalStorage({
+			key: 'whispering-is-copy-to-clipboard-enabled',
+			schema: z.boolean(),
+			defaultValue: true,
+		});
+		return isCopyToClipboardEnabled;
+	},
+	async getIsPasteContentsOnSuccessEnabled() {
+		const whisperingTabId = await getOrCreateWhisperingTab();
+		const getLocalStorage = createGetLocalStorageForTab(whisperingTabId);
+		const isPasteContentsOnSuccessEnabled = await getLocalStorage({
+			key: 'whispering-is-paste-contents-on-success-enabled',
+			schema: z.boolean(),
+			defaultValue: true,
+		});
+		return isPasteContentsOnSuccessEnabled;
+	},
+	async getApiKey() {
+		const whisperingTabId = await getOrCreateWhisperingTab();
+		const getLocalStorage = createGetLocalStorageForTab(whisperingTabId);
+		const apiKey = await getLocalStorage({
+			key: 'whispering-api-key',
+			schema: z.string(),
+			defaultValue: '',
+		});
+		return apiKey;
+	},
+	async getOutputLanguage() {
+		const whisperingTabId = await getOrCreateWhisperingTab();
+		const getLocalStorage = createGetLocalStorageForTab(whisperingTabId);
+		const outputLanguage = await getLocalStorage({
+			key: 'whispering-output-language',
+			schema: z.string(),
+			defaultValue: 'en',
+		});
+		return outputLanguage;
+	},
 };
-
-export const settings = createSettings();
