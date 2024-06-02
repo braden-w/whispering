04ee022b3ec319efce2f442ec35b1a9ddc54d8cd
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-02 15:35:52 -0400

    refactor: update RecorderStateLive
    Former-commit-id: fb08268f92a5e28f05a9be3c7620615be8aa13ec
    Former-commit-id: 702c65f584a6496f6d93b7c16653e6f2c80a3058

--- apps/extension/lib/storage/RecorderStateLive.ts
+++ apps/extension/lib/storage/RecorderStateLive.ts
@@ -10,19 +10,15 @@ export const RecorderStateLive = Layer.effect(
 		const extensionStorageService = yield* ExtensionStorageService;
 		return {
 			get: () =>
-				Effect.gen(function* () {
-					return yield* extensionStorageService.get({
-						key: 'whispering-recording-state',
-						schema: z.union([z.literal('IDLE'), z.literal('RECORDING')]),
-						defaultValue: 'IDLE',
-					});
+				extensionStorageService.get({
+					key: 'whispering-recording-state',
+					schema: z.union([z.literal('IDLE'), z.literal('RECORDING')]),
+					defaultValue: 'IDLE',
 				}),
 			set: (value) =>
-				Effect.gen(function* () {
-					yield* extensionStorageService.set({
-						key: 'whispering-recording-state',
-						value,
-					});
+				extensionStorageService.set({
+					key: 'whispering-recording-state',
+					value,
 				}),
 		};
 	}),
