92495d22cd2a6b2d596c1a00683ee39a8895360e
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-02 16:06:49 -0400

    chore: remove unused code
    Former-commit-id: 5b363293d40d619e5bf73214ce083e047dd68782
    Former-commit-id: e8accbb33394354f797fd2746e6ccfb40b164a72

--- apps/extension/lib/utils/messaging.ts
+++ apps/extension/lib/utils/messaging.ts
@@ -1,6 +1,5 @@
 // import type { Icon } from '$background/setIcon';
 
-import { Effect } from 'effect';
 import { useEffect, useState } from 'react';
 import type { z } from 'zod';
 
@@ -55,43 +54,3 @@ export const sendMessageToContentScript = <R>(
 	message: MessageToContentScriptRequest,
 	options?: chrome.tabs.MessageSendOptions,
 ) => chrome.tabs.sendMessage<MessageToContentScriptRequest, R>(tabId, message, options);
-
-export function getLocalStorageFromTab<TSchema extends z.ZodTypeAny>({
-	tabId,
-	key,
-	schema,
-	defaultValue,
-}: {
-	tabId: number;
-	key: string;
-	schema: TSchema;
-	defaultValue: z.infer<TSchema>;
-}) {
-	const [state, setState] = useState<z.infer<TSchema>>(defaultValue);
-
-	const syncState = () => loadValueFromStorage().then(setState);
-
-	useEffect(() => {
-		syncState();
-		sendMessageToContentScript(tabId, {
-			action: 'registerListener',
-			callback: (event) => {
-				console.log('🚀 ~ registerSyncListener ~ event:', event);
-				if (event.key === key) {
-					syncState();
-				}
-			},
-		});
-	});
-
-	return state;
-}
-
-/** Sends a message to the content script, captured in {@link ~contents/globalToggleRecording}. */
-// export async function sendMessageToContentScript(message: MessageToContentScriptRequest) {
-// 	const [tab] = await chrome.tabs.query({
-// 		active: true,
-// 		lastFocusedWindow: true,
-// 	});
-// 	chrome.tabs.sendMessage(tab.id, message);
-// }
