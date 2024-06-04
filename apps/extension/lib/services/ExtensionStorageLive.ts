cfee0627a7d977956ee5a9cb501bcb6c256733c7
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-04 10:48:30 -0400

    fix: ExtensionStorage Watch api
    Former-commit-id: ca4bba5f42837778e217e4aad7586eaf0d419cd2
    Former-commit-id: b4a6657bb6869f5a1f3bc74bce845269e5dde89f

--- apps/extension/lib/services/ExtensionStorageLive.ts
+++ apps/extension/lib/services/ExtensionStorageLive.ts
@@ -1,7 +1,6 @@
 import { Storage } from '@plasmohq/storage';
 import { Effect, Layer } from 'effect';
 import { ExtensionStorageError, ExtensionStorageService } from './ExtensionStorage';
-import { z } from 'zod';
 
 export const ExtensionStorageLive = Layer.effect(
 	ExtensionStorageService,
@@ -33,23 +32,24 @@ export const ExtensionStorageLive = Layer.effect(
 						});
 					},
 				}),
-			watch: ({ key, schema, callback }) =>
-				Effect.try({
+			watch: ({ key, schema, callback }) => {
+				const callbackProgram = (newValueUnparsed: unknown) =>
+					Effect.gen(function* () {
+						const newValueParseResult = schema.safeParse(newValueUnparsed);
+						if (!newValueParseResult.success) {
+							console.error(
+								`Error parsing change for key: ${key}`,
+								newValueParseResult.error.errors,
+							);
+							return Effect.succeed(undefined);
+						}
+						const newValue = newValueParseResult.data;
+						yield* callback(newValue);
+					});
+				return Effect.try({
 					try: () => {
-						const changeSchema = z.object({
-							newValue: schema.optional(),
-							oldValue: schema.optional(),
-						});
 						storage.watch({
-							key: (unparsedChange) => {
-								const parseResult = changeSchema.safeParse(unparsedChange);
-								if (!parseResult.success) {
-									console.error(`Error parsing change for key: ${key}`, parseResult.error.errors);
-									return Effect.succeed(undefined);
-								}
-								const change = parseResult.data;
-								callback(change).pipe(Effect.runSync);
-							},
+							[key]: ({ newValue }) => callbackProgram(newValue).pipe(Effect.runSync),
 						});
 					},
 					catch: (error) =>
@@ -57,7 +57,8 @@ export const ExtensionStorageLive = Layer.effect(
 							message: `Error watching local storage for key: ${key}`,
 							origError: error,
 						}),
-				}),
+				});
+			},
 		};
 	}),
 );
