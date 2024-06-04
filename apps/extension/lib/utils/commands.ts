dc978540a71029fe783c8e1287f46bc0cccea23e
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-04 00:16:35 -0400

    feat: toggleRecording and such
    Former-commit-id: 3abf97eefee655e4406780b757123c6e9354f96c
    Former-commit-id: 1764bcb4127ee0ab4a382bc1baefa3fbf6ef098b

--- apps/extension/lib/utils/commands.ts
+++ apps/extension/lib/utils/commands.ts
@@ -1,9 +1,21 @@
 import { Data, Effect } from 'effect';
 import { z } from 'zod';
+import { RecorderService } from './RecorderService';
+import { RecorderServiceLive } from './RecorderServiceLive';
+import { RecorderStateService } from '~lib/storage/RecorderState';
+import { RecorderStateLive } from '~lib/storage/RecorderStateLive';
 import type { BackgroundServiceWorkerContext } from '~background';
 import type { GlobalContentScriptContext } from '~contents/globalToggleRecording';
 import type { PopupContext } from '~popup';
 import type { WhisperingContentScriptContext } from '~contents/whispering';
+
+import startSoundSrc from 'data-base64:~assets/zapsplat_household_alarm_clock_button_press_12967.mp3';
+import stopSoundSrc from 'data-base64:~assets/sound_ex_machina_Button_Blip.mp3';
+import cancelSoundSrc from 'data-base64:~assets/zapsplat_multimedia_click_button_short_sharp_73510.mp3';
+
+const startSound = new Audio(startSoundSrc);
+const stopSound = new Audio(stopSoundSrc);
+const cancelSound = new Audio(cancelSoundSrc);
 
 /**
  * Represents the possible contexts where a command can run.
@@ -54,7 +66,7 @@ type ContextConfig<NativeContext extends Context> = {
 	 */
 	[ExecuteContext in Context as ExecuteContext extends NativeContext
 		? `${NativeRunPrefix}${ExecuteContext}`
-		: never]: (...args: any[]) => Effect.Effect<any, any>;
+		: never]: (...args: any[]) => Effect.Effect<any, any> | Effect.Effect<any, any>;
 } & {
 	/**
 	 * The optional functions to invoke the command from other contexts via
@@ -62,7 +74,9 @@ type ContextConfig<NativeContext extends Context> = {
 	 */
 	[OtherContext in Context as OtherContext extends NativeContext
 		? never
-		: `${RemoteInvocationPrefix}${OtherContext}`]?: (...args: any[]) => Effect.Effect<any, any>;
+		: `${RemoteInvocationPrefix}${OtherContext}`]?: (
+		...args: any[]
+	) => Effect.Effect<any, any> | Effect.Effect<any, any>;
 };
 
 /**
@@ -92,8 +106,173 @@ const commandNames = [
 	'getCurrentTabId',
 	'getSettings',
 	'toggleRecording',
+	'cancelRecording',
 ] as const;
 type CommandName = (typeof commandNames)[number];
+
+// --- Begin commands ---
+
+const openOptionsPage = {
+	runsIn: 'BackgroundServiceWorker',
+	runInBackgroundServiceWorker: () =>
+		Effect.tryPromise({
+			try: () => chrome.runtime.openOptionsPage(),
+			catch: (e) => new InvokeCommandError({ message: 'Error opening options page', origError: e }),
+		}),
+	invokeFromGlobalContentScript: () =>
+		Effect.gen(function* () {
+			yield* sendMessageToBackground<void>({ command: 'openOptionsPage' });
+		}),
+} as const satisfies CommandConfig;
+
+const getCurrentTabId = {
+	runsIn: 'BackgroundServiceWorker',
+	runInBackgroundServiceWorker: () =>
+		Effect.gen(function* () {
+			const activeTabs = yield* Effect.tryPromise({
+				try: () => chrome.tabs.query({ active: true, currentWindow: true }),
+				catch: (error) =>
+					new InvokeCommandError({
+						message: 'Error getting active tabs',
+						origError: error,
+					}),
+			});
+			const firstActiveTab = activeTabs[0];
+			if (!firstActiveTab) {
+				return yield* new InvokeCommandError({ message: 'No active tab found' });
+			}
+			return firstActiveTab.id;
+		}),
+} as const satisfies CommandConfig;
+
+const settingsSchema = z.object({
+	isPlaySoundEnabled: z.boolean(),
+	isCopyToClipboardEnabled: z.boolean(),
+	isPasteContentsOnSuccessEnabled: z.boolean(),
+	selectedAudioInputDeviceId: z.string(),
+	currentLocalShortcut: z.string(),
+	currentGlobalShortcut: z.string(),
+	apiKey: z.string(),
+	outputLanguage: z.string(),
+});
+type Settings = z.infer<typeof settingsSchema>;
+
+const getSettings = {
+	runsIn: 'WhisperingContentScript',
+	runInWhisperingContentScript: () =>
+		getLocalStorage({
+			key: 'whispering-settings',
+			schema: settingsSchema,
+			defaultValue: {
+				isPlaySoundEnabled: true,
+				isCopyToClipboardEnabled: true,
+				isPasteContentsOnSuccessEnabled: true,
+				selectedAudioInputDeviceId: '',
+				currentLocalShortcut: 'space',
+				currentGlobalShortcut: '',
+				apiKey: '',
+				outputLanguage: 'en',
+			},
+		}),
+	invokeFromGlobalContentScript: () =>
+		Effect.gen(function* () {
+			const whisperingTabId = yield* getOrCreateWhisperingTabId;
+			return yield* sendMessageToContentScript<Settings>(whisperingTabId, {
+				command: 'getSettings',
+			});
+		}),
+} as const satisfies CommandConfig;
+
+const setSettings = {
+	runsIn: 'WhisperingContentScript',
+	runInWhisperingContentScript: (settings: Settings) =>
+		setLocalStorage({
+			key: 'whispering-settings',
+			value: JSON.stringify(settings),
+		}),
+	invokeFromGlobalContentScript: (settings: Settings) =>
+		Effect.gen(function* () {
+			const whisperingTabId = yield* getOrCreateWhisperingTabId;
+			return yield* sendMessageToContentScript<void>(whisperingTabId, {
+				command: 'setSettings',
+				settings,
+			});
+		}),
+} as const satisfies CommandConfig;
+
+const toggleRecording = {
+	runsIn: 'GlobalContentScript',
+	runInGlobalContentScript: () =>
+		Effect.gen(function* () {
+			const checkAndUpdateSelectedAudioInputDevice = () =>
+				Effect.gen(function* () {
+					const settings = yield* getSettings.invokeFromGlobalContentScript();
+					const recordingDevices = yield* recorderService.enumerateRecordingDevices;
+					const isSelectedDeviceExists = recordingDevices.some(
+						({ deviceId }) => deviceId === settings.selectedAudioInputDeviceId,
+					);
+					if (!isSelectedDeviceExists) {
+						// toast.info('Default audio input device not found, selecting first available device');
+						const firstAudioInput = recordingDevices[0].deviceId;
+						const oldSettings = yield* getSettings.invokeFromGlobalContentScript();
+						yield* setSettings.invokeFromGlobalContentScript({
+							...oldSettings,
+							selectedAudioInputDeviceId: firstAudioInput,
+						});
+					}
+				}).pipe(
+					Effect.catchAll((error) => {
+						// toast.error(error.message);
+						return Effect.succeed(undefined);
+					}),
+				);
+			const recorderService = yield* RecorderService;
+			const recorderStateService = yield* RecorderStateService;
+			const settings = { apiKey: '', selectedAudioInputDeviceId: '', isPlaySoundEnabled: true };
+			if (!settings.apiKey) {
+				alert('Please set your API key in the extension options');
+				yield* openOptionsPage.invokeFromGlobalContentScript();
+				return;
+			}
+			yield* checkAndUpdateSelectedAudioInputDevice();
+			const recorderState = yield* recorderStateService.get();
+			switch (recorderState) {
+				case 'IDLE': {
+					yield* recorderService.startRecording(settings.selectedAudioInputDeviceId);
+					if (settings.isPlaySoundEnabled) startSound.play();
+					// sendMessageToBackground({ command: 'syncIconToRecorderState', recorderState });
+					yield* Effect.logInfo('Recording started');
+					yield* recorderStateService.set('RECORDING');
+					break;
+				}
+				case 'RECORDING': {
+					yield* recorderService.stopRecording();
+					if (settings.isPlaySoundEnabled) stopSound.play();
+					// sendMessageToBackground({ command: 'syncIconToRecorderState', recorderState });
+					yield* Effect.logInfo('Recording stopped');
+					yield* recorderStateService.set('IDLE');
+					break;
+				}
+				default: {
+					yield* Effect.logError('Invalid recorder state');
+				}
+			}
+		}).pipe(Effect.provide(RecorderServiceLive), Effect.provide(RecorderStateLive)),
+	invokeFromBackgroundServiceWorker: () =>
+		Effect.gen(function* () {
+			const activeTabId = yield* getActiveTabId();
+			yield* sendMessageToContentScript(activeTabId, {
+				command: 'toggleRecording',
+			});
+		}),
+	invokeFromPopup: () =>
+		Effect.gen(function* () {
+			const activeTabId = yield* getActiveTabId();
+			yield* sendMessageToContentScript(activeTabId, {
+				command: 'toggleRecording',
+			});
+		}),
+} as const satisfies CommandConfig;
 
 /**
  * Object containing implementations of various commands.
@@ -104,127 +283,14 @@ type CommandName = (typeof commandNames)[number];
  *
  * Example:
  * ```
- * invokeCommand.getCurrentTabId.fromBackgroundServiceWorker();
+ * commands.getCurrentTabId.invokeFromBackgroundServiceWorker();
  * ```
  */
-const commands = {
-	openOptionsPage: {
-		runsIn: 'BackgroundServiceWorker',
-		runInBackgroundServiceWorker: () =>
-			Effect.tryPromise({
-				try: () => chrome.runtime.openOptionsPage(),
-				catch: (e) =>
-					new InvokeCommandError({ message: 'Error opening options page', origError: e }),
-			}),
-	},
-	getCurrentTabId: {
-		runsIn: 'BackgroundServiceWorker',
-		runInBackgroundServiceWorker: () =>
-			Effect.gen(function* () {
-				const activeTabs = yield* Effect.tryPromise({
-					try: () => chrome.tabs.query({ active: true, currentWindow: true }),
-					catch: (error) =>
-						new InvokeCommandError({
-							message: 'Error getting active tabs',
-							origError: error,
-						}),
-				});
-				const firstActiveTab = activeTabs[0];
-				if (!firstActiveTab) {
-					return yield* new InvokeCommandError({ message: 'No active tab found' });
-				}
-				return firstActiveTab.id;
-			}),
-	},
-	getSettings: {
-		runsIn: 'WhisperingContentScript',
-		runInWhisperingContentScript: () =>
-			Effect.gen(function* () {
-				const appStorageService = yield* AppStorageService;
-				const registerShortcutsService = yield* RegisterShortcutsService;
-				return appStorageService.get({
-					key: 'whispering-settings',
-					schema: z.object({
-						isPlaySoundEnabled: z.boolean(),
-						isCopyToClipboardEnabled: z.boolean(),
-						isPasteContentsOnSuccessEnabled: z.boolean(),
-						selectedAudioInputDeviceId: z.string(),
-						currentLocalShortcut: z.string(),
-						currentGlobalShortcut: z.string(),
-						apiKey: z.string(),
-						outputLanguage: z.string(),
-					}),
-					defaultValue: {
-						isPlaySoundEnabled: true,
-						isCopyToClipboardEnabled: true,
-						isPasteContentsOnSuccessEnabled: true,
-						selectedAudioInputDeviceId: '',
-						currentLocalShortcut: registerShortcutsService.defaultLocalShortcut,
-						currentGlobalShortcut: registerShortcutsService.defaultGlobalShortcut,
-						apiKey: '',
-						outputLanguage: 'en',
-					},
-				});
-			}).pipe(Effect.provide(AppStorageLive), Effect.provide(RegisterShortcutsWebLive)),
-	},
-	toggleRecording: {
-		runsIn: 'GlobalContentScript',
-		runInGlobalContentScript: () =>
-			Effect.gen(function* () {
-				const settings = yield* settingsService.get();
-				if (!settings.apiKey) {
-					alert('Please set your API key in the extension options');
-					openOptionsPage();
-					return;
-				}
-				yield* checkAndUpdateSelectedAudioInputDevice();
-				const recorderState = yield* recorderStateService.get();
-				switch (recorderState) {
-					case 'IDLE': {
-						yield* recorderService.startRecording(settings.selectedAudioInputDeviceId);
-						if (settings.isPlaySoundEnabled) startSound.play();
-						sendMessageToBackground({ command: 'syncIconToRecorderState', recorderState });
-						yield* Effect.logInfo('Recording started');
-						yield* recorderStateService.set('RECORDING');
-						break;
-					}
-					case 'RECORDING': {
-						yield* recorderService.stopRecording();
-						if (settings.isPlaySoundEnabled) stopSound.play();
-						sendMessageToBackground({ command: 'syncIconToRecorderState', recorderState });
-						yield* Effect.logInfo('Recording stopped');
-						yield* recorderStateService.set('IDLE');
-						break;
-					}
-					default: {
-						yield* Effect.logError('Invalid recorder state');
-					}
-				}
-			}),
-		invokeFromBackgroundServiceWorker: () =>
-			Effect.gen(function* () {
-				const getActiveTabId = () =>
-					Effect.gen(function* () {
-						const activeTabs = yield* Effect.tryPromise({
-							try: () => chrome.tabs.query({ active: true, currentWindow: true }),
-							catch: (error) =>
-								new InvokeCommandError({
-									message: 'Error getting active tabs',
-									origError: error,
-								}),
-						});
-						const firstActiveTab = activeTabs[0];
-						if (!firstActiveTab) {
-							return yield* new InvokeCommandError({ message: 'No active tab found' });
-						}
-						return firstActiveTab.id;
-					});
-				const activeTabId = yield* getActiveTabId();
-				yield* sendMessageToContentScript(activeTabId, {
-					command: 'toggleRecording',
-				});
-			}),
-	},
+export const commands = {
+	getCurrentTabId,
+	getSettings,
+	openOptionsPage,
+	toggleRecording,
 } as const satisfies Record<CommandName, CommandConfig>;
 
 type MessageToContentScriptRequest = {
@@ -238,3 +304,74 @@ type MessageToBackgroundRequest = {
 		command: K;
 	}; // & Parameters<(typeof commands)[K][`runIn${(typeof commands)[K]['runsIn']}`]>[0];
 }[CommandName];
+
+const getLocalStorage = <TSchema extends z.ZodTypeAny>({
+	key,
+	schema,
+	defaultValue,
+}: {
+	key: string;
+	schema: TSchema;
+	defaultValue: z.infer<TSchema>;
+}) =>
+	Effect.try({
+		try: () => {
+			const valueFromStorage = localStorage.getItem(key);
+			const isEmpty = valueFromStorage === null;
+			if (isEmpty) return defaultValue;
+			return schema.parse(JSON.parse(valueFromStorage)) as z.infer<TSchema>;
+		},
+		catch: (error) =>
+			new InvokeCommandError({
+				message: `Error getting from local storage for key: ${key}`,
+				origError: error,
+			}),
+	}).pipe(Effect.catchAll(() => Effect.succeed(defaultValue)));
+
+const setLocalStorage = ({ key, value }: { key: string; value: any }) =>
+	Effect.try({
+		try: () => localStorage.setItem(key, value),
+		catch: (error) =>
+			new InvokeCommandError({
+				message: `Error setting in local storage for key: ${key}`,
+				origError: error,
+			}),
+	});
+
+const getOrCreateWhisperingTabId = Effect.gen(function* (_) {
+	const tabs = yield* Effect.promise(() => chrome.tabs.query({ url: 'http://localhost:5173/*' }));
+	if (tabs.length > 0) {
+		for (const tab of tabs) {
+			if (tab.pinned) {
+				return tab.id;
+			}
+		}
+		return tabs[0].id;
+	} else {
+		const newTab = yield* Effect.promise(() =>
+			chrome.tabs.create({
+				url: 'http://localhost:5173',
+				active: false,
+				pinned: true,
+			}),
+		);
+		return newTab.id;
+	}
+});
+
+const getActiveTabId = () =>
+	Effect.gen(function* () {
+		const activeTabs = yield* Effect.tryPromise({
+			try: () => chrome.tabs.query({ active: true, currentWindow: true }),
+			catch: (error) =>
+				new InvokeCommandError({
+					message: 'Error getting active tabs',
+					origError: error,
+				}),
+		});
+		const firstActiveTab = activeTabs[0];
+		if (!firstActiveTab.id) {
+			return yield* new InvokeCommandError({ message: 'No active tab ID found' });
+		}
+		return firstActiveTab.id;
+	});
