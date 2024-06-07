a60e137d81db6cad72c44a4e72e5fd7f1e2ac3ad
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-07 13:54:46 -0400

    feat: getSettings command
    Former-commit-id: 5e2f09489d3b7d86836610bea83914c5a1b48632
    Former-commit-id: 00275040a822b490dafba9fbe6beaf5d25c09ad7

--- apps/extension/background/messages/getSettings.ts
+++ apps/extension/background/messages/getSettings.ts
@@ -1,23 +1,22 @@
 import type { PlasmoMessaging } from '@plasmohq/messaging';
 import { Effect } from 'effect';
-import { BackgroundServiceWorkerError, type BackgroundServiceWorkerResponse } from '~background';
+import {
+	sendMessageToWhisperingContentScript,
+	type BackgroundServiceWorkerResponse,
+} from '~background';
+import type { Settings } from '~contents/whispering';
 
 export type RequestBody = {};
 
-export type ResponseBody = BackgroundServiceWorkerResponse<true>;
+export type ResponseBody = BackgroundServiceWorkerResponse<Settings>;
 
 const handler: PlasmoMessaging.MessageHandler<RequestBody, ResponseBody> = (req, res) =>
 	Effect.gen(function* () {
-		yield* Effect.tryPromise({
-			try: () => chrome.runtime.openOptionsPage(),
-			catch: (error) =>
-				new BackgroundServiceWorkerError({
-					title: 'Error opening options page',
-					description: error instanceof Error ? error.message : undefined,
-					error,
-				}),
+		const settings = yield* sendMessageToWhisperingContentScript({
+			commandName: 'getSettings',
+			args: [],
 		});
-		return true as const;
+		return settings;
 	}).pipe(
 		Effect.map((data) => ({ data, error: null })),
 		Effect.catchAll((error) => Effect.succeed({ data: null, error })),
