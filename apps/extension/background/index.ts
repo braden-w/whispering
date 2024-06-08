54267a0cc09e96c8aa2f3be294909ea447d99fbe
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-08 11:05:25 -0400

    feat: register watcher for settin recording-state
    Former-commit-id: a5595b8e52ce36516da35e7fb0031654ec0ce967
    Former-commit-id: b7e0b4a3c55d900315f70e519cda5fe860d95422

--- apps/extension/background/index.ts
+++ apps/extension/background/index.ts
@@ -1,32 +1,40 @@
+import studioMicrophone from 'data-base64:~assets/studio_microphone.png';
+import redLargeSquare from 'data-base64:~assets/red_large_square.png';
 import { Console, Effect } from 'effect';
 import { sendMessageToGlobalContentScript } from './sendMessage';
 import { BackgroundServiceWorkerError } from '~lib/commands';
+import { extensionStorage } from '~lib/services/extension-storage';
 
-// const syncIconWithExtensionStorage = Effect.gen(function* () {
-// 	yield* extensionStorage.watch({
-// 		key: 'whispering-recording-state',
-// 		schema: recorderStateSchema,
-// 		callback: (recordingState) =>
-// 			Effect.gen(function* () {
-// 				switch (recordingState) {
-// 					case 'IDLE':
-// 						yield* Effect.tryPromise({
-// 							try: () => chrome.action.setIcon({ path: studioMicrophone }),
-// 							catch: () =>
-// 								new BackgroundServiceWorkerError({
-// 									message: 'Error setting icon to studio microphone',
-// 								}),
-// 						});
-// 					case 'RECORDING':
-// 						yield* Effect.tryPromise({
-// 							try: () => chrome.action.setIcon({ path: redLargeSquare }),
-// 							catch: () =>
-// 								new BackgroundServiceWorkerError({ message: 'Error setting icon to stop icon' }),
-// 						});
-// 				}
-// 			}),
-// 	});
-// }).pipe(Effect.runSync);
+const _syncIconWithExtensionStorage = Effect.gen(function* () {
+	yield* extensionStorage.watch({
+		key: 'whispering-recording-state',
+		callback: (recordingState) =>
+			Effect.gen(function* () {
+				switch (recordingState) {
+					case 'IDLE':
+						yield* Effect.tryPromise({
+							try: () => chrome.action.setIcon({ path: studioMicrophone }),
+							catch: (error) =>
+								new BackgroundServiceWorkerError({
+									title: 'Error setting icon to studio microphone',
+									description: error instanceof Error ? error.message : undefined,
+									error,
+								}),
+						});
+					case 'RECORDING':
+						yield* Effect.tryPromise({
+							try: () => chrome.action.setIcon({ path: redLargeSquare }),
+							catch: (error) =>
+								new BackgroundServiceWorkerError({
+									title: 'Error setting icon to stop icon',
+									description: error instanceof Error ? error.message : undefined,
+									error,
+								}),
+						});
+				}
+			}),
+	});
+}).pipe(Effect.runSync);
 
 export const openOptionsPage = Effect.tryPromise({
 	try: () => chrome.runtime.openOptionsPage(),
