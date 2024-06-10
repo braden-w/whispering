3414465da97e917a0c54f72d37ee3d5e1f6c4540
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-10 17:02:23 -0400

    refactor: inline some functions
    Former-commit-id: bd96cde88128dd614d36cf3644f2774d839783d8
    Former-commit-id: 1e447e898bae4692a5bd7b375fe06234d66a903a

--- apps/app/src/lib/services/MediaRecorderServiceWebLive.ts
+++ apps/app/src/lib/services/MediaRecorderServiceWebLive.ts
@@ -2,35 +2,6 @@ import AudioRecorder from 'audio-recorder-polyfill';
 import { Effect, Layer } from 'effect';
 import { MediaRecorderError, MediaRecorderService } from './MediaRecorderService';
 
-const getStream = (recordingDeviceId: string) =>
-	Effect.tryPromise({
-		try: () =>
-			navigator.mediaDevices.getUserMedia({
-				audio: { deviceId: { exact: recordingDeviceId } },
-			}),
-		catch: (error) =>
-			new MediaRecorderError({
-				title: 'Error getting media stream',
-				error: error,
-			}),
-	});
-
-const enumerateRecordingDevices = Effect.tryPromise({
-	try: async () => {
-		const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-		const devices = await navigator.mediaDevices.enumerateDevices();
-		stream.getTracks().forEach((track) => track.stop());
-		const audioInputDevices = devices.filter((device) => device.kind === 'audioinput');
-		return audioInputDevices;
-	},
-	catch: (error) =>
-		new MediaRecorderError({
-			title: 'Error enumerating recording devices',
-			description: 'Please make sure you have given permission to access your audio devices',
-			error: error,
-		}),
-});
-
 export const MediaRecorderServiceWebLive = Layer.effect(
 	MediaRecorderService,
 	Effect.gen(function* () {
@@ -45,81 +16,99 @@ export const MediaRecorderServiceWebLive = Layer.effect(
 			mediaRecorder = null;
 		};
 
-		const startRecording = (recordingDeviceId: string) =>
-			Effect.gen(function* () {
-				stream = yield* getStream(recordingDeviceId);
-				recordedChunks.length = 0;
-				mediaRecorder = new AudioRecorder(stream!);
-				(mediaRecorder!.ondataavailable = (event: BlobEvent) => {
-					if (!event.data.size) return;
-					recordedChunks.push(event.data);
-				}),
-					mediaRecorder!.start();
-			});
-
-		const stopRecording = Effect.tryPromise({
-			try: () =>
-				new Promise<Blob>((resolve) => {
-					if (!mediaRecorder) {
-						throw new MediaRecorderError({
-							title: 'Media recorder is not initialized',
-						});
-					}
-					mediaRecorder.addEventListener('stop', () => {
-						const audioBlob = new Blob(recordedChunks, { type: 'audio/wav' });
-						resolve(audioBlob);
-						resetRecorder();
-					});
-					mediaRecorder.stop();
-				}),
-			catch: (error) =>
-				new MediaRecorderError({
-					title: 'Error stopping media recorder and getting audio blob',
-					error: error,
-				}),
-		}).pipe(
-			Effect.catchAll((error) => {
-				resetRecorder();
-				return error;
-			}),
-		);
-
-		const cancelRecording = Effect.tryPromise({
-			try: () =>
-				new Promise<void>((resolve) => {
-					if (!mediaRecorder) {
-						throw new MediaRecorderError({
-							title: 'Media recorder is not initialized',
-						});
-					}
-					mediaRecorder.addEventListener('stop', () => {
-						resetRecorder();
-						resolve();
-					});
-					mediaRecorder.stop();
-				}),
-			catch: (error) =>
-				new MediaRecorderError({
-					title: 'Error stopping media recorder',
-					error: error,
-				}),
-		}).pipe(
-			Effect.catchAll((error) => {
-				resetRecorder();
-				return error;
-			}),
-		);
-
 		return {
 			get recorderState() {
 				if (!mediaRecorder) return 'IDLE';
 				if (mediaRecorder.state === 'recording') return 'RECORDING';
 				return 'IDLE';
 			},
-			enumerateRecordingDevices,
-			startRecording,
-			stopRecording,
-			cancelRecording,
+			enumerateRecordingDevices: Effect.tryPromise({
+				try: async () => {
+					const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+					const devices = await navigator.mediaDevices.enumerateDevices();
+					stream.getTracks().forEach((track) => track.stop());
+					const audioInputDevices = devices.filter((device) => device.kind === 'audioinput');
+					return audioInputDevices;
+				},
+				catch: (error) =>
+					new MediaRecorderError({
+						title: 'Error enumerating recording devices',
+						description: 'Please make sure you have given permission to access your audio devices',
+						error: error,
+					}),
+			}),
+			startRecording: (recordingDeviceId: string) =>
+				Effect.gen(function* () {
+					stream = yield* Effect.tryPromise({
+						try: () =>
+							navigator.mediaDevices.getUserMedia({
+								audio: { deviceId: { exact: recordingDeviceId } },
+							}),
+						catch: (error) =>
+							new MediaRecorderError({
+								title: 'Error getting media stream',
+								error: error,
+							}),
+					});
+					recordedChunks.length = 0;
+					mediaRecorder = new AudioRecorder(stream!);
+					(mediaRecorder!.ondataavailable = (event: BlobEvent) => {
+						if (!event.data.size) return;
+						recordedChunks.push(event.data);
+					}),
+						mediaRecorder!.start();
+				}),
+			stopRecording: Effect.tryPromise({
+				try: () =>
+					new Promise<Blob>((resolve) => {
+						if (!mediaRecorder) {
+							throw new MediaRecorderError({
+								title: 'Media recorder is not initialized',
+							});
+						}
+						mediaRecorder.addEventListener('stop', () => {
+							const audioBlob = new Blob(recordedChunks, { type: 'audio/wav' });
+							resolve(audioBlob);
+							resetRecorder();
+						});
+						mediaRecorder.stop();
+					}),
+				catch: (error) =>
+					new MediaRecorderError({
+						title: 'Error stopping media recorder and getting audio blob',
+						error: error,
+					}),
+			}).pipe(
+				Effect.catchAll((error) => {
+					resetRecorder();
+					return error;
+				}),
+			),
+			cancelRecording: Effect.tryPromise({
+				try: () =>
+					new Promise<void>((resolve) => {
+						if (!mediaRecorder) {
+							throw new MediaRecorderError({
+								title: 'Media recorder is not initialized',
+							});
+						}
+						mediaRecorder.addEventListener('stop', () => {
+							resetRecorder();
+							resolve();
+						});
+						mediaRecorder.stop();
+					}),
+				catch: (error) =>
+					new MediaRecorderError({
+						title: 'Error stopping media recorder',
+						error: error,
+					}),
+			}).pipe(
+				Effect.catchAll((error) => {
+					resetRecorder();
+					return error;
+				}),
+			),
 		};
 	}),
 );
