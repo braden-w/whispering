9ea99e08876669d5c87a7c1796128dacf2f2f9aa
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-11 02:08:04 -0400

    refactor: registerListeners
    Former-commit-id: b250c681358ef51399f6a653d4d20eab8f21c113
    Former-commit-id: 43a9c85cbc6b54e915797b5a03a37d8507dd5ccf

--- apps/extension/background/index.ts
+++ apps/extension/background/index.ts
@@ -1,42 +1,10 @@
-import { Console, Effect } from 'effect';
-import { z } from 'zod';
-import { extensionStorage } from '~lib/services/extension-storage';
-import { recorderStateSchema } from '~lib/recorderStateSchema';
-import { serviceWorkerCommands } from './serviceWorkerCommands';
+import { Effect } from 'effect';
+import {
+	registerOnCommandToggleRecording,
+	registerOnInstallOpenOptionsPage,
+	registerOnMessageExternalToggleRecording,
+} from './registerListeners';
 
-chrome.runtime.onMessageExternal.addListener((requestUnparsed, sender, sendResponse) =>
-	Effect.gen(function* () {
-		yield* Console.info('Received message from external website', requestUnparsed);
-		const { recorderState } = z
-			.object({ recorderState: recorderStateSchema })
-			.parse(requestUnparsed);
-		yield* extensionStorage.set({
-			key: 'whispering-recording-state',
-			value: recorderState,
-		});
-		switch (recorderState) {
-			case 'IDLE':
-				yield* serviceWorkerCommands.setIcon('IDLE');
-				return;
-			case 'RECORDING':
-				yield* serviceWorkerCommands.setIcon('STOP');
-				return;
-		}
-	}).pipe(Effect.runPromise),
-);
-
-chrome.runtime.onInstalled.addListener((details) =>
-	Effect.gen(function* () {
-		if (details.reason !== 'install') return;
-		yield* serviceWorkerCommands.openOptionsPage;
-	}).pipe(Effect.runPromise),
-);
-
-chrome.commands.onCommand.addListener((command) =>
-	Effect.gen(function* () {
-		yield* Console.info('Received command via Chrome Keyboard Shortcut', command);
-		if (command !== 'toggleRecording') return false;
-		yield* serviceWorkerCommands.toggleRecording;
-		return true;
-	}).pipe(Effect.runPromise),
-);
+registerOnInstallOpenOptionsPage.pipe(Effect.runSync);
+registerOnCommandToggleRecording.pipe(Effect.runSync);
+registerOnMessageExternalToggleRecording.pipe(Effect.runSync);
