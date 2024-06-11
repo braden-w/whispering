6b323a1d6a4f436348b44d66bf5a4bf66b1ae273
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-11 18:12:40 -0400

    fix: generics
    Former-commit-id: 9aa292f9f521613da32863bfeb838645c715dc10
    Former-commit-id: a011f2b2accb7222cc5c808888862f52b4a77b04

--- apps/app/src/lib/messaging.ts
+++ apps/app/src/lib/messaging.ts
@@ -1,18 +1,16 @@
-import type {
-	ExternalMessage,
-	Result,
-	WhisperingErrorProperties
-} from '@repo/shared';
+import type { ExternalMessage, Result, WhisperingErrorProperties } from '@repo/shared';
 import { Effect } from 'effect';
 
 const WHISPERING_EXTENSION_ID = 'kiiocjnndmjallnnojknfblenodpbkha';
 
-export const sendMessageToExtension = <M extends ExternalMessage, R>(message: M) =>
+export const sendMessageToExtension = <M extends ExternalMessage, R extends Result<any>>(
+	message: M,
+) =>
 	Effect.async<R, WhisperingErrorProperties>((resume) => {
 		chrome.runtime.sendMessage<ExternalMessage>(
 			WHISPERING_EXTENSION_ID,
 			message,
-			function (response: Result<R>) {
+			function (response: R) {
 				if (!response.isSuccess) {
 					return resume(Effect.fail(response.error));
 				}
