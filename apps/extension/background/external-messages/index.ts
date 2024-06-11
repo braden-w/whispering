7ea2049f9f92987b3ddc53a7a250ca1ffd4882a7
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-11 17:51:37 -0400

    fix: registerExternalListener types and effect pipeline
    Former-commit-id: d8d67040714bc400c19dbb8ae5aec82184348d0d
    Former-commit-id: 0a755d3bd09fe93e3bc6ad4056299afe13c75e37

--- apps/extension/background/external-messages/index.ts
+++ apps/extension/background/external-messages/index.ts
@@ -1,26 +1,32 @@
-import { externalMessageSchema } from '@repo/shared';
+import { externalMessageSchema, type Result } from '@repo/shared';
 import { Console, Effect } from 'effect';
-import { catchErrorsAsToast } from '~lib/errors';
+import { toast } from 'sonner';
 import setClipboardText from './setClipboardText';
 import setRecorderState from './setRecorderState';
 
 export const registerExternalListener = () =>
-	chrome.runtime.onMessageExternal.addListener((requestUnparsed, sender, sendResponse) =>
-		Effect.gen(function* () {
-			yield* Console.info('Received message from external website', requestUnparsed);
-			const externalMessage = externalMessageSchema.parse(requestUnparsed);
-			switch (externalMessage.message) {
-				case 'setRecorderState':
-					const { recorderState } = externalMessage;
-					yield* setRecorderState(recorderState);
-					break;
-				case 'transcription':
-					const { transcription } = externalMessage;
-					const response = yield* setClipboardText(transcription);
-					sendResponse(response);
-					break;
-			}
-		}).pipe(
-			catchErrorsAsToast,
-			Effect.runPromise),
+	chrome.runtime.onMessageExternal.addListener(
+		(requestUnparsed, sender, sendResponse: <R extends Result<any>>(response: R) => void) =>
+			Effect.gen(function* () {
+				yield* Console.info('Received message from external website', requestUnparsed);
+				const externalMessage = externalMessageSchema.parse(requestUnparsed);
+				switch (externalMessage.message) {
+					case 'setRecorderState':
+						const { recorderState } = externalMessage;
+						return yield* setRecorderState(recorderState);
+					case 'transcription':
+						const { transcription } = externalMessage;
+						return yield* setClipboardText(transcription);
+				}
+			}).pipe(
+				Effect.map((result) => ({ isSuccess: true, data: result }) as const),
+				Effect.catchAll((error) => {
+					toast.error(error.title, {
+						description: error.description,
+					});
+					return Effect.succeed({ isSuccess: false, error } as const);
+				}),
+				Effect.map((response) => sendResponse(response)),
+				Effect.runPromise,
+			),
 	);
