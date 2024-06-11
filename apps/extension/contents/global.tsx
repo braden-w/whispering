8be5d6349bc27fde9ff7da15cc0c17f1f7be5990
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-11 01:56:46 -0400

    fix: remove sync listener
    Former-commit-id: 117630fe428cd3cd795687b6fd527795fd9c0c6f
    Former-commit-id: 1b5676ed75cbab278c44416ae93cdd212aa0fb07

--- apps/extension/contents/global.tsx
+++ apps/extension/contents/global.tsx
@@ -1,10 +1,9 @@
 import cssText from 'data-text:~/style.css';
-import { Console, Effect } from 'effect';
+import { Effect } from 'effect';
 import type { PlasmoCSConfig, PlasmoGetStyle } from 'plasmo';
 import { useEffect } from 'react';
 import { Toaster, toast } from 'sonner';
 import { extensionStorage } from '~lib/services/extension-storage';
-import { recorderService } from '~lib/services/recorder';
 
 export const config: PlasmoCSConfig = {
 	matches: ['<all_urls>'],
@@ -16,14 +15,6 @@ export const getStyle: PlasmoGetStyle = () => {
 	style.textContent = cssText;
 	return style;
 };
-
-const syncRecorderStateWithMediaRecorderStateOnLoad = Effect.gen(function* () {
-	const initialRecorderState = recorderService.recorderState;
-	yield* extensionStorage.set({ key: 'whispering-recording-state', value: initialRecorderState });
-	yield* Console.info('Synced recorder state with media recorder state on load', {
-		initialRecorderState,
-	});
-}).pipe(Effect.runPromise);
 
 function ErrorToast() {
 	useEffect(
