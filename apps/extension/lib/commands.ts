2a3b70520442360a7c6a5a49a16b1d862246dc08
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-11 22:04:35 -0400

    chore: remove dead types and lean into plasmo generated types
    Former-commit-id: 2a9e72340aefbb2571646f13c342ac5f58f168af
    Former-commit-id: bd8c3f8cda33651c3f419c054d5747a6b562c122

--- apps/extension/lib/commands.ts
+++ apps/extension/lib/commands.ts
@@ -1,45 +1,5 @@
-import { sendToBackground } from '@plasmohq/messaging';
-import type { Result, WhisperingErrorProperties } from '@repo/shared';
-import { Data, Effect } from 'effect';
-import type { WhisperingMessage } from '~contents/whispering';
-
-type AnyFunction = (...args: any[]) => any;
-type CommandDefinition = Record<string, AnyFunction>;
-
-export type Message<T extends CommandDefinition> = {
-	[K in keyof T]: {
-		commandName: K;
-		args: Parameters<T[K]>;
-	};
-}[keyof T];
-
-export type ExtensionMessage = WhisperingMessage;
-
-type ExtractData<T> = T extends { isSuccess: true; data: infer U } ? U : never;
-type ExtractError<T> = T extends { isSuccess: false; error: infer U } ? U : never;
-
-export const sendToBgsw = <
-	RequestBody,
-	ResponseBody extends Result<any>,
-	TData = ExtractData<ResponseBody>,
-	TError = ExtractError<ResponseBody>,
->(
-	...args: Parameters<typeof sendToBackground<RequestBody, ResponseBody>>
-) =>
-	Effect.tryPromise({
-		try: () => sendToBackground<RequestBody, ResponseBody>(...args),
-		catch: (error) =>
-			new BackgroundServiceWorkerError({
-				title: `Error sending message ${args[0].name} to background service worker`,
-				description: error instanceof Error ? error.message : undefined,
-				error,
-			}),
-	}).pipe(
-		Effect.flatMap(({ data, error }) => {
-			if (error) return Effect.fail(error as TError);
-			return Effect.succeed(data as TData);
-		}),
-	);
+import type { WhisperingErrorProperties } from '@repo/shared';
+import { Data } from 'effect';
 
 export class BackgroundServiceWorkerError extends Data.TaggedError(
 	'BackgroundServiceWorkerError',
