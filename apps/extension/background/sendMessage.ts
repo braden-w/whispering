80ce7f79537eed20a050fdbb5501494babc0f510
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-12 01:11:15 -0400

    feat: handle Option type instead
    Former-commit-id: 5b8dcb02ff2a9502c7989cedf7b505f9b718118c
    Former-commit-id: 0dfcad169e0d8a31178b6f5444f25913e1394779

--- apps/extension/background/sendMessage.ts
+++ apps/extension/background/sendMessage.ts
@@ -1,6 +1,6 @@
 import { Console, Effect, Option } from 'effect';
-import { NoSuchElementException } from 'effect/Cause';
 import type { WhisperingMessage } from '~contents/whispering';
+import { WhisperingError } from '~lib/errors';
 
 export const getWhisperingTabId = Effect.gen(function* () {
 	const tabs = yield* Effect.promise(() => chrome.tabs.query({ url: 'http://localhost:5173/*' }));
@@ -16,9 +16,7 @@ export const getWhisperingTabId = Effect.gen(function* () {
 	}
 	const { id: selectedTabId, discarded: isSelectedTabDiscarded } =
 		tabs.find((tab) => tab.pinned) ?? tabs[0];
-	if (!selectedTabId) {
-		return yield* new NoSuchElementException();
-	}
+	if (!selectedTabId) return undefined;
 	if (isSelectedTabDiscarded) {
 		return yield* Effect.async<number>((resume) => {
 			chrome.tabs.reload(selectedTabId);
@@ -31,11 +29,18 @@ export const getWhisperingTabId = Effect.gen(function* () {
 		});
 	}
 	return selectedTabId;
-}).pipe(Effect.flatMap(Option.fromNullable));
+}).pipe(Effect.map(Option.fromNullable));
 
 export const sendMessageToWhisperingContentScript = <R>(message: WhisperingMessage) =>
 	Effect.gen(function* () {
-		const whisperingTabId = yield* getWhisperingTabId;
+		const maybeWhisperingTabId = yield* getWhisperingTabId;
+		if (Option.isNone(maybeWhisperingTabId)) {
+			yield* new WhisperingError({
+				title: 'Whispering tab not found',
+				description: `Could not find a Whispering tab to call command: ${message.commandName}`,
+			});
+		}
+		const whisperingTabId = maybeWhisperingTabId.value;
 		yield* Console.info('Whispering tab ID:', whisperingTabId);
 		yield* Console.info('Sending message to Whispering content script:', message);
 		const response = yield* Effect.promise(() =>
