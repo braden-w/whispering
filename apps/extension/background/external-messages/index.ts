faff76351a1c60334427dc016a3ea33306ef51cb
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-12 02:30:25 -0400

    refactor: catch and map each tag to WhisperingError
    Former-commit-id: 372e83ca112ab6da1f1a6610e412add9952fa7d3
    Former-commit-id: 325f4d79351dfa6b2fb89865f7d8ef0ca711f671

--- apps/extension/background/external-messages/index.ts
+++ apps/extension/background/external-messages/index.ts
@@ -1,6 +1,6 @@
 import { externalMessageSchema, type Result } from '@repo/shared';
 import { Console, Effect } from 'effect';
-import { renderErrorAsToast } from '~lib/errors';
+import { WhisperingError, renderErrorAsToast } from '~lib/errors';
 import setClipboardText from './setClipboardText';
 import setRecorderState from './setRecorderState';
 
@@ -10,14 +10,33 @@ export const registerExternalListener = () =>
 			Effect.gen(function* () {
 				yield* Console.info('Received message from external website', requestUnparsed);
 				const externalMessage = externalMessageSchema.parse(requestUnparsed);
-				switch (externalMessage.message) {
-					case 'setRecorderState':
-						const { recorderState } = externalMessage;
-						return yield* setRecorderState(recorderState);
-					case 'setClipboardText':
-						const { transcription } = externalMessage;
-						return yield* setClipboardText(transcription);
-				}
+				const processExternalMessageProgram = Effect.gen(function* () {
+					switch (externalMessage.message) {
+						case 'setRecorderState':
+							const { recorderState } = externalMessage;
+							return yield* setRecorderState(recorderState);
+						case 'setClipboardText':
+							const { transcribedText } = externalMessage;
+							return yield* setClipboardText(transcribedText);
+					}
+				}).pipe(
+					Effect.catchTags({
+						GetCurrentTabIdError: () =>
+							new WhisperingError({
+								title: `Unable to get current tab ID while handling "${externalMessage.message}" command from Whispering website`,
+								description:
+									'This might be due to a permissions issue or an unexpected tab state. Please check your Whispering tabs and try again.',
+							}),
+						SetExtensionStorageError: ({ key, value, error }) =>
+							new WhisperingError({
+								title: `Unable to set "${key}" in extension storage while handling "${externalMessage.message}" command from Whispering website`,
+								description:
+									'Please check your local storage settings on the Whispering website and try again.',
+								error: error,
+							}),
+					}),
+				);
+				return yield* processExternalMessageProgram;
 			}).pipe(
 				Effect.map((result) => ({ isSuccess: true, data: result }) as const),
 				Effect.catchAll(renderErrorAsToast),
