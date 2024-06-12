a1b4d155012efeb4346b7022de1d6dda3ef916f0
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-12 01:49:27 -0400

    chore: error handling in handler
    Former-commit-id: 30b3081dfba8e0a30718885b49098c7b6411c246
    Former-commit-id: 760765f407eaf5af8eb8c1cb462d884f4c388ddc

--- apps/extension/background/messages/cancelRecording.ts
+++ apps/extension/background/messages/cancelRecording.ts
@@ -1,7 +1,8 @@
 import type { PlasmoMessaging } from '@plasmohq/messaging';
 import type { Result } from '@repo/shared';
-import { Effect } from 'effect';
+import { Option, Effect } from 'effect';
 import { getWhisperingTabId } from '~background/sendMessage';
+import { WhisperingError } from '~lib/errors';
 
 declare const window: {
 	toggleRecording: () => void;
@@ -14,9 +15,16 @@ export type ResponseBody = Result<true>;
 
 const handler: PlasmoMessaging.MessageHandler<RequestBody, ResponseBody> = (req, res) =>
 	Effect.gen(function* () {
-		const tabId = yield* getWhisperingTabId;
+		const maybeWhisperingTabId = yield* getWhisperingTabId;
+		if (Option.isNone(maybeWhisperingTabId)) {
+			return yield* new WhisperingError({
+				title: 'Whispering tab not found',
+				description: `Could not find a Whispering tab to cancel recording`,
+			});
+		}
+		const whisperingTabId = maybeWhisperingTabId.value;
 		chrome.scripting.executeScript({
-			target: { tabId },
+			target: { tabId: whisperingTabId },
 			world: 'MAIN',
 			func: () => window.cancelRecording(),
 		});
