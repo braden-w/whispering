88b1f8309370b392e2ad6f0321339414639d04dc
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-13 17:52:50 -0400

    fix: inject RecroderStateAsIcon
    Former-commit-id: ec8c763c02d48186f445485d8e3a9531a7549aab
    Former-commit-id: c28e06f5ee91ce70b6b635ef31c24a9f4efba340

--- apps/extension/contents/microphone.tsx
+++ apps/extension/contents/microphone.tsx
@@ -3,6 +3,16 @@ import type { PlasmoCSConfig, PlasmoGetStyle } from 'plasmo';
 import { useStorage } from '@plasmohq/storage/hook';
 import { type RecorderState, recorderStateToIcons } from '@repo/shared';
 
+export const getInlineAnchorList = async () => {
+	const inputs = document.querySelectorAll(
+		"input[type='text'], input[type='search'], input[type='email'], input[type='url'], input[type='tel'], input[type='password'], input[type='number'], input:not([type]), textarea, [contenteditable='true'], [contenteditable='']",
+	);
+	return Array.from(inputs).map((element) => ({
+		element,
+		insertPosition: 'afterend',
+	}));
+};
+
 export const config: PlasmoCSConfig = {
 	matches: ['<all_urls>'],
 	all_frames: true,
