4ecf3b533de21d8b0b169d74f5d0145b77d0edc1
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-13 16:57:11 -0400

    fix: map error for activeTabId
    Former-commit-id: 1318f80a8da29b36d1b4bf633d8fd3e0d0c5febb
    Former-commit-id: 31f902d566613ed2236a210978ce64cf475928b3

--- apps/extension/background/external-messages/playSound.ts
+++ apps/extension/background/external-messages/playSound.ts
@@ -1,26 +1,34 @@
 import { Console, Effect } from 'effect';
-import stopSoundSrc from '/assets/sound_ex_machina_Button_Blip.mp3';
-import startSoundSrc from '/assets/zapsplat_household_alarm_clock_button_press_12967.mp3';
-import cancelSoundSrc from '/assets/zapsplat_multimedia_click_button_short_sharp_73510.mp3';
-
-const startSound = new Audio(startSoundSrc);
-const stopSound = new Audio(stopSoundSrc);
-const cancelSound = new Audio(cancelSoundSrc);
+import { getActiveTabId } from '~background/messages/getActiveTabId';
+import { WhisperingError } from '~lib/errors';
 
 const handler = (sound: 'start' | 'stop' | 'cancel') =>
 	Effect.gen(function* () {
 		yield* Console.info('Playing sound', sound);
-		switch (sound) {
-			case 'start':
-				startSound.play();
-				break;
-			case 'stop':
-				stopSound.play();
-				break;
-			case 'cancel':
-				cancelSound.play();
-				break;
-		}
+		const activeTabId = yield* getActiveTabId.pipe(
+			Effect.mapError(
+				(error) =>
+					new WhisperingError({
+						title: 'Failed to get active tab ID',
+						description: 'Failed to get active tab ID to play sound',
+						error,
+					}),
+			),
+		);
+		yield* Effect.tryPromise({
+			try: () =>
+				chrome.tabs.sendMessage(activeTabId, {
+					message: 'playSound',
+					sound,
+				}),
+			catch: (error) =>
+				new WhisperingError({
+					title: `Failed to play sound ${sound}`,
+					description: `Failed to play sound ${sound} in active tab ${activeTabId}`,
+					error,
+				}),
+		});
+
 		return true;
 	});
 
