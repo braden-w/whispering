33b374100013681876c05b7edf256f32737d3458
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-14 09:52:05 -0400

    refactor: utils for contents
    Former-commit-id: 202a002c4b505c9c22d25560a1b81178df02803d
    Former-commit-id: 6d623c507fa50f783e87c35ac0edc9808025f189

--- apps/extension/contents/microphone.tsx
+++ apps/extension/contents/microphone.tsx
@@ -1,11 +1,8 @@
-import { sendToBackground } from '@plasmohq/messaging';
 import { useStorage } from '@plasmohq/storage/hook';
 import { recorderStateToIcons, type RecorderState } from '@repo/shared';
 import cssText from 'data-text:~/style.css';
-import { Effect } from 'effect';
 import type { PlasmoCSConfig, PlasmoGetInlineAnchor, PlasmoGetStyle } from 'plasmo';
-import { WhisperingError, renderErrorAsToast } from '~lib/errors';
-import type * as ToggleRecording from '../background/messages/toggleRecording';
+import { buttonVariants, toggleRecordingFromContentScript } from './utils';
 
 export const getInlineAnchor: PlasmoGetInlineAnchor = async () => {
 	const element = document.querySelector('#prompt-textarea')?.closest('div');
@@ -26,27 +23,18 @@ export const getStyle: PlasmoGetStyle = () => {
 	return style;
 };
 
-const toggleRecording = () =>
-	Effect.tryPromise({
-		try: () =>
-			sendToBackground<ToggleRecording.RequestBody, ToggleRecording.ResponseBody>({
-				name: 'toggleRecording',
-			}),
-		catch: (error) =>
-			new WhisperingError({
-				title: `Unable to toggle recording via background service worker`,
-				description:
-					error instanceof Error
-						? error.message
-						: 'There was likely an issue sending the message to the background service worker from the popup.',
-				error,
-			}),
-	}).pipe(Effect.catchAll(renderErrorAsToast), Effect.runPromise);
-
 function RecorderStateAsIcon() {
 	const [recorderState] = useStorage<RecorderState>('whispering-recording-state');
 	const recorderStateAsIcon = recorderStateToIcons[recorderState ?? 'IDLE'];
-	return <button onClick={toggleRecording}>{recorderStateAsIcon}</button>;
+	return (
+		<button
+			// className="ring-offset-background hover:bg-accent hover:text-accent-foreground focus-visible:ring-ring inline-flex h-10 w-10 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+			className={buttonVariants({ variant: 'ghost', size: 'icon' })}
+			onClick={toggleRecordingFromContentScript}
+		>
+			{recorderStateAsIcon}
+		</button>
+	);
 }
 
 export default RecorderStateAsIcon;
