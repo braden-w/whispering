08ed86b406768ca24ee5a096de0e150d4e1c7d59
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-13 23:36:54 -0400

    feat: working inline logic for chatgpt website
    Former-commit-id: 30e20016048990f3662334d2f14de9f8a1731ebf
    Former-commit-id: b462f8af624c8d18731c166e310af1381c5910e3

--- apps/extension/contents/microphone.tsx
+++ apps/extension/contents/microphone.tsx
@@ -1,18 +1,18 @@
-import type { PlasmoGetOverlayAnchorList } from 'plasmo';
 import { sendToBackground } from '@plasmohq/messaging';
 import { useStorage } from '@plasmohq/storage/hook';
 import { recorderStateToIcons, type RecorderState } from '@repo/shared';
 import cssText from 'data-text:~/style.css';
 import { Effect } from 'effect';
-import type { PlasmoCSConfig, PlasmoGetStyle, PlasmoWatchOverlayAnchor } from 'plasmo';
+import type { PlasmoCSConfig, PlasmoGetInlineAnchor, PlasmoGetStyle } from 'plasmo';
 import { WhisperingError, renderErrorAsToast } from '~lib/errors';
 import type * as ToggleRecording from '../background/messages/toggleRecording';
 
-export const getOverlayAnchorList: PlasmoGetOverlayAnchorList = async () => {
-	const inputs = document.querySelectorAll(
-		"input[type='text'], input[type='search'], input[type='email'], input[type='url'], input[type='tel'], input[type='password'], input[type='number'], input:not([type]), textarea, [contenteditable='true'], [contenteditable='']",
-	);
-	return inputs;
+export const getInlineAnchor: PlasmoGetInlineAnchor = async () => {
+	const element = document.querySelector('#prompt-textarea')?.closest('div');
+	return {
+		element,
+		insertPosition: 'afterend',
+	};
 };
 
 export const config: PlasmoCSConfig = {
@@ -24,17 +24,6 @@ export const getStyle: PlasmoGetStyle = () => {
 	const style = document.createElement('style');
 	style.textContent = cssText;
 	return style;
-};
-
-export const watchOverlayAnchor: PlasmoWatchOverlayAnchor = (updatePosition) => {
-	const interval = setInterval(() => {
-		updatePosition();
-	}, 420);
-
-	// Clear the interval when unmounted
-	return () => {
-		clearInterval(interval);
-	};
 };
 
 const toggleRecording = () =>
@@ -54,37 +43,10 @@ const toggleRecording = () =>
 			}),
 	}).pipe(Effect.catchAll(renderErrorAsToast), Effect.runPromise);
 
-function DraggableIcon() {
-	const [position, setPosition] = useState({ x: 0, y: 0 });
-	useEffect(() => {
-		// Load saved position from localStorage
-		const savedPosition = localStorage.getItem('buttonPosition');
-		if (savedPosition) {
-			setPosition(JSON.parse(savedPosition));
-		}
-	}, []);
-	return (
-		<Draggable
-			position={position}
-			onStop={(e, data) => {
-				const newPosition = { x: data.x, y: data.y };
-				setPosition(newPosition);
-				// Save position to localStorage
-				localStorage.setItem('buttonPosition', JSON.stringify(newPosition));
-			}}
-		>
-			<RecorderStateAsIcon />
-		</Draggable>
-	);
-}
 function RecorderStateAsIcon() {
 	const [recorderState] = useStorage<RecorderState>('whispering-recording-state');
 	const recorderStateAsIcon = recorderStateToIcons[recorderState ?? 'IDLE'];
-	return (
-		<button className="inset-y absolute right-8" onClick={toggleRecording}>
-			{recorderStateAsIcon}
-		</button>
-	);
+	return <button onClick={toggleRecording}>{recorderStateAsIcon}</button>;
 }
 
 export default RecorderStateAsIcon;
