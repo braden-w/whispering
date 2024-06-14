a0aaac5d659994ab10b5c82d5bbaf2438bf5e8d9
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-14 16:03:50 -0400

    fix: onLoadSendWhisperingLoadedMessageProgram
    Former-commit-id: 6e7edd4dfc979fd1323507cc646cb45445ea1a96
    Former-commit-id: 94a582351cc6d1a98b9ff5b1b31bd6973a790fd5

--- apps/extension/contents/whispering/index.ts
+++ apps/extension/contents/whispering/index.ts
@@ -1,8 +1,11 @@
+import { sendToBackground } from '@plasmohq/messaging';
 import type { Result } from '@repo/shared';
 import { Console, Effect } from 'effect';
 import type { PlasmoCSConfig } from 'plasmo';
 import { toast } from 'sonner';
 import { z } from 'zod';
+import * as GetActiveTabId from '~background/messages/getActiveTabId';
+import { WhisperingError } from '@repo/shared';
 import { settingsSchema } from '~lib/services/local-storage';
 import getSettings from './getSettings';
 import setSettings from './setSettings';
@@ -42,3 +45,40 @@ chrome.runtime.onMessage.addListener(
 			Effect.runPromise,
 		),
 );
+
+const onLoadSendWhisperingLoadedMessageProgram = Effect.gen(function* () {
+	const activeTabIdResult = yield* Effect.tryPromise({
+		try: () =>
+			sendToBackground<GetActiveTabId.RequestBody, GetActiveTabId.ResponseBody>({
+				name: 'getActiveTabId',
+			}),
+		catch: (error) =>
+			new WhisperingError({
+				title: 'Unable to get active tab ID',
+				description:
+					'An error occurred while trying to get the active tab ID. The whisperingTabContentReady message will not be sent to the active tab.',
+				error,
+			}),
+	});
+	if (!activeTabIdResult.isSuccess) {
+		return;
+	}
+	const activeTabId = activeTabIdResult.data;
+	yield* Effect.tryPromise({
+		try: () =>
+			sendToBackground({
+				name: 'whisperingTabContentReady',
+				body: { tabId: activeTabId },
+			}),
+		catch: (error) =>
+			new WhisperingError({
+				title: `Unable to send whisperingTabContentReady message to active tab`,
+				description:
+					error instanceof Error
+						? error.message
+						: 'There was likely an issue sending the whisperingTabContentReady message to the active tab.',
+				error,
+			}),
+	});
+});
+onLoadSendWhisperingLoadedMessageProgram.pipe(Effect.runPromise);
