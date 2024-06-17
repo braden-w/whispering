70febc96f51601e6118d0300849e76453799787b
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-17 13:17:02 -0400

    feat: working injection of extension button by all inputs
    Former-commit-id: 5ce1d87ddb91d4e21a43ba3faccfc144dcc0af20
    Former-commit-id: b5ac3415d47fb5f122624e64762792612779ffb0

--- apps/extension/contents/microphone.tsx
+++ apps/extension/contents/microphone.tsx
@@ -1,17 +1,36 @@
 import { useStorage } from '@plasmohq/storage/hook';
-import type { RecorderState } from '@repo/shared';
+import { recorderStateToIcons, type RecorderState } from '@repo/shared';
 import cssText from 'data-text:~/style.css';
-import { EllipsisIcon, MicIcon, SquareIcon } from 'lucide-react';
-import type { PlasmoCSConfig, PlasmoGetInlineAnchor, PlasmoGetStyle } from 'plasmo';
+import type {
+	PlasmoCSConfig,
+	PlasmoGetInlineAnchorList,
+	PlasmoGetStyle,
+	PlasmoMountShadowHost,
+} from 'plasmo';
 import { cn } from '~lib/utils';
-import { buttonVariants, toggleRecordingFromContentScript } from './utils';
+import { toggleRecordingFromContentScript } from './utils';
 
-export const getInlineAnchor: PlasmoGetInlineAnchor = async () => {
-	const element = document.querySelector('#prompt-textarea')?.closest('div');
-	return {
+export const getInlineAnchorList: PlasmoGetInlineAnchorList = async () => {
+	const inputs = document.querySelectorAll(
+		"input[type='text'], input[type='search'], input[type='email'], input[type='url'], input[type='tel'], input[type='password'], input[type='number'], input:not([type]), textarea, [contenteditable='true'], [contenteditable='']",
+	);
+	return Array.from(inputs).map((element) => ({
 		element,
 		insertPosition: 'afterend',
-	};
+	}));
+};
+
+export const mountShadowHost: PlasmoMountShadowHost = ({ shadowHost, anchor, mountState }) => {
+	const wrapper = document.createElement('div');
+	wrapper.style.display = 'flex';
+	wrapper.style.alignItems = 'center';
+
+	anchor.element.style.width = '100%';
+
+	anchor?.element?.parentNode?.insertBefore(wrapper, anchor.element);
+	wrapper.appendChild(anchor?.element);
+	wrapper.appendChild(shadowHost);
+	mountState.observer.disconnect(); // OPTIONAL DEMO: stop the observer as needed
 };
 
 export const config: PlasmoCSConfig = {
@@ -25,18 +44,17 @@ export const getStyle: PlasmoGetStyle = () => {
 	return style;
 };
 
-const recorderStateToIcons = {
-	RECORDING: <SquareIcon />,
-	LOADING: <EllipsisIcon />,
-	IDLE: <MicIcon />,
-} as const satisfies Record<RecorderState, React.JSX.Element>;
-
 function RecorderStateAsIcon() {
 	const [recorderState] = useStorage<RecorderState>('whispering-recording-state', 'IDLE');
 	const recorderStateAsIcon = recorderStateToIcons[recorderState];
 	return (
 		<button
-			className={cn(buttonVariants({ variant: 'ghost', size: 'icon' }), 'rounded-full')}
+			className={cn(
+				// buttonVariants({ size: 'icon' }),
+				// 'mb-1 mr-1 flex h-8 w-8 items-center justify-center rounded-full bg-black text-white transition-colors hover:opacity-70 focus-visible:outline-none focus-visible:outline-black disabled:bg-[#D7D7D7] disabled:text-[#f4f4f4] disabled:hover:opacity-100 dark:bg-white dark:text-black dark:focus-visible:outline-white',
+				'h-9 w-9',
+				'rounded-md p-1 text-gray-500 transition-colors hover:bg-gray-100 dark:text-white dark:hover:bg-gray-900 md:bottom-3 md:p-2',
+			)}
 			onClick={toggleRecordingFromContentScript}
 		>
 			{recorderStateAsIcon}
