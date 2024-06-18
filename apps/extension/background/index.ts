aad996e8f5b1bf0f8153159d283385607b072363
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-18 17:31:59 -0400

    fix: provide ToastServieLive
    Former-commit-id: 0b82563d252c27262e7570bc707e3db9eed554ea
    Former-commit-id: 5d3f612a85e314146a7f82d5fefa10d948a871a7

--- apps/extension/background/index.ts
+++ apps/extension/background/index.ts
@@ -3,12 +3,17 @@ import { renderErrorAsToast } from '~lib/errors';
 import { registerExternalListener } from './external-messages';
 import { openOptionsPage } from './messages/openOptionsPage';
 import { toggleRecording } from './messages/toggleRecording';
+import { ToastServiceBgswLive } from '~lib/services/ToastServiceBgswLive';
 
 chrome.runtime.onInstalled.addListener((details) =>
 	Effect.gen(function* () {
 		if (details.reason !== 'install') return;
 		yield* openOptionsPage;
-	}).pipe(Effect.catchAll(renderErrorAsToast('bgsw')), Effect.runPromise),
+	}).pipe(
+		Effect.catchAll(renderErrorAsToast),
+		Effect.provide(ToastServiceBgswLive),
+		Effect.runPromise,
+	),
 );
 
 chrome.commands.onCommand.addListener((command) =>
@@ -16,8 +21,11 @@ chrome.commands.onCommand.addListener((command) =>
 		yield* Console.info('Received command via Chrome Keyboard Shortcut', command);
 		if (command !== 'toggleRecording') return false;
 		yield* toggleRecording;
-		return true;
-	}).pipe(Effect.catchAll(renderErrorAsToast('bgsw')), Effect.runPromise),
+	}).pipe(
+		Effect.catchAll(renderErrorAsToast),
+		Effect.provide(ToastServiceBgswLive),
+		Effect.runPromise,
+	),
 );
 
 registerExternalListener();
