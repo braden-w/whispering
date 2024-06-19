2d9abfed11d5e51d527b029763be7456b08e57bf
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-18 23:11:52 -0400

    fix: conditional vercel adapter
    Former-commit-id: d85b5094eae7a8e9c015154ae5b793b20179c879
    Former-commit-id: e34e962620aedad98b06d91d9f03153ec8087ad4

--- apps/app/svelte.config.js
+++ apps/app/svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
+import vercelAdapter from '@sveltejs/adapter-vercel';
+import staticAdapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +12,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: !!process.env.VERCEL ? vercelAdapter() : staticAdapter(),
 	},
 };
 
