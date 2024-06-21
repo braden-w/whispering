d8e2b9228f7d58c7b9f02761eda8eab3f9d9a627
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-21 16:54:04 -0400

    feat: tauri tray that clicks
    Former-commit-id: 3d6ab40f9ea7e82674042610d1736e8d78f411a4
    Former-commit-id: b257527d87d7ffedcd972e43dbdff44282653365

--- apps/app/src-tauri/src/main.rs
+++ apps/app/src-tauri/src/main.rs
@@ -1,8 +1,40 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{CustomMenuItem, Manager, SystemTrayMenuItem};
+use tauri::{SystemTray, SystemTrayEvent, SystemTrayMenu};
+
 fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
+
     tauri::Builder::default()
+        .system_tray(SystemTray::new().with_menu(tray_menu))
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                app.emit_all("toggle-recording", ()).unwrap();
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "hide" => {
+                    let window = app.get_window("main").unwrap();
+                    window.hide().unwrap();
+                }
+                _ => {}
+            },
+            _ => {}
+        })
         .invoke_handler(tauri::generate_handler![write_text])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
