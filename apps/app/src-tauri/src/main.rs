b10a17798372a46ce488c834b7e2bd62683f0765
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-21 16:56:47 -0400

    chore: no more hide button for now, app must be on top
    Former-commit-id: 92ef29cfba8484c8bc27d3804f3af7b2c79a3f52
    Former-commit-id: e1c34ea68a9d68e7aeaab4c6b8cb967b4c045388

--- apps/app/src-tauri/src/main.rs
+++ apps/app/src-tauri/src/main.rs
@@ -1,17 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{CustomMenuItem, Manager, SystemTrayMenuItem};
+use tauri::{CustomMenuItem, Manager};
 use tauri::{SystemTray, SystemTrayEvent, SystemTrayMenu};
 
 fn main() {
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
 
-    let tray_menu = SystemTrayMenu::new()
-        .add_item(quit)
-        .add_native_item(SystemTrayMenuItem::Separator)
-        .add_item(hide);
+    let tray_menu = SystemTrayMenu::new().add_item(quit);
 
     tauri::Builder::default()
         .system_tray(SystemTray::new().with_menu(tray_menu))
@@ -26,10 +22,6 @@ fn main() {
             SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
                 "quit" => {
                     std::process::exit(0);
-                }
-                "hide" => {
-                    let window = app.get_window("main").unwrap();
-                    window.hide().unwrap();
                 }
                 _ => {}
             },
