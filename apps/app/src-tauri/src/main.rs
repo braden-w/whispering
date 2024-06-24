620e80c87fd6d6650403e61136d7315d9acf1aeb
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-24 00:28:30 -0400

    feat: rust command set_tray_icon
    Former-commit-id: 20ea4577a008ea6f37ec13f497d705ac520af66a
    Former-commit-id: 92a8a105780ce2c3f914a06e9b1fc563cd928973

--- apps/app/src-tauri/src/main.rs
+++ apps/app/src-tauri/src/main.rs
@@ -27,7 +27,7 @@ fn main() {
             },
             _ => {}
         })
-        .invoke_handler(tauri::generate_handler![write_text])
+        .invoke_handler(tauri::generate_handler![write_text, set_tray_icon])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -38,4 +38,19 @@ use enigo::{Enigo, Keyboard, Settings};
 fn write_text(text: String) -> Result<(), String> {
     let mut enigo = Enigo::new(&Settings::default()).unwrap();
     enigo.text(&text).map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+async fn set_tray_icon(recorder_state: String, app_handle: tauri::AppHandle) -> Result<(), String> {
+    let icon = match recorder_state.as_str() {
+        "IDLE" => include_bytes!("../icons/recorder_state/studio_microphone.png").to_vec(),
+        "RECORDING" => include_bytes!("../icons/recorder_state/red_large_square.png").to_vec(),
+        "LOADING" => include_bytes!("../icons/recorder_state/arrows_counterclockwise.png").to_vec(),
+        _ => return Err("Invalid state. Must be IDLE, RECORDING, or LOADING.".to_string()),
+    };
+    app_handle
+        .tray_handle()
+        .set_icon(tauri::Icon::Raw(icon))
+        .unwrap();
+    Ok(())
 }
