fcc8afc22b4e5720238fee44729c7803188ead43
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-24 03:20:13 -0400

    rename: env.VERCEL
    Former-commit-id: add862acc1cb7f6acf686996e37830d40b12ad0d
    Former-commit-id: 0b7fb478e1b4654386531de32b819606f5dd4f2f

--- apps/app/svelte.config.js
+++ apps/app/svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: !!process.env.VERCEL_PROJECT_ID ? vercelAdapter() : staticAdapter(),
+		adapter: !!process.env.VERCEL ? vercelAdapter() : staticAdapter(),
 	},
 };
 
