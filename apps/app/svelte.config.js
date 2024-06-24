ce71673573d69682f4aab8dc20b7be9e5a02c9d6
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-24 15:42:25 -0400

    fix: conditional deployment with tauri and isNative This extends the pnpm tauri command to be equivalent to BUILD_TARGET=native tauri
    Former-commit-id: 00a79ef15288fbd60763a4461bb306f768ed61b2
    Former-commit-id: 88edc2d5a56602f0f2a8d2d52035dcff24c611a5

--- apps/app/svelte.config.js
+++ apps/app/svelte.config.js
@@ -2,7 +2,7 @@ import vercelAdapter from '@sveltejs/adapter-vercel';
 import staticAdapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const isInVercelEnvironment = !!process.env.IS_VERCEL;
+const isNative = process.env.BUILD_TARGET === 'native';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,7 +14,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: isInVercelEnvironment ? vercelAdapter() : staticAdapter(),
+		adapter: isNative ? staticAdapter() : vercelAdapter(),
 	},
 };
 
