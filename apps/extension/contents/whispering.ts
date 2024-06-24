96f324853d5bc2899f502bd73366ea173a3d3d12
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-06-24 13:06:39 -0400

    fix: include relays for production at localhost:4173
    Former-commit-id: 69ba0f41ca26f3068d98af099c353207f8bb6a5a
    Former-commit-id: bf5356fa27eea69e33c96cd471cd9120982c4b67

--- apps/extension/contents/whispering.ts
+++ apps/extension/contents/whispering.ts
@@ -2,7 +2,11 @@ import { relayMessage } from '@plasmohq/messaging';
 import type { PlasmoCSConfig } from 'plasmo';
 
 export const config: PlasmoCSConfig = {
-	matches: ['https://whispering.bradenwong.com/*', 'http://localhost:5173/*'],
+	matches: [
+		'https://whispering.bradenwong.com/*',
+		'http://localhost:5173/*',
+		'http://localhost:4173/*',
+	],
 };
 
 relayMessage({ name: 'external/notifications/clear' });
