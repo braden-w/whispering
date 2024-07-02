a8b8870d17b4485f1b9420746e46fd8e7833d51e
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-07-02 09:38:37 -0700

    docs: revamped create persisted state docs
    Former-commit-id: e4195d553308eb7062b358c58f018e434a406c8f
    Former-commit-id: acaa755a430a59c4b7d83d32fa52539c83a5dd49

--- apps/app/src/lib/utils/createPersistedState.svelte.ts
+++ apps/app/src/lib/utils/createPersistedState.svelte.ts
@@ -2,14 +2,7 @@ import { Schema as S } from '@effect/schema';
 import { Option } from 'effect';
 
 /**
- * Creates a persisted state tied to local storage.
- * @param {Object} params - The parameters for creating the persisted state.
- * @param {string} params.key - The key used to store the value in local storage.
- * @param {Schema} params.schema - The schema used to validate the value.
- * @param {S.Schema.Type<Schema>} params.defaultValue - The default value to use if no value is found in local storage.
- * @param {boolean} [params.disableLocalStorage=false] - If true, disables the use of local storage.
- * @returns {Object} The persisted state.
- * @returns {S.Schema.Type<Schema>} value - The reactive value of the persisted state.
+ * Creates a persisted state object tied to local storage, accessible through `.value`
  */
 export function createPersistedState<TSchema extends S.Schema.AnyNoContext>({
 	key,
@@ -17,9 +10,32 @@ export function createPersistedState<TSchema extends S.Schema.AnyNoContext>({
 	defaultValue,
 	disableLocalStorage = false,
 }: {
+	/** The key used to store the value in local storage. */
 	key: string;
+	/**
+	 * The schema is used to validate the value from local storage
+	 * (`defaultValue` will be used if the value from local storage is invalid).
+	 * */
 	schema: TSchema;
+	/**
+	 * The default value to use if no value is found in local storage or the value
+	 * from local storage fails to pass the schema.
+	 * */
 	defaultValue: S.Schema.Type<TSchema>;
+	/**
+	 * If true, disables the use of local storage. In SvelteKit, you set
+	 * this to `!browser` because local storage doesn't exist in the server
+	 * context.
+	 *
+	 * @example
+	 * 
+	 * ```ts
+	 * import { browser } from '$app/environment';
+	 * ...
+	 * const state = createPersistedState({ ..., disableLocalStorage: !browser })
+	 * ...
+	 * ```
+	 * */
 	disableLocalStorage?: boolean;
 }) {
 	let value = $state(defaultValue);
