e0e1ad5c5dcad5c9de4a8d4ed7eb49911eadef8d
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-07-03 22:40:02 -0700

    feat: Formatting and such
    Former-commit-id: b496f1a02c32503fa3efffd886b9d890caf53b28
    Former-commit-id: 8913583ac27f2a41327234653160c9f34b149571

--- apps/app/src/routes/(config)/macos-enable-accessibility/+page.svelte
+++ apps/app/src/routes/(config)/macos-enable-accessibility/+page.svelte
@@ -9,32 +9,38 @@
 </svelte:head>
 
 <main class="flex flex-1 items-center justify-center">
-	<section class="container flex flex-col items-center gap-4 text-center">
+	<section class="container flex max-w-4xl flex-col items-center gap-4 text-center">
 		<h1 class="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">MacOS Accessibility</h1>
-		<p class="text-muted-foreground lg:text-xl">
-			Follow the steps below to re-enable Whispering in your MacOS Accessibility settings.
+		<p class="text-muted-foreground max-w-2xl text-left lg:text-xl">
+			Follow the steps below to re-enable Whispering in your MacOS Accessibility settings. This
+			often is needed after installing a new version of Whispering to get pasting to work, as
+			detailed in this this <Button
+				variant="link"
+				size="inline"
+				href="https://github.com/enigo-rs/enigo/issues/174"
+				target="_blank"
+				rel="noopener noreferrer">Github issue</Button
+			>.
 		</p>
-		<div class="flex max-w-2xl flex-col gap-2">
+		<div class="flex flex-col items-center gap-2">
 			<video
+				class="max-w-md"
 				src="https://github.com/braden-w/whispering/raw/feat/add-accessibility-troubleshooting/images/macos_enable_accessibility.mp4"
 				autoplay
 				loop
 				controls
 			></video>
-
-			<ol class="text-muted-foreground list-inside list-decimal space-y-2 text-left lg:text-xl">
+			<ol
+				class="text-muted-foreground max-w-2xl list-inside list-decimal space-y-2 text-left lg:text-xl"
+			>
 				<li>
-					Go to
-					<span
+					Go to <span
 						class="text-primary whitespace-normal px-1 text-base font-semibold tracking-normal lg:text-xl"
 					>
 						System Preferences > Privacy & Security > Accessibility
-					</span>
+					</span> or use the button below.
 				</li>
-				<Button onclick={() => invoke('open_apple_accessibility')} variant="default" size="lg">
-					<SettingsIcon class="mr-2 h-6 w-6" />
-					Open MacOS Accessibility
-				</Button>
+
 				<li>
 					Click on <span class="text-primary font-semibold tracking-tight">🎙️ Whispering</span> and remove
 					it using the minus icon (-).
@@ -45,7 +51,10 @@
 					>
 				</li>
 			</ol>
+			<Button onclick={() => invoke('open_apple_accessibility')} variant="default" class="w-full">
+				<SettingsIcon class="mr-2 h-6 w-6" />
+				Open MacOS Accessibility Settings
+			</Button>
 		</div>
-		<Button onclick={() => window.history.back()} variant="link">Go back</Button>
 	</section>
 </main>
