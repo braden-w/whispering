d60204dcf38efd56f750b511c01cf4c17cc763b4
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-08-05 17:49:42 -0700

    feat: grid view of settings
    Former-commit-id: 3dedf46d2b186b94bdb389082e7151600399da95
    Former-commit-id: 7602c91f2aac94c680f4546779d01194b4f33921

--- apps/app/src/routes/(config)/settings/+page.svelte
+++ apps/app/src/routes/(config)/settings/+page.svelte
@@ -89,219 +89,221 @@
 		</p>
 	</div>
 	<Separator class="my-6" />
-	<Card.Root class="w-full max-w-xl">
-		<Card.Header>
-			<Card.Title class="text-xl">General</Card.Title>
-		</Card.Header>
-		<Card.Content class="space-y-6">
-			<div class="flex items-center gap-2">
-				<Switch
-					id="play-sound-enabled"
-					aria-labelledby="play-sound-enabled"
-					bind:checked={settings.isPlaySoundEnabled}
-				/>
-				<Label for="play-sound-enabled">Play sound on toggle on and off</Label>
-			</div>
-			<div class="flex items-center gap-2">
-				<Switch
-					id="copy-to-clipboard"
-					aria-labelledby="copy-to-clipboard"
-					bind:checked={settings.isCopyToClipboardEnabled}
-				/>
-				<Label for="copy-to-clipboard">Copy text to clipboard on successful transcription</Label>
-			</div>
-			<div class="flex items-center gap-2">
-				<Switch
-					id="paste-from-clipboard"
-					aria-labelledby="paste-from-clipboard"
-					bind:checked={settings.isPasteContentsOnSuccessEnabled}
-				/>
-				<Label for="paste-from-clipboard">
-					Paste contents from clipboard after successful transcription
-				</Label>
-			</div>
-		</Card.Content>
-	</Card.Root>
-	<Card.Root class="w-full max-w-xl">
-		<Card.Header>
-			<Card.Title class="text-xl">Transcription</Card.Title>
-		</Card.Header>
-		<Card.Content class="space-y-6">
-			<div class="grid gap-2">
-				<SettingsLabelSelect
-					id="selected-transcription-service"
-					label="Transcription Service"
-					items={TRANSCRIPTION_SERVICE_OPTIONS}
-					selected={selectedTranscriptionServiceOption}
-					onSelectedChange={(selected) => {
-						if (!selected) return;
-						settings.selectedTranscriptionService = selected.value;
-					}}
-					placeholder="Select a transcription service"
-				/>
-			</div>
-			{#if settings.selectedTranscriptionService === 'OpenAI'}
-				<div class="grid gap-2">
-					<SettingsLabelInput
-						id="openai-api-key"
-						label="OpenAI API Key"
-						placeholder="Your OpenAI API Key"
-						bind:value={settings.openAiApiKey}
+	<div class="place-content-center gap-6 sm:grid lg:grid-cols-2">
+		<Card.Root class="w-full max-w-xl">
+			<Card.Header>
+				<Card.Title class="text-xl">General</Card.Title>
+			</Card.Header>
+			<Card.Content class="space-y-6">
+				<div class="flex items-center gap-2">
+					<Switch
+						id="play-sound-enabled"
+						aria-labelledby="play-sound-enabled"
+						bind:checked={settings.isPlaySoundEnabled}
 					/>
-					<div class="text-muted-foreground text-sm">
-						You can find your API key in your <Button
-							variant="link"
-							class="px-0.3 py-0.2 h-fit"
-							href="https://platform.openai.com/api-keys"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							account settings
-						</Button>. Make sure <Button
-							variant="link"
-							class="px-0.3 py-0.2 h-fit"
-							href="https://platform.openai.com/settings/organization/billing/overview"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							billing
-						</Button>
-						is enabled.
-					</div>
+					<Label for="play-sound-enabled">Play sound on toggle on and off</Label>
 				</div>
-			{:else if settings.selectedTranscriptionService === 'Groq'}
-				<div class="grid gap-2">
-					<SettingsLabelInput
-						id="groq-api-key"
-						label="Groq API Key"
-						placeholder="Your Groq API Key"
-						bind:value={settings.groqApiKey}
+				<div class="flex items-center gap-2">
+					<Switch
+						id="copy-to-clipboard"
+						aria-labelledby="copy-to-clipboard"
+						bind:checked={settings.isCopyToClipboardEnabled}
 					/>
-					<div class="text-muted-foreground text-sm">
-						You can find your Groq API key in your <Button
-							variant="link"
-							class="px-0.3 py-0.2 h-fit"
-							href="https://console.groq.com/keys"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							Groq console
-						</Button>.
-					</div>
+					<Label for="copy-to-clipboard">Copy text to clipboard on successful transcription</Label>
 				</div>
-			{/if}
-			<div class="grid gap-2">
-				<SettingsLabelSelect
-					id="output-language"
-					label="Output Language"
-					items={SUPPORTED_LANGUAGES_OPTIONS}
-					selected={selectedLanguageOption}
-					onSelectedChange={(selected) => {
-						if (!selected) return;
-						settings.outputLanguage = selected.value;
-					}}
-					placeholder="Select a language"
-				/>
-			</div>
-		</Card.Content>
-	</Card.Root>
-	<Card.Root class="w-full max-w-xl">
-		<Card.Header>
-			<Card.Title class="text-xl">Recording</Card.Title>
-		</Card.Header>
-		<Card.Content class="space-y-6"
-			><div class="grid gap-2">
-				{#await getMediaDevicesPromise}
-					<SettingsLabelSelect
-						id="recording-device"
-						label="Recording Device"
-						placeholder="Loading devices..."
-						disabled
+				<div class="flex items-center gap-2">
+					<Switch
+						id="paste-from-clipboard"
+						aria-labelledby="paste-from-clipboard"
+						bind:checked={settings.isPasteContentsOnSuccessEnabled}
 					/>
-				{:then mediaDevices}
-					{@const items = mediaDevices.map((device) => ({
-						value: device.deviceId,
-						label: device.label,
-					}))}
+					<Label for="paste-from-clipboard">
+						Paste contents from clipboard after successful transcription
+					</Label>
+				</div>
+			</Card.Content>
+		</Card.Root>
+		<Card.Root class="w-full max-w-xl">
+			<Card.Header>
+				<Card.Title class="text-xl">Transcription</Card.Title>
+			</Card.Header>
+			<Card.Content class="space-y-6">
+				<div class="grid gap-2">
 					<SettingsLabelSelect
-						id="recording-device"
-						label="Recording Device"
-						{items}
-						selected={items.find((item) => item.value === settings.selectedAudioInputDeviceId)}
+						id="selected-transcription-service"
+						label="Transcription Service"
+						items={TRANSCRIPTION_SERVICE_OPTIONS}
+						selected={selectedTranscriptionServiceOption}
 						onSelectedChange={(selected) => {
 							if (!selected) return;
-							settings.selectedAudioInputDeviceId = selected.value;
-							mediaStreamManager.refreshStream().pipe(Effect.runPromise);
+							settings.selectedTranscriptionService = selected.value;
 						}}
-						placeholder="Select a device"
+						placeholder="Select a transcription service"
 					/>
-				{:catch error}
-					<p>Error with listing media devices: {error.message}</p>
-				{/await}
-			</div>
-			<div class="grid gap-2">
-				<SettingsLabelSelect
-					id="bit-rate"
-					label="Bitrate"
-					items={BITRATE_OPTIONS}
-					selected={BITRATE_OPTIONS.find((option) => option.value === settings.bitsPerSecond)}
-					onSelectedChange={(selected) => {
-						if (!selected) return;
-						settings.bitsPerSecond = selected.value;
-					}}
-					placeholder="Select a bitrate"
-				/>
-			</div>
-		</Card.Content>
-		<Card.Footer>
-			<Button onclick={() => window.history.back()} class="w-full" variant="secondary">
-				Go Back
-			</Button>
-		</Card.Footer>
-	</Card.Root>
-	<Card.Root class="w-full max-w-xl">
-		<Card.Header>
-			<Card.Title class="text-xl">Shortcuts</Card.Title>
-		</Card.Header>
-		<Card.Content class="space-y-6">
-			<div class="grid gap-2">
-				<SettingsLabelInput
-					id="local-shortcut"
-					label="Local Shortcut"
-					placeholder="Local Shortcut to toggle recording"
-					bind:value={settings.currentLocalShortcut}
-				/>
-			</div>
-			<div class="grid gap-2">
-				{#if settings.isGlobalShortcutEnabled}
-					<SettingsLabelInput
-						id="global-shortcut"
-						label="Global Shortcut"
-						placeholder="Global Shortcut to toggle recording"
-						bind:value={settings.currentGlobalShortcut}
-					/>
-				{:else}
-					<Label class="text-sm" for="global-shortcut">Global Shortcut</Label>
-					<div class="relative">
-						<Input
-							id="global-shortcut"
-							placeholder="Global Shortcut to toggle recording"
-							bind:value={settings.currentGlobalShortcut}
-							type="text"
-							autocomplete="off"
-							disabled
+				</div>
+				{#if settings.selectedTranscriptionService === 'OpenAI'}
+					<div class="grid gap-2">
+						<SettingsLabelInput
+							id="openai-api-key"
+							label="OpenAI API Key"
+							placeholder="Your OpenAI API Key"
+							bind:value={settings.openAiApiKey}
 						/>
-						<Button class="absolute inset-0 backdrop-blur" href="/global-shortcut" variant="link">
-							Enable Global Shortcut
-						</Button>
+						<div class="text-muted-foreground text-sm">
+							You can find your API key in your <Button
+								variant="link"
+								class="px-0.3 py-0.2 h-fit"
+								href="https://platform.openai.com/api-keys"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								account settings
+							</Button>. Make sure <Button
+								variant="link"
+								class="px-0.3 py-0.2 h-fit"
+								href="https://platform.openai.com/settings/organization/billing/overview"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								billing
+							</Button>
+							is enabled.
+						</div>
+					</div>
+				{:else if settings.selectedTranscriptionService === 'Groq'}
+					<div class="grid gap-2">
+						<SettingsLabelInput
+							id="groq-api-key"
+							label="Groq API Key"
+							placeholder="Your Groq API Key"
+							bind:value={settings.groqApiKey}
+						/>
+						<div class="text-muted-foreground text-sm">
+							You can find your Groq API key in your <Button
+								variant="link"
+								class="px-0.3 py-0.2 h-fit"
+								href="https://console.groq.com/keys"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								Groq console
+							</Button>.
+						</div>
 					</div>
 				{/if}
-			</div>
-		</Card.Content>
-		<Card.Footer>
-			<Button onclick={() => window.history.back()} class="w-full" variant="secondary">
-				Go Back
-			</Button>
-		</Card.Footer>
-	</Card.Root>
+				<div class="grid gap-2">
+					<SettingsLabelSelect
+						id="output-language"
+						label="Output Language"
+						items={SUPPORTED_LANGUAGES_OPTIONS}
+						selected={selectedLanguageOption}
+						onSelectedChange={(selected) => {
+							if (!selected) return;
+							settings.outputLanguage = selected.value;
+						}}
+						placeholder="Select a language"
+					/>
+				</div>
+			</Card.Content>
+		</Card.Root>
+		<Card.Root class="w-full max-w-xl">
+			<Card.Header>
+				<Card.Title class="text-xl">Recording</Card.Title>
+			</Card.Header>
+			<Card.Content class="space-y-6"
+				><div class="grid gap-2">
+					{#await getMediaDevicesPromise}
+						<SettingsLabelSelect
+							id="recording-device"
+							label="Recording Device"
+							placeholder="Loading devices..."
+							disabled
+						/>
+					{:then mediaDevices}
+						{@const items = mediaDevices.map((device) => ({
+							value: device.deviceId,
+							label: device.label,
+						}))}
+						<SettingsLabelSelect
+							id="recording-device"
+							label="Recording Device"
+							{items}
+							selected={items.find((item) => item.value === settings.selectedAudioInputDeviceId)}
+							onSelectedChange={(selected) => {
+								if (!selected) return;
+								settings.selectedAudioInputDeviceId = selected.value;
+								mediaStreamManager.refreshStream().pipe(Effect.runPromise);
+							}}
+							placeholder="Select a device"
+						/>
+					{:catch error}
+						<p>Error with listing media devices: {error.message}</p>
+					{/await}
+				</div>
+				<div class="grid gap-2">
+					<SettingsLabelSelect
+						id="bit-rate"
+						label="Bitrate"
+						items={BITRATE_OPTIONS}
+						selected={BITRATE_OPTIONS.find((option) => option.value === settings.bitsPerSecond)}
+						onSelectedChange={(selected) => {
+							if (!selected) return;
+							settings.bitsPerSecond = selected.value;
+						}}
+						placeholder="Select a bitrate"
+					/>
+				</div>
+			</Card.Content>
+			<Card.Footer>
+				<Button onclick={() => window.history.back()} class="w-full" variant="secondary">
+					Go Back
+				</Button>
+			</Card.Footer>
+		</Card.Root>
+		<Card.Root class="w-full max-w-xl">
+			<Card.Header>
+				<Card.Title class="text-xl">Shortcuts</Card.Title>
+			</Card.Header>
+			<Card.Content class="space-y-6">
+				<div class="grid gap-2">
+					<SettingsLabelInput
+						id="local-shortcut"
+						label="Local Shortcut"
+						placeholder="Local Shortcut to toggle recording"
+						bind:value={settings.currentLocalShortcut}
+					/>
+				</div>
+				<div class="grid gap-2">
+					{#if settings.isGlobalShortcutEnabled}
+						<SettingsLabelInput
+							id="global-shortcut"
+							label="Global Shortcut"
+							placeholder="Global Shortcut to toggle recording"
+							bind:value={settings.currentGlobalShortcut}
+						/>
+					{:else}
+						<Label class="text-sm" for="global-shortcut">Global Shortcut</Label>
+						<div class="relative">
+							<Input
+								id="global-shortcut"
+								placeholder="Global Shortcut to toggle recording"
+								bind:value={settings.currentGlobalShortcut}
+								type="text"
+								autocomplete="off"
+								disabled
+							/>
+							<Button class="absolute inset-0 backdrop-blur" href="/global-shortcut" variant="link">
+								Enable Global Shortcut
+							</Button>
+						</div>
+					{/if}
+				</div>
+			</Card.Content>
+			<Card.Footer>
+				<Button onclick={() => window.history.back()} class="w-full" variant="secondary">
+					Go Back
+				</Button>
+			</Card.Footer>
+		</Card.Root>
+	</div>
 </main>
