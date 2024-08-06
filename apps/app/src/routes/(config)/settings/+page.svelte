fc0db1a839c87fb9fa2217ed0e75580828c9cb42
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-08-05 21:00:30 -0700

    chore: imports
    Former-commit-id: f2a727ad7301513a168bdd7fd6c34818828fc9e6
    Former-commit-id: f14120a172f8f942df8ef0185c134af0f7f99ca9

--- apps/app/src/routes/(config)/settings/+page.svelte
+++ apps/app/src/routes/(config)/settings/+page.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
-	import * as Card from '$lib/components/ui/card';
 </script>
 
 <svelte:head>
 	<title>Settings - Whispering</title>
 </svelte:head>
-
-
