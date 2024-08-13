39e4a87a50187d011317519a30d19995c0b9bd72
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-08-13 10:45:34 -0400

    feat: make whisper server work in both browser and desktop contexts
    Former-commit-id: 8de593bf58b9d85f5ff374529d91f17d403c2a99
    Former-commit-id: 389a3b6ccc2fc75038414ec2ff99c3761064e186

--- apps/app/src/lib/services/TranscriptionServiceFasterWhisperServerLive.ts
+++ apps/app/src/lib/services/TranscriptionServiceFasterWhisperServerLive.ts
@@ -1,5 +1,6 @@
 import { settings } from '$lib/stores/settings.svelte.js';
 import { getExtensionFromAudioBlob } from '$lib/utils';
+import { HttpClient, HttpClientRequest, HttpClientResponse } from '@effect/platform';
 import { Schema } from '@effect/schema';
 import { TranscriptionService, WhisperingError } from '@repo/shared';
 import { Body, fetch, ResponseType } from '@tauri-apps/api/http';
@@ -13,6 +14,85 @@ export const TranscriptionServiceFasterWhisperServerLive = Layer.succeed(
 		transcribe: (audioBlob) =>
 			Effect.gen(function* () {
 				const { outputLanguage, fasterWhisperServerUrl, fasterWhisperServerModel } = settings.value;
+
+				const postBodyToTranscriptionServer = (formData: FormData) => {
+					if (!window.__TAURI__)
+						return HttpClientRequest.post(
+							'https://api.groq.com/openai/v1/audio/transcriptions',
+						).pipe(
+							HttpClientRequest.formDataBody(formData),
+							HttpClient.fetch,
+							Effect.andThen(
+								HttpClientResponse.schemaBodyJson(
+									Schema.Union(
+										Schema.Struct({
+											text: Schema.String,
+										}),
+										Schema.Struct({
+											error: Schema.Struct({
+												message: Schema.String,
+											}),
+										}),
+									),
+								),
+							),
+							Effect.scoped,
+							Effect.mapError(
+								(error) =>
+									new WhisperingError({
+										title: 'Error transcribing audio',
+										description: error.message,
+										error,
+									}),
+							),
+						);
+					return Effect.gen(function* () {
+						const formBody = Body.form(formData);
+						const response = yield* Effect.tryPromise({
+							try: () =>
+								fetch(`${fasterWhisperServerUrl}/v1/audio/transcriptions`, {
+									method: 'POST',
+									body: formBody,
+									responseType: ResponseType.JSON,
+									headers: { 'Content-Type': 'multipart/form-data' },
+								}),
+							catch: (error) =>
+								new WhisperingError({
+									title: 'Request to Transcription Server Failed',
+									description: `An error occurred while sending the request to the transcription server. ${error instanceof Error ? error.message : 'Please try again later.'}`,
+									error,
+								}),
+						});
+						if (!response.ok) {
+							return yield* new WhisperingError({
+								title: 'Transcription Server Response Error',
+								description: `The server responded with an error: ${response.status}. Please verify the server status or try again later.`,
+							});
+						}
+						const data = yield* Schema.decodeUnknown(
+							Schema.Union(
+								Schema.Struct({
+									text: Schema.String,
+								}),
+								Schema.Struct({
+									error: Schema.Struct({
+										message: Schema.String,
+									}),
+								}),
+							),
+						)(response.data).pipe(
+							Effect.mapError(
+								(error) =>
+									new WhisperingError({
+										title: 'Unable to parse transcription server response',
+										description: `Failed to parse the response from the transcription server. ${error instanceof Error ? error.message : 'Please try again.'}`,
+										error,
+									}),
+							),
+						);
+						return data;
+					});
+				};
 
 				const blobSizeInMb = audioBlob.size / (1024 * 1024);
 				if (blobSizeInMb > MAX_FILE_SIZE_MB) {
@@ -30,49 +110,7 @@ export const TranscriptionServiceFasterWhisperServerLive = Layer.succeed(
 				formData.append('file', formDataFile);
 				formData.append('model', fasterWhisperServerModel);
 				if (outputLanguage !== 'auto') formData.append('language', outputLanguage);
-				const formBody = Body.form(formData);
-				const response = yield* Effect.tryPromise({
-					try: () =>
-						fetch(`${fasterWhisperServerUrl}/v1/audio/transcriptions`, {
-							method: 'POST',
-							body: formBody,
-							responseType: ResponseType.JSON,
-							headers: { 'Content-Type': 'multipart/form-data' },
-						}),
-					catch: (error) =>
-						new WhisperingError({
-							title: 'Request to Transcription Server Failed',
-							description: `An error occurred while sending the request to the transcription server. ${error instanceof Error ? error.message : 'Please try again later.'}`,
-							error,
-						}),
-				});
-				if (!response.ok) {
-					return yield* new WhisperingError({
-						title: 'Transcription Server Response Error',
-						description: `The server responded with an error: ${response.status}. Please verify the server status or try again later.`,
-					});
-				}
-				const data = yield* Schema.decodeUnknown(
-					Schema.Union(
-						Schema.Struct({
-							text: Schema.String,
-						}),
-						Schema.Struct({
-							error: Schema.Struct({
-								message: Schema.String,
-							}),
-						}),
-					),
-				)(response.data).pipe(
-					Effect.mapError(
-						(error) =>
-							new WhisperingError({
-								title: 'Unable to parse transcription server response',
-								description: `Failed to parse the response from the transcription server. ${error instanceof Error ? error.message : 'Please try again.'}`,
-								error,
-							}),
-					),
-				);
+				const data = yield* postBodyToTranscriptionServer(formData);
 				if ('error' in data) {
 					return yield* new WhisperingError({
 						title: 'Server Error from faster-whisper-server',
