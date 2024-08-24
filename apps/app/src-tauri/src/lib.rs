299d86b79a95a4ec2a53e215a6c2aeb6257ca50f
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-08-23 23:19:36 -0400

    fix: add clipboard_manager
    Former-commit-id: df2585d3c314326c5721c51c7b1dff6117b15cbf
    Former-commit-id: 4d886ebb2347ac23ad67df13776290c8fc5197f3

--- apps/app/src-tauri/src/lib.rs
+++ apps/app/src-tauri/src/lib.rs
@@ -10,6 +10,7 @@ use accessibility::{is_macos_accessibility_enabled, open_apple_accessibility};
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let builder = tauri::Builder::default()
+        .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_notification::init())
