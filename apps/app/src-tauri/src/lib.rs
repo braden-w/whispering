7b2404072e57ce63d944fa59225b3efc0a2527e0
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-08-27 12:04:15 -0400

    chore: scaffold sql module
    Former-commit-id: 36ac3fb35c064e21f1c7e4d57c882e46cb943e5d
    Former-commit-id: 751f4b7be455ebc09139decc0b92ba5f83c8538e

--- apps/app/src-tauri/src/lib.rs
+++ apps/app/src-tauri/src/lib.rs
@@ -16,6 +16,7 @@ pub fn run() {
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_sql::Builder::default().build())
         .plugin(tauri_plugin_window_state::Builder::default().build());
     #[cfg(target_os = "macos")]
     let builder = builder.invoke_handler(tauri::generate_handler![
