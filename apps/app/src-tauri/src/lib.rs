3edc32cce7b2d56f0938160d502cfe5b7a181abd
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-08-27 12:03:19 -0400

    fix: fs module now works
    Former-commit-id: c8462e256ce66948b44774f2c7d3d2464cc7369b
    Former-commit-id: 1b97e1e8e0d89650ac021fd42339911f8fc6a19c

--- apps/app/src-tauri/src/lib.rs
+++ apps/app/src-tauri/src/lib.rs
@@ -10,12 +10,13 @@ use accessibility::{is_macos_accessibility_enabled, open_apple_accessibility};
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let builder = tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_window_state::Builder::default().build())
-        .plugin(tauri_plugin_global_shortcut::Builder::new().build());
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_window_state::Builder::default().build());
     #[cfg(target_os = "macos")]
     let builder = builder.invoke_handler(tauri::generate_handler![
         write_text,
