c15a8978a5788eae1f1b766b8e9e2ceaf4939dbb
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-17 15:54:35 -0800

    major refactor: move DownloadService away from effect's dependency injection in favor of vanilla typescript approach
    Former-commit-id: 1277fb23227d881b7656ab5ea950c00823931095
    Former-commit-id: 52a6db8d5017e7d3d41b53082e651dfb6c881b98

--- apps/app/src/lib/services/index.ts
+++ apps/app/src/lib/services/index.ts
@@ -1,8 +1,6 @@
 import { Layer } from 'effect';
 import { ClipboardServiceDesktopLive } from './ClipboardServiceDesktopLive';
 import { ClipboardServiceWebLive } from './ClipboardServiceWebLive';
-import { DownloadServiceDesktopLive } from './DownloadServiceDesktopLive';
-import { DownloadServiceWebLive } from './DownloadServiceWebLive';
 import { HttpServiceDesktopLive } from './HttpServiceDesktopLive';
 import { HttpServiceWebLive } from './HttpServiceWebLive';
 import { NotificationServiceDesktopLive } from './NotificationServiceDesktopLive';
@@ -12,9 +10,6 @@ export const MainLive = Layer.mergeAll(
 	window.__TAURI_INTERNALS__
 		? ClipboardServiceDesktopLive
 		: ClipboardServiceWebLive,
-	window.__TAURI_INTERNALS__
-		? DownloadServiceDesktopLive
-		: DownloadServiceWebLive,
 	window.__TAURI_INTERNALS__
 		? NotificationServiceDesktopLive
 		: NotificationServiceWebLive,
