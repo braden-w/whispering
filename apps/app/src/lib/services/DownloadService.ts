e2cd9fd335eb1adee1aecfa4b9416882faf25d01
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-17 15:54:35 -0800

    major refactor: move DownloadService away from effect's dependency injection in favor of vanilla typescript approach
    Former-commit-id: c1be5c02130531c6bcfccf9b2a529ef2db529d1c
    Former-commit-id: 4e3b97bc3e143d122a9d6ed41044917badba62e8

--- apps/app/src/lib/services/DownloadService.ts
+++ apps/app/src/lib/services/DownloadService.ts
@@ -1,13 +1,69 @@
-import type { WhisperingError } from '@repo/shared';
-import type { Effect } from 'effect';
-import { Context } from 'effect';
+import { getExtensionFromAudioBlob } from '$lib/utils';
+import { type Result, tryAsync, trySync } from '@repo/shared';
+import { save } from '@tauri-apps/plugin-dialog';
+import { writeFile } from '@tauri-apps/plugin-fs';
 
-export class DownloadService extends Context.Tag('DownloadService')<
-	DownloadService,
-	{
-		readonly downloadBlob: (config: {
-			name: string;
-			blob: Blob;
-		}) => Effect.Effect<void, WhisperingError>;
-	}
->() {}
+type DownloadService = {
+	readonly downloadBlob: (config: {
+		name: string;
+		blob: Blob;
+	}) => Promise<Result<void>> | Result<void>;
+};
+
+export const DownloadService = window.__TAURI_INTERNALS__
+	? createDownloadServiceDesktopLive()
+	: createDownloadServiceWebLive();
+
+function createDownloadServiceDesktopLive(): DownloadService {
+	return {
+		downloadBlob: async ({ name, blob }) => {
+			const extension = getExtensionFromAudioBlob(blob);
+			return await tryAsync({
+				try: async () => {
+					const path = await save({
+						filters: [{ name, extensions: [extension] }],
+					});
+					if (path === null) return;
+					const contents = new Uint8Array(await blob.arrayBuffer());
+					return writeFile(path, contents);
+				},
+				catch: (error) => ({
+					_tag: 'WhisperingError',
+					title: 'Error saving recording',
+					description:
+						'There was an error saving the recording using the Tauri Filesystem API. Please try again.',
+					action: { type: 'more-details', error },
+				}),
+			});
+		},
+	};
+}
+
+function createDownloadServiceWebLive(): DownloadService {
+	return {
+		downloadBlob: ({ name, blob }) =>
+			trySync({
+				try: () => {
+					const file = new File([blob], name, { type: blob.type });
+					const url = URL.createObjectURL(file);
+					const a = document.createElement('a');
+					a.href = url;
+					a.download = name;
+					document.body.appendChild(a);
+					a.click();
+					document.body.removeChild(a);
+					URL.revokeObjectURL(url);
+				},
+				catch: (error) => ({
+					_tag: 'WhisperingError',
+					title: 'Error saving recording',
+					description:
+						'There was an error saving the recording in your browser. Please try again.',
+					action: {
+						type: 'more-details',
+						error,
+					},
+				}),
+			}),
+	};
+}
