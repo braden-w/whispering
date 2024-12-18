b74e36f8b08ddb09b79b7e089250873d9fd59078
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-18 12:56:24 -0800

    chore: use latest shadcn version
    Former-commit-id: c90c561b4f33f3a72bb883af1f881ddb81bc717f
    Former-commit-id: 1c1377033e69712aa673466659a8ed432f98b593

--- apps/extension/components/ui/button.tsx
+++ apps/extension/components/ui/button.tsx
@@ -1,10 +1,10 @@
+import { cn } from '@/lib/utils';
 import { Slot } from '@radix-ui/react-slot';
 import { type VariantProps, cva } from 'class-variance-authority';
 import * as React from 'react';
-import { cn } from '~lib/utils';
 
 const buttonVariants = cva(
-	'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+	'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
 	{
 		variants: {
 			variant: {
--- apps/extension/tailwind.config.js
+++ apps/extension/tailwind.config.js
@@ -1,5 +1,3 @@
-const { fontFamily } = require('tailwindcss/defaultTheme');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: ['class'],
@@ -9,13 +7,6 @@ module.exports = {
 		'./*.{ts,tsx}',
 	],
 	theme: {
-		container: {
-			center: true,
-			padding: '2rem',
-			screens: {
-				'2xl': '1400px',
-			},
-		},
 		extend: {
 			colors: {
 				border: 'hsl(var(--border))',
@@ -56,23 +47,6 @@ module.exports = {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)',
-			},
-			fontFamily: {
-				sans: ['var(--font-sans)', ...fontFamily.sans],
-			},
-			keyframes: {
-				'accordion-down': {
-					from: { height: '0' },
-					to: { height: 'var(--radix-accordion-content-height)' },
-				},
-				'accordion-up': {
-					from: { height: 'var(--radix-accordion-content-height)' },
-					to: { height: '0' },
-				},
-			},
-			animation: {
-				'accordion-down': 'accordion-down 0.2s ease-out',
-				'accordion-up': 'accordion-up 0.2s ease-out',
 			},
 		},
 	},
