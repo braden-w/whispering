a0f87d36dbd0709a37efb41eef3a251c9e0a2a18
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-18 11:40:35 -0800

    refactor: can remove problematic watch since it's not used in favor of useStorage
    Former-commit-id: 6de0537550cf6c9eae6e1da22086fabea2226213
    Former-commit-id: f767922319e27468ca97f5932cfc12189f022fcc

--- apps/extension/lib/services/extension-storage.ts
+++ apps/extension/lib/services/extension-storage.ts
@@ -32,64 +32,25 @@ export const SHARED_EXTENSION_STATE_KEYS = {
 
 const storage = new Storage();
 
-const createSetWatch = <TSchema extends z.ZodSchema, A = z.infer<TSchema>>({
+const createSetter = <TSchema extends z.ZodSchema, A = z.infer<TSchema>>({
 	key,
 	schema,
 }: {
 	key: string;
 	schema: TSchema;
 }) => {
-	const parseValueFromStorage = (valueFromStorage: unknown) => {
-		const parseJsonResult = parseJson(valueFromStorage);
-		if (!parseJsonResult.ok) {
-			return parseJsonResult;
-		}
-		const valueFromStorageMaybeInvalid = parseJsonResult.data;
-		const parseResult = schema.safeParse(valueFromStorageMaybeInvalid);
-		if (!parseResult.success) {
-			return Err({
-				_tag: 'WhisperingError',
-				title: 'Unable to parse storage value',
-				description:
-					'There was an error running Schema.decodeUnknown on the storage value.',
-				action: {
-					type: 'more-details',
-					error: parseResult.error,
-				},
-			});
-		}
-		return Ok(parseResult.data);
-	};
-
-	return {
-		set: (value: A) => storage.set(key, value),
-		watch: (callback: (newValue: A) => void) => {
-			const listener = (({ newValue: newValueUnparsed }) => {
-				const parseResult = parseValueFromStorage(newValueUnparsed);
-				if (!parseResult.ok) return parseResult;
-				const newValue: A = parseResult.data;
-				console.info('watch', key, newValue);
-				callback(newValue);
-				return Ok(undefined);
-			}) satisfies StorageWatchCallback;
-
-			storage.watch({
-				[key]: (...props) => {
-					renderErrorAsNotification(listener(...props));
-				},
-			});
-		},
-	};
+	return { set: (value: A) => storage.set(key, value) };
 };
 
 export const extensionStorageService = {
-	[SHARED_EXTENSION_STATE_KEYS.RECORDER_STATE]: createSetWatch({
+	[SHARED_EXTENSION_STATE_KEYS.RECORDER_STATE]: createSetter({
 		key: SHARED_EXTENSION_STATE_KEYS.RECORDER_STATE,
 		schema: recorderStateSchema,
 	}),
-	[SHARED_EXTENSION_STATE_KEYS.LATEST_RECORDING_TRANSCRIBED_TEXT]:
-		createSetWatch({
+	[SHARED_EXTENSION_STATE_KEYS.LATEST_RECORDING_TRANSCRIBED_TEXT]: createSetter(
+		{
 			key: SHARED_EXTENSION_STATE_KEYS.LATEST_RECORDING_TRANSCRIBED_TEXT,
 			schema: z.string(),
-		}),
+		},
+	),
 } as const;
