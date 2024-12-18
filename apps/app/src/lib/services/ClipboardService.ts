24acaec47a0fb1bc8ef8eeee88eb908571f450d4
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-17 16:39:23 -0800

    major refactor: move ClipboardService away from effect's dependency injection in favor of vanilla typescript approach
    Former-commit-id: b2c395f1db1dcd2f4edbd12d6094f67db57f9a04
    Former-commit-id: 36c2fb051c2fd48c48104e9614cc7daa0a80d3c9

--- apps/app/src/lib/services/ClipboardService.ts
+++ apps/app/src/lib/services/ClipboardService.ts
@@ -1,25 +1,24 @@
-import type { WhisperingError } from '@repo/shared';
-import type { Effect } from 'effect';
-import { Context } from 'effect';
+import type { Result } from '@repo/shared';
+import { createClipboardServiceDesktopLive } from './ClipboardServiceDesktopLive';
+import { createClipboardServiceWebLive } from './ClipboardServiceWebLive';
 
-export class ClipboardService extends Context.Tag('ClipboardService')<
-	ClipboardService,
-	{
-		/**
-		 * Writes text to the user's clipboard.
-		 * @param text The text to write to the clipboard.
-		 */
-		readonly setClipboardText: (
-			text: string,
-		) => Effect.Effect<void, WhisperingError>;
-		/**
-		 * Pastes text from the user's clipboard.
-		 * - Web: No need to implement this function.
-		 * - Desktop: This function should trigger a paste action, as if the user had pressed `Ctrl` + `V`.
-		 * - Mobile: This function should trigger a paste action, as if the user had pressed `Paste` in the context menu.
-		 */
-		readonly writeTextToCursor: (
-			text: string,
-		) => Effect.Effect<void, WhisperingError>;
-	}
->() {}
+export type ClipboardService = {
+	/**
+	 * Writes text to the user's clipboard.
+	 * @param text The text to write to the clipboard.
+	 */
+	readonly setClipboardText: (text: string) => Promise<Result<void>>;
+	/**
+	 * Pastes text from the user's clipboard.
+	 * - Web: No need to implement this function.
+	 * - Desktop: This function should trigger a paste action, as if the user had pressed `Ctrl` + `V`.
+	 * - Mobile: This function should trigger a paste action, as if the user had pressed `Paste` in the context menu.
+	 */
+	readonly writeTextToCursor: (
+		text: string,
+	) => Promise<Result<void>> | Result<void>;
+};
+
+export const ClipboardService = window.__TAURI_INTERNALS__
+	? createClipboardServiceDesktopLive()
+	: createClipboardServiceWebLive();
