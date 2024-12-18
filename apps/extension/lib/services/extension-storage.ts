438f009c6cd608b55bc3f3d2fd14e8a31bba9032
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-17 23:59:51 -0800

    wip feat: watcher for extension
    Former-commit-id: 5a3f6abb780f7bf70a137fb1afab09d1feba5063
    Former-commit-id: b067ffde80c0d22e43675785ff3d959f9ae78511

--- apps/extension/lib/services/extension-storage.ts
+++ apps/extension/lib/services/extension-storage.ts
@@ -1,10 +1,10 @@
 import { Schema } from '@effect/schema';
-import type { ParseError } from '@effect/schema/ParseResult';
 import { Storage, type StorageWatchCallback } from '@plasmohq/storage';
-import { WhisperingError, recorderStateSchema } from '@repo/shared';
+import { Err, Ok, parseJson, recorderStateSchema } from '@repo/shared';
 import { Console, Effect } from 'effect';
 import { renderErrorAsNotification } from '~lib/errors';
 import { NotificationServiceBgswLive } from './NotificationServiceBgswLive';
+import type { z } from 'zod';
 
 export const STORAGE_KEYS = {
 	RECORDER_STATE: 'whispering-recorder-state',
@@ -15,47 +15,52 @@ export const STORAGE_KEYS = {
 
 const storage = new Storage();
 
-const createSetWatch = <
-	TSchema extends Schema.Schema.AnyNoContext,
-	A = Schema.Schema.Type<TSchema>,
->({
+const createSetWatch = <TSchema extends z.ZodSchema, A = z.infer<TSchema>>({
 	key,
 	schema,
 }: {
 	key: string;
 	schema: TSchema;
 }) => {
-	const parseValueFromStorage = (
-		valueFromStorage: unknown,
-	): Effect.Effect<A, ParseError> =>
-		Schema.decodeUnknown(Schema.parseJson(schema))(valueFromStorage);
+	const parseValueFromStorage = (valueFromStorage: unknown) => {
+		const parseJsonResult = parseJson(valueFromStorage);
+		if (!parseJsonResult.ok) {
+			return parseJsonResult;
+		}
+		const valueFromStorageMaybeInvalid = parseJsonResult.data;
+		const parseResult = schema.safeParse(valueFromStorageMaybeInvalid);
+		if (!parseResult.success) {
+			return Err({
+				_tag: 'WhisperingError',
+				title: 'Unable to parse storage value',
+				description:
+					'There was an error running Schema.decodeUnknown on the storage value.',
+				action: {
+					type: 'more-details',
+					error: parseResult.error,
+				},
+			});
+		}
+		return Ok(parseResult.data);
+	};
+
 	return {
 		set: (value: A) => Effect.promise(() => storage.set(key, value)),
 		watch: (callback: (newValue: A) => void) => {
-			const listener: StorageWatchCallback = ({ newValue: newValueUnparsed }) =>
-				Effect.gen(function* () {
-					const newValue: A = yield* parseValueFromStorage(
-						newValueUnparsed,
-					).pipe(
-						Effect.mapError((error) => ({
-							_tag: 'WhisperingError',
-							title: 'Unable to parse storage value',
-							description:
-								'There was an error running Schema.decodeUnknown on the storage value.',
-							action: {
-								type: 'more-details',
-								error,
-							},
-						})),
-					);
-					yield* Console.info('watch', key, newValue);
-					callback(newValue);
-				}).pipe(
-					Effect.catchAll(renderErrorAsNotification),
-					Effect.provide(NotificationServiceBgswLive),
-					Effect.runSync,
-				);
-			return Effect.sync(() => storage.watch({ [key]: listener }));
+			const listener = (({ newValue: newValueUnparsed }) => {
+				const parseResult = parseValueFromStorage(newValueUnparsed);
+				if (!parseResult.ok) return parseResult;
+				const newValue: A = parseResult.data;
+				console.info('watch', key, newValue);
+				callback(newValue);
+				return Ok(undefined);
+			}) satisfies StorageWatchCallback;
+
+			storage.watch({
+				[key]: (...props) => {
+					renderErrorAsNotification(listener(...props));
+				},
+			});
 		},
 	};
 };
