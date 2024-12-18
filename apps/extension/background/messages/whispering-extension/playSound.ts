db6683c6339abaa053a776135106b66274c506a6
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-18 01:26:15 -0800

    refactor: playSound
    Former-commit-id: 66b98680f5711a1486618828ea6f00d68df39964
    Former-commit-id: 07890b69c1c2deec1c495d2cb31e12b99685bcd8

--- apps/extension/background/messages/whispering-extension/playSound.ts
+++ apps/extension/background/messages/whispering-extension/playSound.ts
@@ -1,44 +1,12 @@
 import type { PlasmoMessaging } from '@plasmohq/messaging';
 import {
+	Err,
 	type ExternalMessageBody,
 	type ExternalMessageReturnType,
+	Ok,
 	type Result,
-	WhisperingError,
-	effectToResult,
 } from '@repo/shared';
-import { Console, Effect } from 'effect';
-import { renderErrorAsNotification } from '~lib/errors';
 import { getActiveTabId } from '~lib/getActiveTabId';
-import { NotificationServiceBgswLive } from '~lib/services/NotificationServiceBgswLive';
-
-const playSound = (sound: 'start' | 'stop' | 'cancel') =>
-	Effect.gen(function* () {
-		yield* Console.info('Playing sound', sound);
-		const activeTabId = yield* getActiveTabId.pipe(
-			Effect.mapError((error) => ({
-				_tag: 'WhisperingError',
-				title: 'Failed to get active tab ID',
-				description: 'Failed to get active tab ID to play sound',
-				action: { type: 'more-details', error },
-			})),
-		);
-		yield* Effect.tryPromise({
-			try: () =>
-				chrome.tabs.sendMessage(activeTabId, {
-					message: 'playSound',
-					sound,
-				}),
-			catch: (error) => ({
-				_tag: 'WhisperingError',
-				title: `Failed to play ${sound} sound`,
-				description: `Failed to play ${sound} sound in active tab ${activeTabId}`,
-				action: { type: 'more-details', error },
-			}),
-		});
-	}).pipe(
-		// Silently catch playSound errors and log them to the console instead of render them as toast
-		Effect.catchAll(Console.error),
-	);
 
 export type RequestBody = ExternalMessageBody<'whispering-extension/playSound'>;
 
@@ -46,27 +14,57 @@ export type ResponseBody = Result<
 	ExternalMessageReturnType<'whispering-extension/playSound'>
 >;
 
-const handler: PlasmoMessaging.MessageHandler<RequestBody, ResponseBody> = (
-	{ body },
-	res,
-) =>
-	Effect.gen(function* () {
-		if (!body?.sound) {
-			return yield* {
+const handler: PlasmoMessaging.MessageHandler<
+	RequestBody,
+	ResponseBody
+> = async ({ body: sound }, res) => {
+	const playSound = async () => {
+		if (!sound) {
+			return Err({
 				_tag: 'WhisperingError',
 				title: 'Error invoking playSound command',
 				description:
 					'Sound must be provided in the request body of the message',
 				action: { type: 'none' },
-			};
+			});
 		}
-		yield* playSound(body.sound);
-	}).pipe(
-		Effect.tapError(renderErrorAsNotification),
-		Effect.provide(NotificationServiceBgswLive),
-		effectToResult,
-		Effect.map(res.send),
-		Effect.runPromise,
-	);
+		console.info('Playing sound', sound);
+		const getActiveTabIdResult = await getActiveTabId();
+		if (!getActiveTabIdResult.ok) {
+			return Err({
+				_tag: 'WhisperingError',
+				title: 'Failed to get active tab ID',
+				description: 'Failed to get active tab ID to play sound',
+				action: {
+					type: 'more-details',
+					error: getActiveTabIdResult.error,
+				},
+			});
+		}
+		const activeTabId = getActiveTabIdResult.data;
+		if (!activeTabId) {
+			return Err({
+				_tag: 'WhisperingError',
+				title: 'Failed to get active tab ID',
+				description: 'Failed to get active tab ID to play sound',
+				action: { type: 'none' },
+			});
+		}
+		const sendMessageResult = await chrome.tabs.sendMessage(activeTabId, {
+			message: 'playSound',
+			sound,
+		});
+		if (!sendMessageResult) {
+			return Err({
+				_tag: 'WhisperingError',
+				title: `Failed to play ${sound} sound`,
+				description: `Failed to play ${sound} sound in active tab ${activeTabId}`,
+				action: { type: 'more-details', error: sendMessageResult },
+			});
+		}
+		return Ok(undefined);
+	};
+	res.send(await playSound());
+};
 
 export default handler;
