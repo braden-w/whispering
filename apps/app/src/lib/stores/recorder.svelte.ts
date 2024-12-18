ad40d7491b85a5b5b41f2d5d94d5646099864659
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-17 22:26:37 -0800

    wip: move .svelte.ts files away from effect
    Former-commit-id: 7a1027adaac9c54d5753766241782f3b39d0bbbd
    Former-commit-id: 19dfc4444eeffc8960c6fc0d71241e272392e19d

--- apps/app/src/lib/stores/recorder.svelte.ts
+++ apps/app/src/lib/stores/recorder.svelte.ts
@@ -1,15 +1,12 @@
 import { sendMessageToExtension } from '$lib/sendMessageToExtension';
-import { MainLive } from '$lib/services';
 import { setAlwaysOnTop } from '$lib/services/AlwaysOnTopService';
 import { mediaRecorder } from '$lib/services/MediaRecorderService';
-import { NotificationServiceDesktopLive } from '$lib/services/NotificationServiceDesktopLive';
-import { NotificationServiceWebLive } from '$lib/services/NotificationServiceWebLive';
+import { NotificationService } from '$lib/services/NotificationService';
 import { SetTrayIconService } from '$lib/services/SetTrayIconService';
-import { renderErrorAsToast } from '$lib/services/renderErrorAsToast';
+import { renderErrAsToast } from '$lib/services/renderErrorAsToast';
 import { recordings } from '$lib/stores/recordings.svelte';
 import { settings } from '$lib/stores/settings.svelte';
-import { NotificationService, type RecorderState } from '@repo/shared';
-import { Effect } from 'effect';
+import { Ok, type RecorderState, type Result } from '@repo/shared';
 import { nanoid } from 'nanoid/non-secure';
 import type { Recording } from '../services/RecordingDbService';
 import stopSoundSrc from './assets/sound_ex_machina_Button_Blip.mp3';
@@ -20,8 +17,7 @@ const startSound = new Audio(startSoundSrc);
 const stopSound = new Audio(stopSoundSrc);
 const cancelSound = new Audio(cancelSoundSrc);
 
-export const recorderState = Effect.gen(function* () {
-	const { setTrayIcon } = yield* SetTrayIconService;
+export const recorderState = (() => {
 	let value = $state<RecorderState>('IDLE');
 	return {
 		get value() {
@@ -29,44 +25,51 @@ export const recorderState = Effect.gen(function* () {
 		},
 		set value(newValue: RecorderState) {
 			value = newValue;
-			setTrayIcon(newValue).pipe(
-				Effect.catchAll(renderErrorAsToast),
-				Effect.runPromise,
-			);
+			(async () => {
+				const result = await SetTrayIconService.setTrayIcon(newValue);
+				if (!result.ok) renderErrAsToast(result);
+			})();
 		},
 	};
-}).pipe(Effect.provide(MainLive), Effect.runSync);
+})();
 
 const IS_RECORDING_NOTIFICATION_ID = 'WHISPERING_RECORDING_NOTIFICATION';
 
-export const recorder = Effect.gen(function* () {
-	const { notify } = yield* NotificationService;
+const createRecorder = () => {
+	const { notify } = NotificationService;
 
 	return {
 		get recorderState() {
 			return recorderState.value;
 		},
-		toggleRecording: () =>
-			Effect.gen(function* () {
+
+		async toggleRecording() {
+			const toggleRecordingResult = await (async (): Promise<
+				Result<undefined>
+			> => {
 				switch (mediaRecorder.recordingState) {
-					case 'inactive':
+					case 'inactive': {
 						if (settings.value.alwaysOnTop === 'When Recording') {
 							await setAlwaysOnTop(true);
 						}
-						yield* mediaRecorder.startRecording();
+						const startRecordingResult = await mediaRecorder.startRecording();
+						if (!startRecordingResult.ok) return startRecordingResult;
 						recorderState.value = 'RECORDING';
-						yield* Effect.logInfo('Recording started');
+						console.info('Recording started');
 						if (settings.value.isPlaySoundEnabled) {
 							if (!document.hidden) {
 								startSound.play();
 							} else {
-								yield* sendMessageToExtension({
-									name: 'whispering-extension/playSound',
-									body: { sound: 'start' },
-								}).pipe(Effect.catchAll(renderErrorAsToast));
+								const sendMessageToExtensionResult =
+									await sendMessageToExtension({
+										name: 'whispering-extension/playSound',
+										body: { sound: 'start' },
+									});
+								if (!sendMessageToExtensionResult.ok)
+									return sendMessageToExtensionResult;
 							}
 						}
-						yield* notify({
+						notify({
 							id: IS_RECORDING_NOTIFICATION_ID,
 							title: 'Whispering is recording...',
 							description: 'Click to go to recorder',
@@ -75,21 +78,27 @@ export const recorder = Effect.gen(function* () {
 								label: 'Go to recorder',
 								goto: '/',
 							},
-						}).pipe(Effect.catchAll(renderErrorAsToast));
-						return;
+						});
+						return Ok(undefined);
+					}
 					case 'recording': {
-						const audioBlob = yield* mediaRecorder.stopRecording;
+						const stopRecordingResult = await mediaRecorder.stopRecording();
+						if (!stopRecordingResult.ok) return stopRecordingResult;
+						const audioBlob = stopRecordingResult.data;
 						recorderState.value = 'IDLE';
-						yield* Effect.logInfo('Recording stopped');
+						console.info('Recording stopped');
 
 						if (settings.value.isPlaySoundEnabled) {
 							if (!document.hidden) {
 								stopSound.play();
 							} else {
-								yield* sendMessageToExtension({
-									name: 'whispering-extension/playSound',
-									body: { sound: 'stop' },
-								}).pipe(Effect.catchAll(renderErrorAsToast));
+								const sendMessageToExtensionResult =
+									await sendMessageToExtension({
+										name: 'whispering-extension/playSound',
+										body: { sound: 'stop' },
+									});
+								if (!sendMessageToExtensionResult.ok)
+									return sendMessageToExtensionResult;
 							}
 						}
 
@@ -102,44 +111,48 @@ export const recorder = Effect.gen(function* () {
 							blob: audioBlob,
 							transcriptionStatus: 'UNPROCESSED',
 						};
-						yield* recordings.addRecording(newRecording);
+						await recordings.addRecording(newRecording);
+						await recordings.transcribeRecording(newRecording.id);
 
-						yield* recordings.transcribeRecording(newRecording.id);
 						if (settings.value.alwaysOnTop === 'When Recording') {
 							await setAlwaysOnTop(false);
 						}
+						return Ok(undefined);
 					}
 				}
-			}).pipe(
-				Effect.tapError(() =>
-					Effect.gen(function* () {
-						recorderState.value = 'IDLE';
-						if (settings.value.alwaysOnTop === 'When Recording') {
-							await setAlwaysOnTop(false);
-						}
-					}),
-				),
-				Effect.catchAll(renderErrorAsToast),
-				Effect.runPromise,
-			),
-		cancelRecording: () =>
-			Effect.gen(function* () {
-				yield* mediaRecorder.cancelRecording;
-				if (settings.value.isPlaySoundEnabled) {
-					if (!document.hidden) {
-						cancelSound.play();
-					} else {
-						yield* sendMessageToExtension({
-							name: 'whispering-extension/playSound',
-							body: { sound: 'cancel' },
-						});
-					}
-				}
-				yield* Effect.logInfo('Recording cancelled');
+			})();
+			if (!toggleRecordingResult.ok) {
 				recorderState.value = 'IDLE';
 				if (settings.value.alwaysOnTop === 'When Recording') {
 					await setAlwaysOnTop(false);
 				}
-			}).pipe(Effect.runPromise),
+				renderErrAsToast(toggleRecordingResult);
+				return toggleRecordingResult;
+			}
+			return toggleRecordingResult;
+		},
+		cancelRecording: async () => {
+			const cancelRecordingResult = await mediaRecorder.cancelRecording();
+			if (!cancelRecordingResult.ok) return cancelRecordingResult;
+			if (settings.value.isPlaySoundEnabled) {
+				if (!document.hidden) {
+					cancelSound.play();
+				} else {
+					const sendMessageToExtensionResult = await sendMessageToExtension({
+						name: 'whispering-extension/playSound',
+						body: { sound: 'cancel' },
+					});
+					if (!sendMessageToExtensionResult.ok)
+						return sendMessageToExtensionResult;
+				}
+			}
+			console.info('Recording cancelled');
+			recorderState.value = 'IDLE';
+			if (settings.value.alwaysOnTop === 'When Recording') {
+				await setAlwaysOnTop(false);
+			}
+		},
 	};
-}).pipe(Effect.provide(MainLive), Effect.runSync);
+};
+
+export const recorder = createRecorder();
