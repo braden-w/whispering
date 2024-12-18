5d49e9ae808492c1d9f9a43224170d7f9322bffc
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-18 00:43:32 -0800

    refactor:  setTrayIcon and related
    Former-commit-id: a8d9740db2680caef3ab3485a94d8f82fa6f553e
    Former-commit-id: 12ce640f70e03c3b85c59ff1391405a1398b558d

--- apps/extension/background/messages/whispering-extension/setTrayIcon.ts
+++ apps/extension/background/messages/whispering-extension/setTrayIcon.ts
@@ -1,6 +1,3 @@
-import arrowsCounterclockwise from 'data-base64:~assets/arrows_counterclockwise.png';
-import redLargeSquare from 'data-base64:~assets/red_large_square.png';
-import studioMicrophone from 'data-base64:~assets/studio_microphone.png';
 import type { PlasmoMessaging } from '@plasmohq/messaging';
 import type {
 	ExternalMessageBody,
@@ -8,10 +5,10 @@ import type {
 	RecorderState,
 	Result,
 } from '@repo/shared';
-import { WhisperingError, effectToResult } from '@repo/shared';
-import { Effect } from 'effect';
-import { renderErrorAsNotification } from '~lib/errors';
-import { NotificationServiceBgswLive } from '~lib/services/NotificationServiceBgswLive';
+import { Err, Ok, tryAsync } from '@repo/shared';
+import arrowsCounterclockwise from 'data-base64:~assets/arrows_counterclockwise.png';
+import redLargeSquare from 'data-base64:~assets/red_large_square.png';
+import studioMicrophone from 'data-base64:~assets/studio_microphone.png';
 import {
 	STORAGE_KEYS,
 	extensionStorageService,
@@ -23,24 +20,6 @@ const iconPaths = {
 	LOADING: arrowsCounterclockwise,
 } as const satisfies Record<RecorderState, string>;
 
-const setTrayIcon = (recorderState: RecorderState) =>
-	Effect.gen(function* () {
-		yield* extensionStorageService[STORAGE_KEYS.RECORDER_STATE].set(
-			recorderState,
-		);
-		const path = iconPaths[recorderState];
-		yield* Effect.tryPromise({
-			try: () => chrome.action.setIcon({ path }),
-			catch: (error) => ({
-				_tag: 'WhisperingError',
-				title: `Error setting icon to ${recorderState} icon`,
-				description:
-					"There was an error setting the tray icon using the browser's action API. Please try again.",
-				action: { type: 'more-details', error },
-			}),
-		});
-	});
-
 export type RequestBody =
 	ExternalMessageBody<'whispering-extension/setTrayIcon'>;
 
@@ -48,27 +27,39 @@ export type ResponseBody = Result<
 	ExternalMessageReturnType<'whispering-extension/setTrayIcon'>
 >;
 
-const handler: PlasmoMessaging.MessageHandler<RequestBody, ResponseBody> = (
-	{ body },
-	res,
-) =>
-	Effect.gen(function* () {
+const handler: PlasmoMessaging.MessageHandler<
+	RequestBody,
+	ResponseBody
+> = async ({ body }, res) => {
+	const setTrayIcon = async () => {
 		if (!body?.recorderState) {
-			return yield* {
+			return Err({
 				_tag: 'WhisperingError',
 				title: 'Error invoking setTrayIcon command',
 				description:
 					'RecorderState must be provided in the request body of the message',
 				action: { type: 'none' },
-			};
+			});
 		}
-		yield* setTrayIcon(body.recorderState);
-	}).pipe(
-		Effect.tapError(renderErrorAsNotification),
-		Effect.provide(NotificationServiceBgswLive),
-		effectToResult,
-		Effect.map(res.send),
-		Effect.runPromise,
-	);
+
+		await extensionStorageService[STORAGE_KEYS.RECORDER_STATE].set(
+			body.recorderState,
+		);
+		const path = iconPaths[body.recorderState];
+		const setIconResult = await tryAsync({
+			try: () => chrome.action.setIcon({ path }),
+			catch: (error) => ({
+				_tag: 'WhisperingError',
+				title: `Error setting icon to ${body.recorderState} icon`,
+				description:
+					"There was an error setting the tray icon using the browser's action API. Please try again.",
+				action: { type: 'more-details', error },
+			}),
+		});
+		if (!setIconResult.ok) return setIconResult;
+		return Ok(undefined);
+	};
+	res.send(await setTrayIcon());
+};
 
 export default handler;
