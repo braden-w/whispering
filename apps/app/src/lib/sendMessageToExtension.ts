d93f6e85fb73d4eca797c3d498bd356e7b778da9
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-17 22:34:51 -0800

    wip feat: attempt to move sendMessageToExtension away from effect
    Former-commit-id: b47aae77734919c7ea387c2198b9526ad0185a06
    Former-commit-id: 7d4fd342a642a1e66a6d474bfa47cf583dfe2742

--- apps/app/src/lib/sendMessageToExtension.ts
+++ apps/app/src/lib/sendMessageToExtension.ts
@@ -4,34 +4,33 @@ import type {
 	ExternalMessageReturnType,
 	Result,
 } from '@repo/shared';
-import { WhisperingError, resultToEffect } from '@repo/shared';
-import { Effect } from 'effect';
+import { Ok, tryAsync } from '@repo/shared';
 
-export const sendMessageToExtension = <
+export const sendMessageToExtension = async <
 	M extends ExternalMessage,
 	ExternalMessageName extends ExternalMessage['name'] = M['name'],
 	T = ExternalMessageReturnType<ExternalMessageName>,
 >(
 	message: M,
-) =>
-	Effect.gen(function* () {
-		if (window.__TAURI_INTERNALS__) return;
-		const response = yield* Effect.tryPromise({
-			try: () =>
-				sendToBackgroundViaRelay({
-					name: message.name as never,
-					body: message.body,
-				}) as Promise<Result<T>>,
-			catch: (error) =>
-				new WhisperingError({
-					title: 'Unable to send message to extension',
-					description:
-						'There was likely an issue sending the message to the extension.',
-					action: {
-						type: 'more-details',
-						error,
-					},
-				}),
-		}).pipe(Effect.flatMap(resultToEffect));
-		return response;
+) => {
+	if (window.__TAURI_INTERNALS__) return Ok(undefined);
+	const responseResult = await tryAsync({
+		try: () =>
+			sendToBackgroundViaRelay({
+				name: message.name as never,
+				body: message.body,
+			}) as Promise<Result<T>>,
+		catch: (error) => ({
+			_tag: 'WhisperingError',
+			title: 'Unable to send message to extension',
+			description:
+				'There was likely an issue sending the message to the extension.',
+			action: {
+				type: 'more-details',
+				error,
+			},
+		}),
 	});
+
+	return responseResult;
+};
