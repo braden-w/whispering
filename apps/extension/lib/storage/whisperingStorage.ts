b97e03294d31e6d3ddd276474fcd127a34d0d828
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-18 23:02:27 -0800

    fix: import storage
    Former-commit-id: cb2015ec24af973c2efc0d344e7214ff914b8054
    Former-commit-id: 4a4e368352249bf66c5cf5bb09ac9387d5d391ea

--- apps/extension/lib/storage/whisperingStorage.ts
+++ apps/extension/lib/storage/whisperingStorage.ts
@@ -1,8 +1,8 @@
+import { Storage } from '@plasmohq/storage';
 import type { WhisperingStorageKey, WhisperingStorageKeyMap } from './keys';
 
 const createWhisperingStorage = () => {
-	const storage = new Storage();
-
+	const storage = new Storage({ area: 'session' });
 	return {
 		setItem: <K extends WhisperingStorageKey>(
 			key: K,
