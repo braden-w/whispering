37066ce00f444627067c44ba17b6c26a9abf08a4
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-18 22:37:55 -0800

    chore: install alert dialog
    Former-commit-id: a053b98a20858b6fb720da2ac3a779021b722ad3
    Former-commit-id: 075135effcb11e80094e4487ae4ec6d74106a49c

---
+++ apps/app/src/lib/components/ui/alert-dialog/alert-dialog-action.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { AlertDialog as AlertDialogPrimitive } from 'bits-ui';
+	import { buttonVariants } from '$lib/components/ui/button/index.js';
+	import { cn } from '$lib/utils.js';
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		...restProps
+	}: AlertDialogPrimitive.ActionProps = $props();
+</script>
+
+<AlertDialogPrimitive.Action
+	bind:ref
+	class={cn(buttonVariants(), className)}
+	{...restProps}
+/>
