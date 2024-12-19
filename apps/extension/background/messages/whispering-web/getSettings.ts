fa936b59b1f4f9c08ddb5b2d912e2b9efaac772a
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-18 23:21:40 -0800

    fix: getSettings logic
    Former-commit-id: 4e5b4983c5a02e1c6f8c99d60672566e9b7f53b1
    Former-commit-id: 09f0754f0dc8fbcb0b8521534740a188a6165b3a

--- apps/extension/background/messages/whispering-web/getSettings.ts
+++ apps/extension/background/messages/whispering-web/getSettings.ts
@@ -2,6 +2,7 @@ import type { PlasmoMessaging } from '@plasmohq/messaging';
 import {
 	getDefaultSettings,
 	Ok,
+	parseJson,
 	settingsSchema,
 	WhisperingErr,
 	type Settings,
@@ -24,7 +25,7 @@ const handler: PlasmoMessaging.MessageHandler<
 		if (!getWhisperingTabIdResult.ok) return getWhisperingTabIdResult;
 		const whisperingTabId = getWhisperingTabIdResult.data;
 
-		const valueFromStorage = await injectScript<
+		const valueFromStorageResult = await injectScript<
 			string | null,
 			[WhisperingStorageKey]
 		>({
@@ -33,18 +34,7 @@ const handler: PlasmoMessaging.MessageHandler<
 			func: (settingsKey) => {
 				try {
 					const valueFromStorage = localStorage.getItem(settingsKey);
-					if (valueFromStorage === null)
-						return {
-							ok: false,
-							error: {
-								_tag: 'WhisperingError',
-								title: 'Whispering settings not found',
-								description:
-									'Local storage does not contain Whispering settings.',
-							},
-						};
-					const value = JSON.parse(valueFromStorage);
-					return { ok: true, data: value } as const;
+					return { ok: true, data: valueFromStorage } as const;
 				} catch (error) {
 					return {
 						ok: false,
@@ -63,10 +53,27 @@ const handler: PlasmoMessaging.MessageHandler<
 			},
 			args: ['whispering-settings'],
 		});
+		if (!valueFromStorageResult.ok) return valueFromStorageResult;
+		const valueFromStorage = valueFromStorageResult.data;
 
-		if (valueFromStorage === null) return Ok(getDefaultSettings('extension'));
-		const parseResult = settingsSchema.safeParse(valueFromStorage);
-		if (!parseResult.success)
+		if (valueFromStorage === null) {
+			return WhisperingErr({
+				title: 'Whispering settings not found',
+				description: 'Local storage does not contain Whispering settings.',
+			});
+		}
+
+		const parseJsonResult = parseJson(valueFromStorage);
+		if (!parseJsonResult.ok)
+			return WhisperingErr({
+				title: 'Unable to parse Whispering settings',
+				description:
+					'There was an error parsing the Whispering settings from localStorage with JSON.parse.',
+			});
+		const maybeSettings = parseJsonResult.data;
+
+		const parseResult = settingsSchema.safeParse(maybeSettings);
+		if (!parseResult.success) {
 			return WhisperingErr({
 				title: 'Unable to parse Whispering settings',
 				description:
@@ -76,6 +83,7 @@ const handler: PlasmoMessaging.MessageHandler<
 					error: parseResult.error,
 				},
 			});
+		}
 		const settings = parseResult.data;
 		return Ok(settings);
 	};
