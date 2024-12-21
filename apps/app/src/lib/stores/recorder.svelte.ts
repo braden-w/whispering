bcd057f018e63609ad79fa38fcf5141051ed49ed
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-21 13:57:32 -0800

    refactor: recorder and toggleRecording
    Former-commit-id: 55798af26088375756eef835f0d209c96de99b7e
    Former-commit-id: 61940cee099a594b3706a4e338ebc258c257cdeb

--- apps/app/src/lib/stores/recorder.svelte.ts
+++ apps/app/src/lib/stores/recorder.svelte.ts
@@ -16,103 +16,201 @@ import type { Recording } from '../services/RecordingDbService';
 import stopSoundSrc from './assets/sound_ex_machina_Button_Blip.mp3';
 import startSoundSrc from './assets/zapsplat_household_alarm_clock_button_press_12967.mp3';
 import cancelSoundSrc from './assets/zapsplat_multimedia_click_button_short_sharp_73510.mp3';
+import { MediaRecorderService } from '$lib/services/MediaRecorderService';
 
 const startSound = new Audio(startSoundSrc);
 const stopSound = new Audio(stopSoundSrc);
 const cancelSound = new Audio(cancelSoundSrc);
-
-export const recorderState = (() => {
-	let value = $state<WhisperingRecordingState>('IDLE');
-	return {
-		get value() {
-			return value;
-		},
-		set value(newValue: WhisperingRecordingState) {
-			value = newValue;
-			(async () => {
-				const result = await SetTrayIconService.setTrayIcon(newValue);
-				if (!result.ok) renderErrAsToast(result);
-			})();
-		},
-	};
-})();
 
 const IS_RECORDING_NOTIFICATION_ID = 'WHISPERING_RECORDING_NOTIFICATION';
 
 export const recorder = createRecorder();
 
 function createRecorder() {
+	let recorderState = $state<WhisperingRecordingState>('IDLE');
+
 	return {
 		get recorderState() {
-			return recorderState.value;
+			return recorderState;
+		},
+		set recorderState(newValue: WhisperingRecordingState) {
+			recorderState = newValue;
+			(async () => {
+				const result = await SetTrayIconService.setTrayIcon(newValue);
+				if (!result.ok) renderErrAsToast(result);
+			})();
 		},
 
 		async toggleRecording(): Promise<void> {
-			if (mediaRecorder.recordingState === 'RECORDING') {
-				void mediaRecorder.stopRecording({
-					onSuccess: (audioBlob) => {
-						recorderState.value = 'IDLE';
-						console.info('Recording stopped');
-						void playSound('stop');
+			const onStopError = renderErrAsToast;
+			const onStopSuccess = (blob: Blob) => {
+				recorderState = 'IDLE';
+				console.info('Recording stopped');
+				void playSound('stop');
 
-						const newRecording: Recording = {
-							id: nanoid(),
-							title: '',
-							subtitle: '',
-							timestamp: new Date().toISOString(),
-							transcribedText: '',
-							blob: audioBlob,
-							transcriptionStatus: 'UNPROCESSED',
-						};
+				const newRecording: Recording = {
+					id: nanoid(),
+					title: '',
+					subtitle: '',
+					timestamp: new Date().toISOString(),
+					transcribedText: '',
+					blob,
+					transcriptionStatus: 'UNPROCESSED',
+				};
 
-						const addRecordingAndTranscribeResultToastId = nanoid();
+				const addRecordingAndTranscribeResultToastId = nanoid();
 
-						void recordings.addRecording(newRecording, {
-							onSuccess: () => {
-								toast.loading({
-									id: addRecordingAndTranscribeResultToastId,
-									title: 'Recording added!',
-									description: 'Your recording has been added successfully.',
-								});
-								recordings.transcribeRecording(newRecording.id, {
-									toastId: addRecordingAndTranscribeResultToastId,
-								});
-							},
-							onError: renderErrAsToast,
+				void recordings.addRecording(newRecording, {
+					onSuccess: () => {
+						toast.loading({
+							id: addRecordingAndTranscribeResultToastId,
+							title: 'Recording added!',
+							description: 'Your recording has been added successfully.',
+						});
+						recordings.transcribeRecording(newRecording.id, {
+							toastId: addRecordingAndTranscribeResultToastId,
 						});
 					},
 					onError: renderErrAsToast,
 				});
+			};
+
+			const onStartSuccess = () => {
+				recorderState = 'RECORDING';
+				console.info('Recording started');
+				void playSound('start');
+				void NotificationService.notify({
+					id: IS_RECORDING_NOTIFICATION_ID,
+					title: 'Whispering is recording...',
+					description: 'Click to go to recorder',
+					action: {
+						type: 'link',
+						label: 'Go to recorder',
+						goto: '/',
+					},
+				});
+			};
+
+			if (recorderState === 'RECORDING') {
+				if (settings.value.isFasterRerecordEnabled) {
+					const stopResult = await MediaRecorderService.stopAndCloseStream();
+					if (!stopResult.ok) {
+						onStopError(stopResult);
+						return;
+					}
+					const blob = stopResult.data;
+					onStopSuccess(blob);
+				} else {
+					const stopResult = await MediaRecorderService.stopKeepStream();
+					if (!stopResult.ok) {
+						onStopError(stopResult);
+						return;
+					}
+					const blob = stopResult.data;
+					onStopSuccess(blob);
+				}
+			} else {
+				if (settings.value.isFasterRerecordEnabled) {
+					const startResult =
+						await MediaRecorderService.startFromExistingStream({
+							bitsPerSecond: Number(settings.value.bitrateKbps) * 1000,
+						});
+					if (!startResult.ok) {
+						switch (startResult.error._tag) {
+							case 'OpenStreamDoesNotExistErr': {
+								toast.loading({
+									title: 'Existing recording session not found',
+									description: 'Creating a new recording session...',
+								});
+								const startResult =
+									await MediaRecorderService.startFromNewStream({
+										bitsPerSecond: Number(settings.value.bitrateKbps) * 1000,
+									});
+								if (!startResult.ok) {
+									toast.error({
+										title: 'Error creating new recording session',
+										description: 'Please try again',
+									});
+									return;
+								}
+								toast.loading({
+									title: 'Recording session created',
+									description: 'Recording in progress...',
+								});
+								break;
+							}
+							case 'OpenStreamIsInactiveErr':
+								toast.loading({
+									title: 'Existing recording session is inactive',
+									description: 'Refreshing recording session...',
+								});
+								break;
+							case 'InitMediaRecorderFromStreamErr':
+								toast.loading({
+									title:
+										'Error initializing media recorder with preferred device',
+									description:
+										'Trying to find another available audio input device...',
+								});
+								break;
+						}
+						return;
+					}
+					onStartSuccess();
+				} else {
+					const startResult =
+						await MediaRecorderService.startFromExistingStream({
+							bitsPerSecond: Number(settings.value.bitrateKbps) * 1000,
+						});
+					if (!startResult.ok) {
+						switch (startResult.error._tag) {
+							case 'OpenStreamDoesNotExistErr':
+								toast.loading({
+									title: 'Existing recording session not found',
+									description: 'Creating a new recording session...',
+								});
+								break;
+							case 'OpenStreamIsInactiveErr':
+								toast.loading({
+									title: 'Existing recording session is inactive',
+									description: 'Refreshing recording session...',
+								});
+								break;
+							case 'InitMediaRecorderFromStreamErr':
+								toast.loading({
+									title:
+										'Error initializing media recorder with preferred device',
+									description:
+										'Trying to find another available audio input device...',
+								});
+								break;
+						}
+						return;
+					}
+					onStartSuccess();
+				}
+			}
+		},
+		async cancelRecording() {
+			const onCancelSuccess = () => {
+				void playSound('cancel');
+				console.info('Recording cancelled');
+				recorderState = 'IDLE';
+			};
+
+			const cancelResult = await MediaRecorderService.cancelAndCloseStream();
+			if (!cancelResult.ok) {
+				switch (cancelResult.error._tag) {
+					case 'OpenStreamDoesNotExistErr':
+						toast.success({
+							title: 'No existing recording session found to cancel',
+							description: 'You can start a new recording session',
+						});
+						break;
+				}
 				return;
 			}
-			void mediaRecorder.startRecording({
-				onSuccess: () => {
-					recorderState.value = 'RECORDING';
-					console.info('Recording started');
-					void playSound('start');
-					void NotificationService.notify({
-						id: IS_RECORDING_NOTIFICATION_ID,
-						title: 'Whispering is recording...',
-						description: 'Click to go to recorder',
-						action: {
-							type: 'link',
-							label: 'Go to recorder',
-							goto: '/',
-						},
-					});
-				},
-				onError: renderErrAsToast,
-			});
-		},
-		cancelRecording() {
-			void mediaRecorder.cancelRecording({
-				onSuccess: () => {
-					void playSound('cancel');
-					console.info('Recording cancelled');
-					recorderState.value = 'IDLE';
-				},
-				onError: renderErrAsToast,
-			});
+			onCancelSuccess();
 		},
 	};
 }
