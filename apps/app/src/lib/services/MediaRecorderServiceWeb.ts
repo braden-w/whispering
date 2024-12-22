3960b0b337096d9e5361642d6184b774c445c8d6
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-21 19:52:11 -0800

    wip feat: new redesign of createMediaRecorderServiceWeb
    Former-commit-id: 0ab5633a4642f597905f65ac9b79ba9b26858683
    Former-commit-id: 96c77ed45abf3a1dfbcb5f73c6e83d76346819cd

--- apps/app/src/lib/services/MediaRecorderServiceWeb.ts
+++ apps/app/src/lib/services/MediaRecorderServiceWeb.ts
@@ -1,6 +1,7 @@
 import { settings } from '$lib/stores/settings.svelte.js';
 import {
 	BubbleErr,
+	type BubbleResult,
 	Ok,
 	type OnlyErrors,
 	WhisperingErr,
@@ -31,186 +32,145 @@ const OpenStreamIsInactiveErr = BubbleErr({
 } as const);
 type OpenStreamIsInactiveErr = typeof OpenStreamIsInactiveErr;
 
-export const createMediaRecorderServiceWeb = () => {
-	let recorder: MediaRecorder | null = null;
-	let isInRecordingSession = $state<boolean>(false);
-	const recordedChunks: Blob[] = [];
+type UserRecordingSessionConfig = {
+	deviceId: string;
+	bitsPerSecond: number;
+};
+type RecordingSession = UserRecordingSessionConfig & {
+	stream: MediaStream;
+	recorder: MediaRecorder | null;
+	recordedChunks: Blob[];
+};
 
-	const mediaStream = createMediaStreamManager({
-		onSetStream: (stream) => {
-			isInRecordingSession = stream?.active ?? false;
-		},
-	});
+type MediaRecorderService = {
+	enumerateRecordingDevices: () => Promise<BubbleResult<MediaDeviceInfo[]>>;
+	initRecordingSession: (
+		userRecordingSessionConfig: UserRecordingSessionConfig,
+	) => Promise<BubbleResult<void>>;
+	closeRecordingSession: () => Promise<BubbleResult<void>>;
+	startRecording: (opts: { recordingId: string }) => Promise<
+		BubbleResult<void>
+	>;
+	stopRecording: () => Promise<BubbleResult<Blob>>;
+	cancelRecording: () => Promise<BubbleResult<void>>;
+};
 
-	const startRecording = (
-		stream: MediaStream,
-		{ bitsPerSecond }: { bitsPerSecond: number },
-	) => {
-		const newRecorderResult = trySyncBubble({
-			try: () => new MediaRecorder(stream, { bitsPerSecond }),
-			catch: (error) =>
-				({
-					_tag: 'InitMediaRecorderFromStreamErr',
-					message: 'Error initializing media recorder from stream',
-				}) as const,
-		});
-		if (!newRecorderResult.ok) return newRecorderResult;
-		const newRecorder = newRecorderResult.data;
-		newRecorder.addEventListener('dataavailable', (event: BlobEvent) => {
-			if (!event.data.size) return;
-			recordedChunks.push(event.data);
-		});
-		newRecorder.start(TIMESLICE_MS);
-		recorder = newRecorder;
-		return Ok(undefined);
-	};
-
-	const stopRecorder = async () => {
-		const stopResult = await tryAsyncWhispering({
-			try: () =>
-				new Promise<Blob>((resolve, reject) => {
-					if (!recorder) {
-						reject(new Error('No active media recorder'));
-						return;
-					}
-					recorder.addEventListener('stop', () => {
-						if (!recorder) {
-							reject(
-								new Error('Media recorder was nullified during stop event'),
-							);
-							return;
-						}
-						const audioBlob = new Blob(recordedChunks, {
-							type: recorder.mimeType,
-						});
-						resolve(audioBlob);
-					});
-					recorder.stop();
-				}),
-			catch: (error) =>
-				({
-					_tag: 'WhisperingError',
-					title: 'Error stopping media recorder',
-					description: 'Please try again',
-
-					action: {
-						type: 'more-details',
-						error,
-					},
-				}) as const,
-		});
-		if (!stopResult.ok) return stopResult;
-		const blob = stopResult.data;
-		return Ok(blob);
-	};
-
-	const cancelRecording = () => {
-		if (!recorder) return OpenStreamDoesNotExistErr;
-		recorder.stop();
-		recorder = null;
-		return Ok(undefined);
-	};
-
-	const getReusedStream = async () => {
-		if (!mediaStream.stream) {
-			return OpenStreamDoesNotExistErr;
-		}
-		if (!mediaStream.stream.active) {
-			return OpenStreamIsInactiveErr;
-		}
-		return Ok(mediaStream.stream);
-	};
+export const createMediaRecorderServiceWeb = (): MediaRecorderService => {
+	let currentSession: RecordingSession | null = null;
 
 	return {
-		async startFromExistingStream(
-			{ bitsPerSecond }: { bitsPerSecond: number },
-			{
-				onSuccess,
-				onError,
-			}: {
-				onSuccess: () => void;
-				onError: (
-					errResult: OnlyErrors<
-						| Awaited<ReturnType<typeof getReusedStream>>
-						| Awaited<ReturnType<typeof startRecording>>
-					>,
-				) => void;
-			},
-		) {
-			const reusedStreamResult = await getReusedStream();
-			if (!reusedStreamResult.ok) {
-				onError(reusedStreamResult);
-				return;
+		enumerateRecordingDevices,
+		async initRecordingSession(config) {
+			const getStreamForDeviceIdResult = await getStreamForDeviceId(
+				config.deviceId,
+			);
+			if (!getStreamForDeviceIdResult.ok) return getStreamForDeviceIdResult;
+			const stream = getStreamForDeviceIdResult.data;
+			currentSession = {
+				...config,
+				stream,
+				recorder: null,
+				recordedChunks: [],
+			};
+			return Ok(undefined);
+		},
+		async closeRecordingSession() {
+			if (!currentSession) {
+				return BubbleErr({
+					_tag: 'RecordingSessionNotInitializedErr',
+					message: 'Attempted to close recording session without initializing',
+				} as const);
 			}
-			const reusedStream = reusedStreamResult.data;
-			const startRecordingResult = startRecording(reusedStream, {
-				bitsPerSecond,
+			if (currentSession.recorder) {
+				return BubbleErr({
+					_tag: 'RecordingSessionNotInitializedErr',
+					message:
+						'Attempted to close recording session without stopping or canceling',
+				} as const);
+			}
+			for (const track of currentSession.stream.getTracks()) {
+				track.stop();
+			}
+			currentSession.recorder = null;
+			currentSession.recordedChunks = [];
+			currentSession = null;
+			return Ok(undefined);
+		},
+		async startRecording({ recordingId }) {
+			if (!currentSession) {
+				return BubbleErr({
+					_tag: 'RecordingSessionNotInitializedErr',
+					message: 'Recording session not initialized',
+				});
+			}
+			const { stream, bitsPerSecond } = currentSession;
+			const newRecorderResult = trySyncBubble({
+				try: () => new MediaRecorder(stream, { bitsPerSecond }),
+				catch: (error) =>
+					({
+						_tag: 'InitMediaRecorderFromStreamErr',
+						message: 'Error initializing media recorder from stream',
+					}) as const,
 			});
-			if (!startRecordingResult.ok) {
-				onError(startRecordingResult);
-				return;
-			}
-			onSuccess();
+			if (!newRecorderResult.ok) return newRecorderResult;
+			const newRecorder = newRecorderResult.data;
+			newRecorder.addEventListener('dataavailable', (event: BlobEvent) => {
+				if (!event.data.size) return;
+				recordedChunks.push(event.data);
+			});
+			newRecorder.start(TIMESLICE_MS);
+			recorder = newRecorder;
+			return Ok(undefined);
 		},
-		async startFromNewStream({ bitsPerSecond }: { bitsPerSecond: number }) {
-			const getNewStream = () => mediaStream.refreshStream();
-			const newStreamResult = await getNewStream();
-			if (!newStreamResult.ok) return newStreamResult;
-			const newStream = newStreamResult.data;
-			const startRecordingResult = startRecording(newStream, { bitsPerSecond });
-			return startRecordingResult;
-		},
-		async stopKeepStream({
-			onSuccess,
-			onError,
-		}: {
-			onSuccess: (blob: Blob) => void;
-			onError: (
-				errResult: OnlyErrors<Awaited<ReturnType<typeof stopRecorder>>>,
-			) => void;
-		}) {
-			const stopResult = await stopRecorder();
-			if (!stopResult.ok) {
-				onError(stopResult);
-				return stopResult;
-			}
+		async stopRecording() {
+			const stopResult = await tryAsyncBubble({
+				try: () =>
+					new Promise<Blob>((resolve, reject) => {
+						if (!recorder) {
+							reject(new Error('No active media recorder'));
+							return;
+						}
+						recorder.addEventListener('stop', () => {
+							if (!recorder) {
+								reject(
+									new Error('Media recorder was nullified during stop event'),
+								);
+								return;
+							}
+							const audioBlob = new Blob(recordedChunks, {
+								type: recorder.mimeType,
+							});
+							resolve(audioBlob);
+						});
+						recorder.stop();
+					}),
+				catch: (error) =>
+					({
+						_tag: 'StopMediaRecorderErr',
+						message: 'Error stopping media recorder',
+					}) as const,
+			});
+			if (!stopResult.ok) return stopResult;
 			const blob = stopResult.data;
-			onSuccess(blob);
+			return Ok(blob);
 		},
-		async stopAndCloseStream({
-			onSuccess,
-			onError,
-		}: {
-			onSuccess: (blob: Blob) => void;
-			onError: (
-				errResult: OnlyErrors<Awaited<ReturnType<typeof stopRecorder>>>,
-			) => void;
-		}) {
-			const stopResult = await stopRecorder();
-			if (!stopResult.ok) {
-				onError(stopResult);
-				return stopResult;
-			}
-			const blob = stopResult.data;
-			mediaStream.destroy();
-			onSuccess(blob);
-		},
-		async cancelKeepStream() {
-			const cancelResult = cancelRecording();
-			return cancelResult;
-		},
-		async cancelAndCloseStream() {
-			const cancelResult = cancelRecording();
-			mediaStream.destroy();
-			return cancelResult;
+		async cancelRecording() {
+			if (!currentSession?.recorder)
+				return BubbleErr({
+					_tag: 'CancelRecordingErr',
+					message: 'No active media recorder',
+				} as const);
+			currentSession.recorder.stop();
+			currentSession.recorder = null;
+			return Ok(undefined);
 		},
 	};
 };
 
 const createMediaRecorderServiceNative = () => {
 	return {
-		startFromExistingStream() {},
-		startFromNewStream() {
+		startFromExistingRecordingSession() {},
+		startFromNewRecordingSession() {
 			invoke('start');
 		},
 		stopKeepStream() {},
@@ -228,13 +188,9 @@ const createMediaRecorderServiceNative = () => {
 // stop it and return the blob
 // if enabled, destroy the stream and recorder
 
-function createMediaStreamManager({
-	onSetStream,
-}: { onSetStream: (stream: MediaStream | null) => void }) {
-	let currentStream: MediaStream | null = null;
+function createMediaStreamManager() {
 	const setStream = (stream: MediaStream | null) => {
 		currentStream = stream;
-		onSetStream(stream);
 	};
 
 	return {
@@ -255,7 +211,15 @@ function createMediaStreamManager({
 					title: 'No device selected',
 					description: 'Defaulting to first available audio input device...',
 				});
-				const firstAvailableStreamResult = await getFirstAvailableStream();
+				const firstAvailableStreamResult = await getFirstAvailableStream({
+					onSuccess: (stream, deviceId) => {
+						settings.value.selectedAudioInputDeviceId = deviceId;
+						setStream(stream);
+					},
+					onError: () => {
+						toast.dismiss(toastId);
+					},
+				});
 				if (!firstAvailableStreamResult.ok) return firstAvailableStreamResult;
 				const firstAvailableStream = firstAvailableStreamResult.data;
 				setStream(firstAvailableStream);
@@ -283,7 +247,15 @@ function createMediaStreamManager({
 				title: 'Error connecting to selected audio input device',
 				description: 'Trying to find another available audio input device...',
 			});
-			const firstAvailableStreamResult = await getFirstAvailableStream();
+			const firstAvailableStreamResult = await getFirstAvailableStream({
+				onSuccess: (stream, deviceId) => {
+					settings.value.selectedAudioInputDeviceId = deviceId;
+					setStream(stream);
+				},
+				onError: () => {
+					toast.dismiss(toastId);
+				},
+			});
 			if (!firstAvailableStreamResult.ok) {
 				toast.dismiss(toastId);
 				return firstAvailableStreamResult;
@@ -307,9 +279,7 @@ function createMediaStreamManager({
 	} as const;
 }
 
-export function enumerateRecordingDevices(): Promise<
-	WhisperingResult<MediaDeviceInfo[]>
-> {
+async function enumerateRecordingDevices() {
 	return tryAsyncWhispering({
 		try: async () => {
 			const allAudioDevicesStream = await navigator.mediaDevices.getUserMedia({
@@ -337,36 +307,39 @@ export function enumerateRecordingDevices(): Promise<
 	});
 }
 
-async function getFirstAvailableStream(): Promise<
-	WhisperingResult<MediaStream>
-> {
+const NoAvailableAudioInputDevicesErr = WhisperingErr({
+	title: 'No available audio input devices',
+	description: 'Please make sure you have a microphone connected',
+	action: {
+		type: 'link',
+		label: 'Open Settings',
+		goto: '/settings',
+	},
+} as const);
+
+async function getFirstAvailableStream({
+	onSuccess,
+	onError,
+}: {
+	onSuccess: (stream: MediaStream, deviceId: string) => void;
+	onError: (error: typeof NoAvailableAudioInputDevicesErr) => void;
+}) {
 	const recordingDevicesResult = await enumerateRecordingDevices();
 	if (!recordingDevicesResult.ok) return recordingDevicesResult;
 	const recordingDevices = recordingDevicesResult.data;
 
 	for (const device of recordingDevices) {
-		const maybeStream = await getStreamForDeviceId(device.deviceId);
-		if (maybeStream) {
-			settings.value.selectedAudioInputDeviceId = device.deviceId;
-			mediaStream.refreshStream();
-			return Ok(maybeStream);
+		const streamResult = await getStreamForDeviceId(device.deviceId);
+		if (streamResult.ok) {
+			onSuccess(streamResult.data, device.deviceId);
+			return streamResult;
 		}
 	}
-	return WhisperingErr({
-		title: 'No available audio input devices',
-		description: 'Please make sure you have a microphone connected',
-		action: {
-			type: 'link',
-			label: 'Open Settings',
-			goto: '/settings',
-		},
-	});
+	onError(NoAvailableAudioInputDevicesErr);
 }
 
-async function getStreamForDeviceId(
-	recordingDeviceId: string,
-): Promise<MediaStream | null> {
-	const result = await tryAsyncBubble({
+async function getStreamForDeviceId(recordingDeviceId: string) {
+	return tryAsyncBubble({
 		try: async () => {
 			const stream = await navigator.mediaDevices.getUserMedia({
 				audio: {
@@ -378,11 +351,8 @@ async function getStreamForDeviceId(
 			return stream;
 		},
 		catch: (error) => ({
-			_tag: 'GetStreamError',
-			message:
-				error instanceof Error ? error.message : 'Please try again later.',
+			_tag: 'GetStreamForDeviceIdErr',
+			message: `Error getting stream for device id ${recordingDeviceId}: ${error.message}`,
 		}),
 	});
-	if (result.ok) return result.data;
-	return null;
 }
