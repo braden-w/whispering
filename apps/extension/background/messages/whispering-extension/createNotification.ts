39291bd9835425a7f3d4020488ed874ed2d9d8b4
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-25 12:43:00 -0800

    wip feat: inline create Notification
    Former-commit-id: a2557402ef9b56591b569ae7e42b599122ab6812
    Former-commit-id: 517bc786d232a2e858dd1420a56180685bf3086c

--- apps/extension/background/messages/whispering-extension/createNotification.ts
+++ apps/extension/background/messages/whispering-extension/createNotification.ts
@@ -1,7 +1,12 @@
+import { Ok, tryAsync } from '@epicenterhq/result';
 import type { PlasmoMessaging } from '@plasmohq/messaging';
 import type { ToastAndNotifyOptions, WhisperingResult } from '@repo/shared';
 import { WhisperingErr } from '@repo/shared';
-import { NotificationServiceBgswLive } from '~lib/services/NotificationServiceBgswLive';
+import studioMicrophone from 'data-base64:~assets/studio_microphone.png';
+import { nanoid } from 'nanoid';
+import { injectScript } from '~background/injectScript';
+import { renderErrorAsNotification } from '~lib/errors';
+import { getOrCreateWhisperingTabId } from '~lib/getOrCreateWhisperingTabId';
 
 export type CreateNotificationMessage = {
 	notifyOptions: ToastAndNotifyOptions;
@@ -20,12 +25,123 @@ const handler: PlasmoMessaging.MessageHandler<
 					'ToastOptions must be provided in the request body of the message',
 			});
 		}
-		const notifyResult = await NotificationServiceBgswLive.notify(
-			body.notifyOptions,
-		);
-		return notifyResult;
+		const { id = nanoid(), title, description, action } = body.notifyOptions;
+		if (action?.type === 'link') {
+			const createLinkNotificationResult = await tryAsync({
+				try: async () => {
+					chrome.notifications.create(id, {
+						priority: 2,
+						title,
+						message: description,
+						type: 'basic',
+						buttons: [{ title: action.label }],
+						iconUrl: studioMicrophone,
+					});
+
+					chrome.notifications.onClicked.addListener(async (clickedId) => {
+						if (clickedId === id) {
+							chrome.notifications.clear(id);
+							const gotoTargetUrlInWhisperingTabResult =
+								await gotoTargetUrlInWhisperingTab(action.goto);
+							if (!gotoTargetUrlInWhisperingTabResult.ok)
+								return renderErrorAsNotification(
+									gotoTargetUrlInWhisperingTabResult,
+								);
+						}
+					});
+
+					chrome.notifications.onButtonClicked.addListener(
+						async (id, buttonIndex) => {
+							if (buttonIndex === 0) {
+								chrome.notifications.clear(id);
+								const gotoTargetUrlInWhisperingTabResult =
+									await gotoTargetUrlInWhisperingTab(action.goto);
+								if (!gotoTargetUrlInWhisperingTabResult.ok)
+									return renderErrorAsNotification(
+										gotoTargetUrlInWhisperingTabResult,
+									);
+							}
+						},
+					);
+					return id;
+				},
+				mapErr: (error) =>
+					WhisperingErr({
+						title: 'Failed to show notification',
+						description:
+							'There was an error showing the notification in the background service worker.',
+						action: { type: 'more-details', error },
+					}),
+			});
+			if (!createLinkNotificationResult.ok) return createLinkNotificationResult;
+			return Ok(createLinkNotificationResult.data);
+		}
+		const createNotificationResult = await tryAsync({
+			try: async () => {
+				chrome.notifications.create(id, {
+					priority: 2,
+					requireInteraction: true,
+					title,
+					message: description,
+					type: 'basic',
+					iconUrl: studioMicrophone,
+				});
+				return id;
+			},
+			mapErr: (error) =>
+				WhisperingErr({
+					title: 'Failed to show notification',
+					description:
+						'There was an error showing the notification in the background service worker.',
+					action: { type: 'more-details', error },
+				}),
+		});
+		if (!createNotificationResult.ok) return createNotificationResult;
+		return Ok(createNotificationResult.data);
 	};
 	res.send(await createNotification());
 };
+
+export async function gotoTargetUrlInWhisperingTab(
+	path: string,
+): Promise<WhisperingResult<void>> {
+	const getWhisperingTabIdResult = await getOrCreateWhisperingTabId();
+	if (!getWhisperingTabIdResult.ok) return getWhisperingTabIdResult;
+	const whisperingTabId = getWhisperingTabIdResult.data;
+	if (!whisperingTabId)
+		return WhisperingErr({
+			title: 'Whispering tab not found',
+			description: 'The Whispering tab was not found.',
+		});
+	const injectScriptResult = await injectScript<undefined, [string]>({
+		tabId: whisperingTabId,
+		commandName: 'goto',
+		func: (route) => {
+			try {
+				window.goto(route);
+				return { ok: true, data: undefined } as const;
+			} catch (error) {
+				return {
+					ok: false,
+					error: {
+						_tag: 'WhisperingError',
+						variant: 'error',
+						title: `Unable to go to route ${route} in Whispering tab`,
+						description:
+							'There was an error going to the route in the Whispering tab.',
+						action: {
+							type: 'more-details',
+							error,
+						},
+					},
+				} as const;
+			}
+		},
+		args: [path],
+	});
+	if (!injectScriptResult.ok) return injectScriptResult;
+	await chrome.tabs.update(whisperingTabId, { active: true });
+	return Ok(undefined);
+}
 
 export default handler;
