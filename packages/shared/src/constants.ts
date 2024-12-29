4377312529aa71082cf28661fab0fd1726729cbc
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-29 15:19:49 -0800

    feat: open directly to settings in options page
    Former-commit-id: 5285487682ff45ff0517aa914601888b3962690d
    Former-commit-id: 49fa3602703593d929b8b767c4d62762558f240f

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -9,6 +9,8 @@ export const WHISPERING_URL_WILDCARD = `${WHISPERING_URL}/*` as const;
 
 export const WHISPERING_RECORDINGS_PATHNAME = '/recordings' as const;
 
+export const WHISPERING_SETTINGS_PATHNAME = '/settings' as const;
+
 export const BITRATE_VALUES_KBPS = [
 	'64',
 	'96',
