1abc2e56429956a2cd57b8bab704eacd300f959b
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-29 12:55:17 -0800

    feat: offer to go to settings when api key is invalid
    Former-commit-id: bce172f078a5afbe5ae218ebb0c1b65a1cf6bce5
    Former-commit-id: 38c46840c08a9ceeda82c88cf03199cbdb40ac7d

--- apps/app/src/lib/services/transcription/TranscriptionService.ts
+++ apps/app/src/lib/services/transcription/TranscriptionService.ts
@@ -1,6 +1,7 @@
 import { Err, type Ok } from '@epicenterhq/result';
 import type { WhisperingErr, WhisperingErrProperties } from '@repo/shared';
 import type { HttpServiceErr } from '../http/HttpService';
+import { goto } from '$app/navigation';
 
 export type TranscriptionServiceErr = WhisperingErr;
 export type TranscriptionServiceResult<T> = Ok<T> | WhisperingErr;
@@ -40,7 +41,12 @@ export function HttpServiceErrIntoTranscriptionServiceErr({
 					title: '🔑 Authentication Failed',
 					description:
 						'Your current API key is invalid or has expired. Head to settings to enter a valid key and continue transcribing!',
-					action: { type: 'more-details', error: error.error },
+					// action: { type: 'more-details', error: error.error },
+					action: {
+						type: 'link',
+						label: 'Go to settings',
+						goto: '/settings/transcription',
+					},
 				});
 			}
 
