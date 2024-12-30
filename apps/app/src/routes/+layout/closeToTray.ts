68cba17cf074a2dfd2e1fd97d9744f4855e9ec7a
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-29 21:50:21 -0800

    fix: hide window or exit(0)
    Former-commit-id: 19ced9222ec2f60cced450110007d18d90a0d25c
    Former-commit-id: 294ea4e6267565da4a700ae2792a99cafe4d1df7

--- apps/app/src/routes/+layout/closeToTray.ts
+++ apps/app/src/routes/+layout/closeToTray.ts
@@ -1,15 +1,18 @@
-import { getCurrentWindow } from '@tauri-apps/api/window';
-import { onDestroy, onMount } from 'svelte';
 import { settings } from '$lib/stores/settings.svelte';
 import type { UnlistenFn } from '@tauri-apps/api/event';
+import { getCurrentWindow } from '@tauri-apps/api/window';
+import { exit } from '@tauri-apps/plugin-process';
+import { onDestroy, onMount } from 'svelte';
 
 export function closeToTrayIfEnabled() {
 	let unlisten: UnlistenFn;
 	onMount(async () => {
 		unlisten = await getCurrentWindow().onCloseRequested(async (event) => {
+			event.preventDefault();
 			if (settings.value.closeToTray) {
-				event.preventDefault();
 				getCurrentWindow().hide();
+			} else {
+				void exit(0);
 			}
 		});
 	});
