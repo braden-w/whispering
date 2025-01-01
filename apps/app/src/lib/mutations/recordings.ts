934ad7cc93c3db0a08fab476bcf0405ce7ab4032
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-01-01 10:33:37 -0800

    fix: createMutation calls
    Former-commit-id: d5f72b51d0f5c2703c063ea4c5411c99f3b805ed
    Former-commit-id: 0a38c5acd1dbd0eb8044de8128b47096f53a6453

--- apps/app/src/lib/mutations/recordings.ts
+++ apps/app/src/lib/mutations/recordings.ts
@@ -3,47 +3,49 @@ import type { Recording } from '$lib/services/db';
 import { toast } from '$lib/utils/toast';
 import { createMutation } from '@tanstack/svelte-query';
 
-export const createUpdateRecordingWithToast = createMutation(() => ({
-	mutationFn: async (recording: Recording) => {
-		const result = await DbService.updateRecording(recording);
-		if (!result.ok) {
-			toast.error({
-				title: 'Failed to update recording!',
-				description: 'Your recording could not be updated.',
+export const createUpdateRecordingWithToast = () =>
+	createMutation(() => ({
+		mutationFn: async (recording: Recording) => {
+			const result = await DbService.updateRecording(recording);
+			if (!result.ok) {
+				toast.error({
+					title: 'Failed to update recording!',
+					description: 'Your recording could not be updated.',
+				});
+				return;
+			}
+			toast.success({
+				title: 'Updated recording!',
+				description: 'Your recording has been updated successfully.',
 			});
-			return;
-		}
-		toast.success({
-			title: 'Updated recording!',
-			description: 'Your recording has been updated successfully.',
-		});
-	},
-}));
+		},
+	}));
 
-export const createDownloadRecordingWithToast = createMutation(() => ({
-	mutationFn: async (recording: Recording) => {
-		if (!recording.blob) {
-			toast.error({
-				title: '⚠️ Recording blob not found',
-				description: "Your recording doesn't have a blob to download.",
+export const createDownloadRecordingWithToast = () =>
+	createMutation(() => ({
+		mutationFn: async (recording: Recording) => {
+			if (!recording.blob) {
+				toast.error({
+					title: '⚠️ Recording blob not found',
+					description: "Your recording doesn't have a blob to download.",
+				});
+				return;
+			}
+			const result = await DownloadService.downloadBlob({
+				name: `whispering_recording_${recording.id}`,
+				blob: recording.blob,
 			});
-			return;
-		}
-		const result = await DownloadService.downloadBlob({
-			name: `whispering_recording_${recording.id}`,
-			blob: recording.blob,
-		});
-		if (!result.ok) {
-			toast.error({
-				title: 'Failed to download recording!',
-				description: 'Your recording could not be downloaded.',
-				action: { type: 'more-details', error: result.error },
+			if (!result.ok) {
+				toast.error({
+					title: 'Failed to download recording!',
+					description: 'Your recording could not be downloaded.',
+					action: { type: 'more-details', error: result.error },
+				});
+				return;
+			}
+			toast.success({
+				title: 'Recording downloading!',
+				description: 'Your recording is being downloaded.',
 			});
-			return;
-		}
-		toast.success({
-			title: 'Recording downloading!',
-			description: 'Your recording is being downloaded.',
-		});
-	},
-}));
+		},
+	}));
