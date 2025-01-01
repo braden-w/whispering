394af98b90461645f3f43c449a719b7ba79bbc43
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-31 20:09:10 -0800

    chore: simplify recordingKeys for now
    Former-commit-id: a61d260e8ec9f671f9638d78e83ba94cfb63bfe5
    Former-commit-id: c46fbdbd0ad509d0fb6090816b1341316bed4c57

--- apps/app/src/lib/queries/recordings.ts
+++ apps/app/src/lib/queries/recordings.ts
@@ -3,11 +3,8 @@ import { DbService } from '$lib/services.svelte';
 import { createQuery } from '@tanstack/svelte-query';
 
 // Define the query key as a constant array
-export const recordingsKeys = {
+const recordingsKeys = {
 	all: ['recordings'] as const,
-	lists: () => [...recordingsKeys.all, 'list'] as const,
-	details: () => [...recordingsKeys.all, 'detail'] as const,
-	detail: (id: string) => [...recordingsKeys.details(), id] as const,
 };
 
 export const createRecordingsQuery = () =>
