63e391e9ae73e7de933b79272f63d12372e11590
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-01-02 14:37:53 -0800

    feat: RenderTransformation working
    Former-commit-id: b2acd3fed4e7d93118eaf2f70ff482bfc8e61d94
    Former-commit-id: c15fac7b6717bf6163d1eda0c0c76bc50803a8c1

---
+++ apps/app/src/routes/(config)/transformations/RenderTransformation.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+	import type { Transformation } from '$lib/services/db';
+	import {
+		SettingsLabelInput,
+		SettingsLabelSelect,
+		SettingsLabelSwitch,
+	} from '$lib/components/labeled-input/index.js';
+
+	let { initialTransformation }: { initialTransformation: Transformation } =
+		$props();
+	let transformation = $state(structuredClone(initialTransformation));
+
+	$effect(() => {
+		transformation = structuredClone(initialTransformation);
+	});
+</script>
+
+<SettingsLabelInput
+	id="title"
+	label="Title"
+	bind:value={transformation.title}
+	placeholder="Enter a title"
+/>
+
+<SettingsLabelInput
+	id="description"
+	label="Description"
+	bind:value={transformation.description}
+	placeholder="Enter a description"
+/>
+
+{#each transformation.steps as step}
+	<SettingsLabelSelect
+		id="step-type"
+		label="Step Type"
+		selected={step.type}
+		items={[
+			{ value: 'find_replace', label: 'Find Replace' },
+			{ value: 'prompt_transform', label: 'Prompt Transform' },
+		]}
+		onSelectedChange={(value) => {
+			step.type = value;
+		}}
+		placeholder="Select a step type"
+	/>
+	{#if step.type === 'find_replace'}
+		<SettingsLabelInput
+			id="find_replace.findText"
+			label="Find Text"
+			bind:value={step['find_replace.findText']}
+			placeholder="Enter a find text"
+		/>
+		<SettingsLabelInput
+			id="find_replace.replaceText"
+			label="Replace Text"
+			bind:value={step['find_replace.replaceText']}
+			placeholder="Enter a replace text"
+		/>
+		<SettingsLabelSwitch
+			id="find_replace.useRegex"
+			label="Use Regex"
+			checked={step['find_replace.useRegex']}
+			onCheckedChange={(v) => {
+				step['find_replace.useRegex'] = v;
+			}}
+		/>
+	{:else if step.type === 'prompt_transform'}
+		<SettingsLabelSelect
+			id="prompt_transform.model"
+			label="Model"
+			items={[
+				{ value: 'gpt-4o', label: 'GPT-4o' },
+				{ value: 'gpt-4o-mini', label: 'GPT-4o Mini' },
+			]}
+			selected={step['prompt_transform.model']}
+			placeholder="Select a model"
+			onSelectedChange={(value) => {
+				step['prompt_transform.model'] = value;
+			}}
+		/>
+		<SettingsLabelInput
+			id="prompt_transform.systemPromptTemplate"
+			label="System Prompt Template"
+			bind:value={step['prompt_transform.systemPromptTemplate']}
+			placeholder="Enter a system prompt template"
+		/>
+		<SettingsLabelInput
+			id="prompt_transform.userPromptTemplate"
+			label="User Prompt Template"
+			bind:value={step['prompt_transform.userPromptTemplate']}
+			placeholder="Enter a user prompt template"
+		/>
+	{/if}
+{/each}
