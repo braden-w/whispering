94ccbbbc6e87965e4b188a755f6fe5b7647a65ad
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-01-04 02:33:42 -0800

    refactor: rename to .openai.ts
    Former-commit-id: bbe53f6a70745f5889a7305c2a41266e0d3bb3a7
    Former-commit-id: 71e5ef3a9a0e33bade62dc622ebecfca2a8422f0

--- apps/app/src/lib/services.svelte.ts
+++ apps/app/src/lib/services.svelte.ts
@@ -17,7 +17,7 @@ import { createPlaySoundServiceDesktop } from './services/sound/PlaySoundService
 import { createPlaySoundServiceWeb } from './services/sound/PlaySoundService.web';
 import { createTranscriptionServiceFasterWhisperServer } from './services/transcription/TranscriptionService.fasterWhisperServer';
 import { createTranscriptionServiceGroq } from './services/transcription/TranscriptionService.groq';
-import { createTranscriptionServiceWhisper } from './services/transcription/TranscriptionService.whisper';
+import { createTranscriptionServiceWhisper } from './services/transcription/TranscriptionService.openai';
 
 // Services that are not determined by the user's settings, but by the platform.
 
