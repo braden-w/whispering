c75105e6d82fe4f1c4ba922b9152103ae32f694d
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-01-04 03:10:50 -0800

    fix: LabeledTextarea can have optional description
    Former-commit-id: 2e40c7dfea21de346410428bd06de9828f10ec18
    Former-commit-id: 08b50d42f2334d222556c471f6b9c32dad9d6238

--- apps/app/src/lib/components/labeled/LabeledTextarea.svelte
+++ apps/app/src/lib/components/labeled/LabeledTextarea.svelte
@@ -2,6 +2,7 @@
 	import { Textarea } from '$lib/components/ui/textarea';
 	import { Label } from '$lib/components/ui/label';
 	import type { HTMLTextareaAttributes } from 'svelte/elements';
+	import type { Snippet } from 'svelte';
 
 	let {
 		id,
@@ -16,7 +17,7 @@
 		value: string;
 		placeholder?: string;
 		disabled?: boolean;
-		description?: string;
+		description?: string | Snippet;
 	} = $props();
 </script>
 
@@ -24,6 +25,12 @@
 	<Label class="text-sm" for={id}>{label}</Label>
 	<Textarea {id} bind:value {placeholder} {disabled} {...restProps} />
 	{#if description}
-		<div class="text-muted-foreground text-sm">{description}</div>
+		<div class="text-muted-foreground text-sm">
+			{#if typeof description === 'string'}
+				{description}
+			{:else}
+				{@render description()}
+			{/if}
+		</div>
 	{/if}
 </div>
