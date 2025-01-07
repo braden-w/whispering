af79acb4b34aae38880ea119ce17ec1f38cf621f
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-01-06 20:07:41 -0800

    feat: use resizeable dividers
    Former-commit-id: 68f6cd84179443dea4e8faf0a7942ad23db2c9bf
    Former-commit-id: 3aa9c455b47cb864504b05254b657978fd895b66

--- apps/app/src/routes/(config)/transformations/+page.svelte
+++ apps/app/src/routes/(config)/transformations/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import * as Resizable from '$lib/components/ui/resizable/index.js';
 	import { confirmationDialog } from '$lib/components/ConfirmationDialog.svelte';
 	import WhisperingButton from '$lib/components/WhisperingButton.svelte';
 	import { TrashIcon } from '$lib/components/icons';
@@ -45,33 +46,9 @@
 	import { nanoid } from 'nanoid/non-secure';
 	import { Skeleton } from '$lib/components/ui/skeleton/index.js';
 	import { settings } from '$lib/stores/settings.svelte';
+	import * as Separator from '$lib/components/ui/separator';
 
 	const columns: ColumnDef<Transformation>[] = [
-		{
-			id: 'selected',
-			meta: {
-				headerText: 'Selected',
-			},
-			header: (headerContext) =>
-				renderComponent(DataTableHeader, headerContext),
-			cell: ({ row }) => {
-				const isSelected =
-					row.id === settings.value['transformations.selectedTransformationId'];
-				return renderComponent(Button, {
-					variant: isSelected ? 'ghost' : 'default',
-					onclick: () => {
-						settings.value = {
-							...settings.value,
-							'transformations.selectedTransformationId': isSelected
-								? null
-								: row.original.id,
-						};
-					},
-					value: isSelected ? 'Selected' : 'Select',
-				});
-			},
-			enableSorting: false,
-		},
 		{
 			id: 'select',
 			header: ({ table }) =>
@@ -86,16 +63,6 @@
 					onCheckedChange: (value) => row.toggleSelected(!!value),
 					'aria-label': 'Select row',
 				}),
-			enableSorting: false,
-			enableHiding: false,
-		},
-		{
-			accessorKey: 'id',
-			meta: {
-				headerText: 'ID',
-			},
-			header: (headerContext) =>
-				renderComponent(DataTableHeader, headerContext),
 			enableSorting: false,
 			enableHiding: false,
 		},
@@ -116,22 +83,6 @@
 				renderComponent(DataTableHeader, headerContext),
 		},
 		{
-			accessorKey: 'createdAt',
-			meta: {
-				headerText: 'Created At',
-			},
-			header: (headerContext) =>
-				renderComponent(DataTableHeader, headerContext),
-		},
-		{
-			accessorKey: 'updatedAt',
-			meta: {
-				headerText: 'Updated At',
-			},
-			header: (headerContext) =>
-				renderComponent(DataTableHeader, headerContext),
-		},
-		{
 			id: 'actions',
 			accessorFn: (transformation) => transformation,
 			meta: {
@@ -147,7 +98,7 @@
 
 	let sorting = createPersistedState({
 		key: 'whispering-transformations-data-table-sorting',
-		defaultValue: [{ id: 'createdAt', desc: true }],
+		defaultValue: [{ id: 'title', desc: false }],
 		schema: z.array(z.object({ desc: z.boolean(), id: z.string() })),
 	});
 	let columnFilters = createPersistedState({
@@ -160,11 +111,7 @@
 	});
 	let columnVisibility = createPersistedState({
 		key: 'whispering-transformations-data-table-column-visibility',
-		defaultValue: {
-			id: false,
-			createdAt: false,
-			updatedAt: false,
-		},
+		defaultValue: {},
 		schema: z.record(z.string(), z.boolean()),
 	});
 	let rowSelection = createPersistedState({
@@ -251,6 +198,13 @@
 	let isDialogOpen = $state(false);
 	const createTransformationWithToastMutation =
 		createCreateTransformationWithToast();
+
+	const selectedTransformation = $derived(
+		transformationsQuery.data?.find(
+			(t) =>
+				t.id === settings.value['transformations.selectedTransformationId'],
+		),
+	);
 </script>
 
 <svelte:head>
@@ -264,223 +218,242 @@
 	<p class="text-muted-foreground">
 		Your text transformations, stored locally in IndexedDB.
 	</p>
-	<div class="space-y-4 rounded-md border p-6">
-		<div class="flex flex-col items-center gap-2 overflow-auto sm:flex-row">
-			<form
-				class="flex w-full max-w-sm gap-2"
-				onsubmit={(e) => {
-					e.preventDefault();
-					table.getColumn('title')?.setFilterValue(filterQuery);
-				}}
-			>
-				<Input
-					placeholder="Filter transformations..."
-					type="text"
-					bind:value={filterQuery}
-				/>
-				<Button variant="outline" type="submit">Search</Button>
-			</form>
-			<div class="flex w-full items-center justify-end gap-2">
-				{#if selectedTransformationRows.length > 0}
-					<WhisperingButton
-						tooltipContent="Delete selected transformations"
-						variant="outline"
-						size="icon"
-						onclick={() => {
-							confirmationDialog.open({
-								title: 'Delete transformations',
-								subtitle:
-									'Are you sure you want to delete these transformations?',
-								confirmText: 'Delete',
-								onConfirm: () => {
-									deleteTransformationsWithToastMutation.mutate(
-										selectedTransformationRows.map(({ original }) => original),
-									);
-								},
-							});
-						}}
-					>
-						<TrashIcon class="h-4 w-4" />
-					</WhisperingButton>
-				{/if}
 
-				<div class="ml-auto flex items-center gap-2">
-					<Dialog.Root bind:open={isDialogOpen}>
-						<Dialog.Trigger>
-							{#snippet child({ props })}
-								<Button {...props}>
-									<PlusIcon class="h-4 w-4" />
-									Create Transformation
-								</Button>
-							{/snippet}
-						</Dialog.Trigger>
+	<Resizable.PaneGroup direction="horizontal" class="rounded-lg border">
+		<Resizable.Pane defaultSize={50}>
+			<div class="flex flex-col gap-2">
+				<form
+					class="flex w-full gap-2"
+					onsubmit={(e) => {
+						e.preventDefault();
+						table.getColumn('title')?.setFilterValue(filterQuery);
+					}}
+				>
+					<Input
+						placeholder="Filter transformations..."
+						type="text"
+						bind:value={filterQuery}
+					/>
+					<Button variant="outline" type="submit">Search</Button>
+				</form>
 
-						<Dialog.Content
-							class="overflow-y-auto max-h-[90vh] max-w-3xl"
-							onInteractOutside={(e) => {
-								e.preventDefault();
-								if (isDialogOpen) {
-									confirmationDialog.open({
-										title: 'Unsaved changes',
-										subtitle:
-											'You have unsaved changes. Are you sure you want to leave?',
-										confirmText: 'Leave',
-										onConfirm: () => {
-											isDialogOpen = false;
-										},
-									});
-								}
+				<div class="flex items-center justify-between">
+					{#if selectedTransformationRows.length > 0}
+						<WhisperingButton
+							tooltipContent="Delete selected transformations"
+							variant="outline"
+							size="icon"
+							onclick={() => {
+								confirmationDialog.open({
+									title: 'Delete transformations',
+									subtitle:
+										'Are you sure you want to delete these transformations?',
+									confirmText: 'Delete',
+									onConfirm: () => {
+										deleteTransformationsWithToastMutation.mutate(
+											selectedTransformationRows.map(
+												({ original }) => original,
+											),
+										);
+									},
+								});
 							}}
 						>
-							<Dialog.Header>
-								<Dialog.Title>Create Transformation</Dialog.Title>
-								<Dialog.Description>
-									Create a new transformation to transform text.
-								</Dialog.Description>
-							</Dialog.Header>
-							<RenderTransformation
-								{transformation}
-								onChange={(newTransformation) => {
-									transformation = newTransformation;
-								}}
-							/>
-							<Dialog.Footer>
-								<Button
-									variant="outline"
-									onclick={() => (isDialogOpen = false)}
-								>
-									Cancel
-								</Button>
-								<Button
-									type="submit"
-									onclick={() =>
-										createTransformationWithToastMutation.mutate(
-											$state.snapshot(transformation),
-											{
-												onSuccess: () => {
-													isDialogOpen = false;
-													transformation = generateDefaultTransformation();
-												},
-											},
-										)}
-								>
-									Create
-								</Button>
-							</Dialog.Footer>
-						</Dialog.Content>
-					</Dialog.Root>
+							<TrashIcon class="h-4 w-4" />
+						</WhisperingButton>
+					{/if}
 
-					<DropdownMenu.Root>
-						<DropdownMenu.Trigger
-							class={cn(
-								buttonVariants({ variant: 'outline' }),
-								'transition-all [&[data-state=open]>svg]:rotate-180',
-							)}
-						>
-							Columns
-							<ChevronDownIcon
-								class="ml-2 h-4 w-4 transition-transform duration-200"
-							/>
-						</DropdownMenu.Trigger>
-						<DropdownMenu.Content>
-							{#each table
-								.getAllColumns()
-								.filter((c) => c.getCanHide()) as column (column.id)}
-								<DropdownMenu.CheckboxItem
-									checked={column.getIsVisible()}
-									onCheckedChange={(value) => column.toggleVisibility(!!value)}
-								>
-									{column.columnDef.meta?.headerText}
-								</DropdownMenu.CheckboxItem>
-							{/each}
-						</DropdownMenu.Content>
-					</DropdownMenu.Root>
+					<div class="flex items-center gap-2">
+						<Dialog.Root bind:open={isDialogOpen}>
+							<Dialog.Trigger>
+								{#snippet child({ props })}
+									<Button {...props}>
+										<PlusIcon class="h-4 w-4 mr-2" />
+										Create Transformation
+									</Button>
+								{/snippet}
+							</Dialog.Trigger>
+
+							<Dialog.Content
+								class="overflow-y-auto max-h-[90vh] max-w-3xl"
+								onInteractOutside={(e) => {
+									e.preventDefault();
+									if (isDialogOpen) {
+										confirmationDialog.open({
+											title: 'Unsaved changes',
+											subtitle:
+												'You have unsaved changes. Are you sure you want to leave?',
+											confirmText: 'Leave',
+											onConfirm: () => {
+												isDialogOpen = false;
+											},
+										});
+									}
+								}}
+							>
+								<Dialog.Header>
+									<Dialog.Title>Create Transformation</Dialog.Title>
+									<Dialog.Description>
+										Create a new transformation to transform text.
+									</Dialog.Description>
+								</Dialog.Header>
+								<RenderTransformation
+									{transformation}
+									onChange={(newTransformation) => {
+										transformation = newTransformation;
+									}}
+								/>
+								<Dialog.Footer>
+									<Button
+										variant="outline"
+										onclick={() => (isDialogOpen = false)}
+									>
+										Cancel
+									</Button>
+									<Button
+										type="submit"
+										onclick={() =>
+											createTransformationWithToastMutation.mutate(
+												$state.snapshot(transformation),
+												{
+													onSuccess: () => {
+														isDialogOpen = false;
+														transformation = generateDefaultTransformation();
+													},
+												},
+											)}
+									>
+										Create
+									</Button>
+								</Dialog.Footer>
+							</Dialog.Content>
+						</Dialog.Root>
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="rounded-md border">
-			<Table.Root>
-				<Table.Header>
-					{#each table.getHeaderGroups() as headerGroup}
-						<Table.Row>
-							{#each headerGroup.headers as header}
-								<Table.Head colspan={header.colSpan}>
-									{#if !header.isPlaceholder}
-										<FlexRender
-											content={header.column.columnDef.header}
-											context={header.getContext()}
-										/>
-									{/if}
-								</Table.Head>
-							{/each}
-						</Table.Row>
-					{/each}
-				</Table.Header>
-				<Table.Body>
-					{#if transformationsQuery.isPending}
-						{#each { length: 5 }}
+			<div class="rounded-md border">
+				<Table.Root>
+					<Table.Header>
+						{#each table.getHeaderGroups() as headerGroup}
 							<Table.Row>
-								<Table.Cell>
-									<Skeleton class="h-4 w-4" />
-								</Table.Cell>
-								<Table.Cell colspan={columns.length - 1}>
-									<Skeleton class="h-4 w-full" />
-								</Table.Cell>
-							</Table.Row>
-						{/each}
-					{:else if table.getRowModel().rows?.length}
-						{#each table.getRowModel().rows as row (row.id)}
-							<Table.Row>
-								{#each row.getVisibleCells() as cell}
-									<Table.Cell>
-										<FlexRender
-											content={cell.column.columnDef.cell}
-											context={cell.getContext()}
-										/>
-									</Table.Cell>
+								{#each headerGroup.headers as header}
+									<Table.Head colspan={header.colSpan}>
+										{#if !header.isPlaceholder}
+											<FlexRender
+												content={header.column.columnDef.header}
+												context={header.getContext()}
+											/>
+										{/if}
+									</Table.Head>
 								{/each}
 							</Table.Row>
 						{/each}
-					{:else}
-						<Table.Row>
-							<Table.Cell colspan={columns.length} class="h-24 text-center">
-								{#if filterQuery}
-									No transformations found.
-								{:else}
-									No transformations yet. Click "Create Transformation" to add
-									one.
-								{/if}
-							</Table.Cell>
-						</Table.Row>
-					{/if}
-				</Table.Body>
-			</Table.Root>
-		</div>
+					</Table.Header>
+					<Table.Body>
+						{#if transformationsQuery.isPending}
+							{#each { length: 5 }}
+								<Table.Row>
+									<Table.Cell>
+										<Skeleton class="h-4 w-4" />
+									</Table.Cell>
+									<Table.Cell colspan={columns.length - 1}>
+										<Skeleton class="h-4 w-full" />
+									</Table.Cell>
+								</Table.Row>
+							{/each}
+						{:else if table.getRowModel().rows?.length}
+							{#each table.getRowModel().rows as row (row.id)}
+								<Table.Row
+									class={cn('cursor-pointer hover:bg-muted/50', {
+										'bg-muted/75':
+											row.id ===
+											settings.value[
+												'transformations.selectedTransformationId'
+											],
+									})}
+									onclick={() => {
+										settings.value = {
+											...settings.value,
+											'transformations.selectedTransformationId': row.id,
+										};
+									}}
+								>
+									{#each row.getVisibleCells() as cell}
+										<Table.Cell>
+											<FlexRender
+												content={cell.column.columnDef.cell}
+												context={cell.getContext()}
+											/>
+										</Table.Cell>
+									{/each}
+								</Table.Row>
+							{/each}
+						{:else}
+							<Table.Row>
+								<Table.Cell colspan={columns.length} class="h-24 text-center">
+									{#if filterQuery}
+										No transformations found.
+									{:else}
+										No transformations yet. Click "Create Transformation" to add
+										one.
+									{/if}
+								</Table.Cell>
+							</Table.Row>
+						{/if}
+					</Table.Body>
+				</Table.Root>
+			</div>
 
-		<div class="flex items-center justify-between">
-			<div class="text-muted-foreground text-sm">
-				{selectedTransformationRows.length} of {table.getFilteredRowModel().rows
-					.length} row(s) selected.
+			<div class="flex items-center justify-between">
+				<div class="text-muted-foreground text-sm">
+					{selectedTransformationRows.length} of {table.getFilteredRowModel()
+						.rows.length} row(s) selected.
+				</div>
+				<div class="flex items-center space-x-2">
+					<Button
+						variant="outline"
+						size="sm"
+						onclick={() => table.previousPage()}
+						disabled={!table.getCanPreviousPage()}
+					>
+						Previous
+					</Button>
+					<Button
+						variant="outline"
+						size="sm"
+						onclick={() => table.nextPage()}
+						disabled={!table.getCanNextPage()}
+					>
+						Next
+					</Button>
+				</div>
 			</div>
-			<div class="flex items-center space-x-2">
-				<Button
-					variant="outline"
-					size="sm"
-					onclick={() => table.previousPage()}
-					disabled={!table.getCanPreviousPage()}
+		</Resizable.Pane>
+		<Resizable.Handle />
+		<Resizable.Pane defaultSize={50}>
+			{#if selectedTransformation}
+				<div class="rounded-md border p-6">
+					<RenderTransformation
+						transformation={selectedTransformation}
+						onChange={(newTransformation) => {
+							// TODO: Add update mutation
+							console.log('Update transformation:', newTransformation);
+						}}
+					/>
+				</div>
+			{:else}
+				<div
+					class="flex h-[50vh] items-center justify-center rounded-md border"
 				>
-					Previous
-				</Button>
-				<Button
-					variant="outline"
-					size="sm"
-					onclick={() => table.nextPage()}
-					disabled={!table.getCanNextPage()}
-				>
-					Next
-				</Button>
-			</div>
-		</div>
-	</div>
+					<div class="text-center">
+						<h3 class="text-lg font-medium">No transformation selected</h3>
+						<p class="text-muted-foreground mt-2">
+							Select a transformation from the list to edit it
+						</p>
+					</div>
+				</div>
+			{/if}
+		</Resizable.Pane>
+	</Resizable.PaneGroup>
 </main>
