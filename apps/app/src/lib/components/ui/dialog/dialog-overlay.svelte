d29708e3ab1ce925fec298aed272e7408e7eb935
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-01-06 18:17:32 -0800

    feat: z-10 instead of z-50
    Former-commit-id: a596d6d17a48dc70267f8abaa3809c8f1627820f
    Former-commit-id: e68b5ffea3823c5f4bc97b50ed0efe7154627320

--- apps/app/src/lib/components/ui/dialog/dialog-overlay.svelte
+++ apps/app/src/lib/components/ui/dialog/dialog-overlay.svelte
@@ -12,7 +12,7 @@
 <DialogPrimitive.Overlay
 	bind:ref
 	class={cn(
-		'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0  fixed inset-0 z-50 bg-black/80',
+		'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0  fixed inset-0 z-10 bg-black/80',
 		className,
 	)}
 	{...restProps}
