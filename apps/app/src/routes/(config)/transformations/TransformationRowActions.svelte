a2629f37eef348f3031f0428be36dad952de69f5
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-01-06 21:35:19 -0800

    feat: isTransformationActive
    Former-commit-id: 47ee6b775e48e14c60db7d3df982b3dcd8ec04e6
    Former-commit-id: 9d3a4c2d065cc5df9f2493b9df9e68613abe388a

--- apps/app/src/routes/(config)/transformations/TransformationRowActions.svelte
+++ apps/app/src/routes/(config)/transformations/TransformationRowActions.svelte
@@ -4,15 +4,31 @@
 	import { TrashIcon } from '$lib/components/icons';
 	import { createDeleteTransformationWithToast } from '$lib/mutations/transformations';
 	import type { Transformation } from '$lib/services/db';
+	import { CheckIcon } from 'lucide-svelte';
 	import EditTransformationDialog from './EditTransformationDialog.svelte';
+	import { settings } from '$lib/stores/settings.svelte';
 
 	let { transformation }: { transformation: Transformation } = $props();
+
+	const isTransformationActive = $derived(
+		settings.value['transformations.selectedTransformationId'] ===
+			transformation.id,
+	);
 
 	const deleteTransformationWithToastMutation =
 		createDeleteTransformationWithToast();
 </script>
 
 <div class="flex items-center">
+	<WhisperingButton
+		tooltipContent={isTransformationActive
+			? 'Transformation is active'
+			: 'Mark transformation as active'}
+		variant="ghost"
+		disabled={isTransformationActive}
+	>
+		<CheckIcon class="h-4 w-4" />
+	</WhisperingButton>
 	<EditTransformationDialog {transformation}></EditTransformationDialog>
 
 	<WhisperingButton
