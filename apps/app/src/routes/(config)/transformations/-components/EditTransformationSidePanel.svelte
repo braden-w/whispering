7a671f52f00a6a391c06fc0b55e646a484ffd0fc
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-01-10 07:15:46 -0500

    fix: size="icon" for sidebar
    Former-commit-id: 2e6408421765b06e839222287de2308356f19f2f
    Former-commit-id: c6e985fd9b83619e923238ffaff6c6263879a545

--- apps/app/src/routes/(config)/transformations/-components/EditTransformationSidePanel.svelte
+++ apps/app/src/routes/(config)/transformations/-components/EditTransformationSidePanel.svelte
@@ -52,7 +52,7 @@
 		{#snippet closeButtons()}
 			{#if transformation}
 				<div class="flex items-center absolute right-4 top-4 gap-1">
-					<MarkTransformationActiveButton {transformation} />
+					<MarkTransformationActiveButton size="icon" {transformation} />
 					<WhisperingButton
 						tooltipContent="Close"
 						variant="outline"
