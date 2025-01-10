eba1e0b6301a4912f6608a870562dd965ed98151
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-01-09 23:39:58 -0800

    fix: use sounds in an object dictinory to match whisperingSoundNames
    Former-commit-id: 628fcf3d19efdcbe1e9bcb6783424f1e5d378ba0
    Former-commit-id: e1133582598693227f44f537936b0f94b829be52

--- apps/app/src/lib/services/sound/PlaySoundService.desktop.ts
+++ apps/app/src/lib/services/sound/PlaySoundService.desktop.ts
@@ -4,29 +4,19 @@ import stopSoundSrc from './assets/sound_ex_machina_Button_Blip.mp3';
 import startSoundSrc from './assets/zapsplat_household_alarm_clock_button_press_12967.mp3';
 import cancelSoundSrc from './assets/zapsplat_multimedia_click_button_short_sharp_73510.mp3';
 import transcriptionCompleteSoundSrc from './assets/zapsplat_multimedia_ui_notification_classic_bell_synth_success_107505.mp3';
+import type { WhisperingSoundNames } from '@repo/shared';
 
-const startSound = new Audio(startSoundSrc);
-const stopSound = new Audio(stopSoundSrc);
-const cancelSound = new Audio(cancelSoundSrc);
-const transcriptionCompleteSound = new Audio(transcriptionCompleteSoundSrc);
+const sounds = {
+	start: new Audio(startSoundSrc),
+	stop: new Audio(stopSoundSrc),
+	cancel: new Audio(cancelSoundSrc),
+	transcriptionComplete: new Audio(transcriptionCompleteSoundSrc),
+} satisfies Record<WhisperingSoundNames, HTMLAudioElement>;
 
 export function createPlaySoundServiceDesktop(): PlaySoundService {
 	return {
 		playSound: async (soundName) => {
-			switch (soundName) {
-				case 'start':
-					await startSound.play();
-					break;
-				case 'stop':
-					await stopSound.play();
-					break;
-				case 'cancel':
-					await cancelSound.play();
-					break;
-				case 'transcriptionComplete':
-					await transcriptionCompleteSound.play();
-					break;
-			}
+			await sounds[soundName].play();
 			return Ok(undefined);
 		},
 	};
