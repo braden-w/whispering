967f598233e9df339e0379ad19c1c8afc53a8e51
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-01-09 23:38:05 -0800

    Revert "refactor: playSoundService to have a function for each sound"
    This reverts commit 7ff6cf71dbce25e73c03600725427e6394e2dd9e [formerly f654e78c880b4251e8d179e7be797ed2a304b72e] [formerly 324ea1c03d9fdf835b7961aee74be66c362e2976].
    
    
    Former-commit-id: e911ae365e0f5ca3aeb0d2f99356cb4bd2befeb2
    Former-commit-id: a13ae57686117cd2a48450aa4d051f43c7e125bd

--- apps/app/src/lib/services/sound/PlaySoundService.desktop.ts
+++ apps/app/src/lib/services/sound/PlaySoundService.desktop.ts
@@ -12,20 +12,21 @@ const transcriptionCompleteSound = new Audio(transcriptionCompleteSoundSrc);
 
 export function createPlaySoundServiceDesktop(): PlaySoundService {
 	return {
-		playStartSound: async () => {
-			await startSound.play();
-			return Ok(undefined);
-		},
-		playStopSound: async () => {
-			await stopSound.play();
-			return Ok(undefined);
-		},
-		playTranscriptionCompleteSound: async () => {
-			await transcriptionCompleteSound.play();
-			return Ok(undefined);
-		},
-		playCancelSound: async () => {
-			await cancelSound.play();
+		playSound: async (soundName) => {
+			switch (soundName) {
+				case 'start':
+					await startSound.play();
+					break;
+				case 'stop':
+					await stopSound.play();
+					break;
+				case 'cancel':
+					await cancelSound.play();
+					break;
+				case 'transcriptionComplete':
+					await transcriptionCompleteSound.play();
+					break;
+			}
 			return Ok(undefined);
 		},
 	};
--- apps/app/src/lib/services/sound/PlaySoundService.ts
+++ apps/app/src/lib/services/sound/PlaySoundService.ts
@@ -1,7 +1,7 @@
 import type { WhisperingResult, WhisperingSoundNames } from '@repo/shared';
 
 export type PlaySoundService = {
-	[k in WhisperingSoundNames as `play${Capitalize<k>}Sound`]: () => Promise<
-		WhisperingResult<void>
-	>;
+	playSound: (
+		soundName: WhisperingSoundNames,
+	) => Promise<WhisperingResult<void>>;
 };
