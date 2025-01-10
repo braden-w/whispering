9faa63f857aec6f411390d789b978c7fb41951a3
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-01-10 07:46:29 -0500

    feat: renderTransformationo simplification
    Former-commit-id: 418f70c8f9514df156deb3cd932af07c98f68689
    Former-commit-id: 562c1e969d0d672339f9f9d8b88a4a4202e7dc19

--- apps/app/src/routes/(config)/transformations/-components/RenderTransformation.svelte
+++ apps/app/src/routes/(config)/transformations/-components/RenderTransformation.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
 	import * as Tabs from '$lib/components/ui/tabs';
-	import WhisperingButton from '$lib/components/WhisperingButton.svelte';
 	import type { Transformation } from '$lib/services/db';
-	import { XIcon } from 'lucide-svelte';
-	import type { Snippet } from 'svelte';
-	import MarkTransformationActiveButton from '../MarkTransformationActiveButton.svelte';
-	import { sidebar } from './EditTransformationSidePanel.svelte';
 	import RenderTransformationConfigurationAndSteps from './RenderTransformationConfigurationAndSteps.svelte';
 	import RenderTransformationTest from './RenderTransformationTest.svelte';
 	let {
@@ -24,23 +19,10 @@
 	class="w-full flex flex-col gap-2"
 	onValueChange={(v) => (activeTab = v)}
 >
-	<div class="flex items-center gap-2">
-		<Tabs.List class="grid w-full grid-cols-2">
-			<Tabs.Trigger value="configure">Configure</Tabs.Trigger>
-			<Tabs.Trigger value="test">Test</Tabs.Trigger>
-		</Tabs.List>
-		<div class="flex items-center gap-1">
-			<MarkTransformationActiveButton size="icon" {transformation} />
-			<WhisperingButton
-				tooltipContent="Close"
-				variant="outline"
-				size="icon"
-				onclick={() => sidebar.close()}
-			>
-				<XIcon class="size-4" />
-			</WhisperingButton>
-		</div>
-	</div>
+	<Tabs.List class="grid w-full grid-cols-2">
+		<Tabs.Trigger value="configure">Configure</Tabs.Trigger>
+		<Tabs.Trigger value="test">Test</Tabs.Trigger>
+	</Tabs.List>
 
 	<Tabs.Content value="configure" class="relative">
 		<RenderTransformationConfigurationAndSteps {transformation} {onChange} />
