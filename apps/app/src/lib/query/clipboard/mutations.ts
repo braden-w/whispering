fd28040db536e43755010366356f5d7c46b62816
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-01-28 22:25:02 -0500

    feat: simplify mutations of clipboard functions
    Former-commit-id: f972752569bec5323af0b03606442a84136117b3
    Former-commit-id: e6871b000bab9054a98ff3abbae017f707034866

--- apps/app/src/lib/query/clipboard/mutations.ts
+++ apps/app/src/lib/query/clipboard/mutations.ts
@@ -1,51 +1,42 @@
-import { ClipboardService } from '$lib/services';
+import { ClipboardService, createResultMutation } from '$lib/services';
 import { toast } from '$lib/services/toast';
-import { createMutation } from '@tanstack/svelte-query';
 
-export const useCopyTextToClipboard = () =>
-	createMutation(() => ({
-		mutationFn: async (text: string) => {
-			const result = await ClipboardService.setClipboardText(text);
-			return result;
-		},
-	}));
+export async function copyTextToClipboard(text: string) {
+	const result = await ClipboardService.setClipboardText(text);
+	return result;
+}
 
 export type CopyToClipboardLabel =
 	| 'transcribed text'
 	| 'transcribed text (joined)'
 	| 'code'
 	| 'latest transformation run output';
-export const useCopyTextToClipboardWithToast = () =>
-	createMutation(() => ({
-		mutationFn: async ({
-			label,
-			text,
-		}: {
-			label: CopyToClipboardLabel;
-			text: string;
-		}) => {
-			const result = await ClipboardService.setClipboardText(text);
-			if (!result.ok) {
-				toast.error({
-					title: `Error copying ${label} to clipboard`,
-					description: result.error.description,
-					action: { type: 'more-details', error: result.error },
-				});
-				return result;
-			}
-			toast.success({
-				title: `Copied ${label} to clipboard!`,
-				description: text,
-				descriptionClass: 'line-clamp-2',
-			});
-			return result;
-		},
-	}));
 
-export const useWriteTextToCursor = () =>
-	createMutation(() => ({
-		mutationFn: async (text: string) => {
-			const result = await ClipboardService.writeTextToCursor(text);
-			return result;
-		},
-	}));
+export async function copyTextToClipboardWithToast({
+	label,
+	text,
+}: {
+	label: CopyToClipboardLabel;
+	text: string;
+}) {
+	const result = await ClipboardService.setClipboardText(text);
+	if (!result.ok) {
+		toast.error({
+			title: `Error copying ${label} to clipboard`,
+			description: result.error.description,
+			action: { type: 'more-details', error: result.error },
+		});
+		return result;
+	}
+	toast.success({
+		title: `Copied ${label} to clipboard!`,
+		description: text,
+		descriptionClass: 'line-clamp-2',
+	});
+	return result;
+}
+
+export async function writeTextToCursor(text: string) {
+	const result = await ClipboardService.writeTextToCursor(text);
+	return result;
+}
