9e97290979e75b1e7191ab4efe6c031e7f4ab705
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-01-28 23:50:24 -0500

    formatting
    Former-commit-id: 101f3aeebebd46e654ff879e85a36a0f8bc780dc
    Former-commit-id: a4079455e651e2e0e0d83416ba212012a3f9bad9

--- apps/app/src/lib/query/recorder/mutations.ts
+++ apps/app/src/lib/query/recorder/mutations.ts
@@ -39,7 +39,6 @@ function createRecorder() {
 	const stopRecordingAndTranscribeAndCopyToClipboardAndPasteToCursorWithToast =
 		useStopRecordingAndTranscribeAndCopyToClipboardAndPasteToCursorWithToast();
 	const ensureRecordingSessionClosed = useEnsureRecordingSessionClosed();
-
 	const cancelRecorderWithToast = useCancelRecorderWithToast();
 
 	return {
