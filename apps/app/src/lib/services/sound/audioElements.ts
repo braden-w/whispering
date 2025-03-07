bb4e6eea7287e9d872821ba5296838193a966e7e
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-03-07 11:32:45 -0500

    rename: stopVadSoundSrc
    Former-commit-id: 230dfd751cfd410ac70d240054bedc20ae49f3bc
    Former-commit-id: b2654358cf7156cbcfcec21afce4e99c65458962

--- apps/app/src/lib/services/sound/audioElements.ts
+++ apps/app/src/lib/services/sound/audioElements.ts
@@ -2,7 +2,7 @@ import type { WhisperingSoundNames } from '@repo/shared';
 import stopManualSoundSrc from './assets/sound_ex_machina_Button_Blip.mp3';
 import captureVadSoundSrc from './assets/sound_ex_machina_Button_Blip.mp3';
 import startManualSoundSrc from './assets/zapsplat_household_alarm_clock_button_press_12967.mp3';
-import stopVadSoudnSrc from './assets/zapsplat_household_alarm_clock_large_snooze_button_press_001_12968.mp3';
+import stopVadSoundSrc from './assets/zapsplat_household_alarm_clock_large_snooze_button_press_001_12968.mp3';
 import startVadSoundSrc from './assets/zapsplat_household_alarm_clock_large_snooze_button_press_002_12969.mp3';
 import cancelSoundSrc from './assets/zapsplat_multimedia_click_button_short_sharp_73510.mp3';
 import transformationCompleteSoundSrc from './assets/zapsplat_multimedia_notification_alert_ping_bright_chime_001_93276.mp3';
@@ -14,7 +14,7 @@ export const audioElements = {
 	'stop-manual': new Audio(stopManualSoundSrc),
 	'start-vad': new Audio(startVadSoundSrc),
 	'capture-vad': new Audio(captureVadSoundSrc),
-	'stop-vad': new Audio(stopVadSoudnSrc),
+	'stop-vad': new Audio(stopVadSoundSrc),
 	transcriptionComplete: new Audio(transcriptionCompleteSoundSrc),
 	transformationComplete: new Audio(transformationCompleteSoundSrc),
 } satisfies Record<WhisperingSoundNames, HTMLAudioElement>;
