07ecdec06ba728da0c70ae1c98200cf52cf8a373
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-03-12 11:49:29 +0900

    feat: improve aria
    Former-commit-id: 48248ee89f11d4af004982dca92594dbf6c0b247
    Former-commit-id: 50142123b71664cf88a40b2e76640c49ee1956df

--- apps/app/src/lib/components/ui/keyboard-shortcut-recorder/KeyboardShortcutRecorder.svelte
+++ apps/app/src/lib/components/ui/keyboard-shortcut-recorder/KeyboardShortcutRecorder.svelte
@@ -158,15 +158,13 @@
 
 <svelte:window on:keydown={handleKeyDown} on:keyup={handleKeyUp} />
 
-<div
+<button
 	class={cn(
 		'relative flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background transition-all',
 		isRecording && 'ring-2 ring-ring ring-offset-2',
 		className,
 	)}
-	on:click={startRecording}
-	on:keydown={(e) => e.key === 'Enter' && startRecording()}
-	role="button"
+	onclick={startRecording}
 	tabindex="0"
 >
 	<div class="flex flex-1 items-center gap-1">
@@ -226,4 +224,4 @@
 			</Button>
 		</div>
 	{/if}
-</div>
+</button>
