f90fb91dd2719df8fef990c701982f61a8f90338
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-03-12 11:22:33 +0900

    feat: default keyboard shortcuts
    Former-commit-id: 58cf5e903757135235bc8996dc109201f7dd7af1
    Former-commit-id: f38fc5d5006a34c60a039e2329254f00fa82f3cc

--- packages/shared/src/commands.ts
+++ packages/shared/src/commands.ts
@@ -3,22 +3,31 @@ export const commands = [
 		id: 'toggleManualRecording',
 		description: 'Toggle manual recording',
 		defaultLocalShortcut: 'space',
+		defaultGlobalShortcut: 'CommandOrControl+Shift+{',
 	},
 	{
 		id: 'cancelManualRecording',
 		description: 'Cancel manual recording',
 		defaultLocalShortcut: 'c',
+		defaultGlobalShortcut: 'CommandOrControl+Shift+}',
 	},
 	{
 		id: 'closeManualRecordingSession',
 		description: 'Close manual recording session',
 		defaultLocalShortcut: 'shift+c',
+		defaultGlobalShortcut: 'CommandOrControl+Shift+\\',
 	},
-	{ id: 'pushToTalk', description: 'Push to talk', defaultLocalShortcut: 'p' },
+	{
+		id: 'pushToTalk',
+		description: 'Push to talk',
+		defaultLocalShortcut: 'p',
+		defaultGlobalShortcut: 'CommandOrControl+Shift+;',
+	},
 	{
 		id: 'toggleVadRecording',
 		description: 'Toggle vad recording',
 		defaultLocalShortcut: 'v',
+		defaultGlobalShortcut: "CommandOrControl+Shift+'",
 	},
 ] as const;
 
