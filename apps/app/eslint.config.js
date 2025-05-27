6665ac6f71a63e16cbb73762f12953658d6e0f2b
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-05-27 07:23:32 -0700

    chore(eslint): update Svelte configuration
    Former-commit-id: 13806ea1b10dd1b74195066bb4271caba2cd1d51
    Former-commit-id: b039c220845afa2ca80111587fca6b6bc6b42ae5

--- apps/app/eslint.config.js
+++ apps/app/eslint.config.js
@@ -1,35 +1,36 @@
-import { fileURLToPath } from 'node:url';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
+import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
+import svelteConfig from './svelte.config.js';
+
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
 	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
 	...ts.configs.recommended,
-	...svelte.configs['flat/recommended'],
+	...svelte.configs.recommended,
 	prettier,
-	...svelte.configs['flat/prettier'],
+	...svelte.configs.prettier,
 	{
 		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node,
-			},
+			globals: { ...globals.browser, ...globals.node },
 		},
+		rules: { 'no-undef': 'off' },
 	},
 	{
-		files: ['**/*.svelte'],
-
+		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
 		languageOptions: {
 			parserOptions: {
+				projectService: true,
+				extraFileExtensions: ['.svelte'],
 				parser: ts.parser,
+				svelteConfig,
 			},
 		},
 	},
-	{ ignores: ['src-tauri/**'] },
 );
