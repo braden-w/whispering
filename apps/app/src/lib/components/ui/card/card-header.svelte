9ebd3a7e2aef8f4443d07df399b7484c264b363e
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-06-01 09:01:54 -0700

    style: format with prettier and eslint
    Former-commit-id: 59dc75ed5750e24b7c17d043192c1a86268610e3
    Former-commit-id: 7051300ed8a7ff014caff89570133a7006e1bb0d

--- apps/app/src/lib/components/ui/card/card-header.svelte
+++ apps/app/src/lib/components/ui/card/card-header.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { cn, type WithElementRef } from "$lib/utils.js";
-	import type { HTMLAttributes } from "svelte/elements";
+	import { cn, type WithElementRef } from '$lib/utils.js';
+	import type { HTMLAttributes } from 'svelte/elements';
 
 	let {
 		ref = $bindable(null),
@@ -14,8 +14,8 @@
 	bind:this={ref}
 	data-slot="card-header"
 	class={cn(
-		"@container/card-header has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6 grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6",
-		className
+		'@container/card-header has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6 grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6',
+		className,
 	)}
 	{...restProps}
 >
--- apps/app/src/lib/components/ui/card/card-title.svelte
+++ apps/app/src/lib/components/ui/card/card-title.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import type { HTMLAttributes } from "svelte/elements";
-	import { cn, type WithElementRef } from "$lib/utils.js";
+	import type { HTMLAttributes } from 'svelte/elements';
+	import { cn, type WithElementRef } from '$lib/utils.js';
 
 	let {
 		ref = $bindable(null),
@@ -13,7 +13,7 @@
 <div
 	bind:this={ref}
 	data-slot="card-title"
-	class={cn("font-semibold leading-none", className)}
+	class={cn('font-semibold leading-none', className)}
 	{...restProps}
 >
 	{@render children?.()}
