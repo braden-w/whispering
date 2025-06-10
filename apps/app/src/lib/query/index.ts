86ce26921834f377802ade9d2cdc2232c9a0835c
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-06-09 21:54:19 -0700

    refactor(query): add executeMutation and defineMutation functions for enhanced mutation handling
    Former-commit-id: ee55f73dc8918a9a118240447ab6008fab179b55
    Former-commit-id: 6aeae8572ec2926fd805db3b153f1557d4534614

--- apps/app/src/lib/query/index.ts
+++ apps/app/src/lib/query/index.ts
@@ -1,5 +1,5 @@
 import { browser } from '$app/environment';
-import { QueryClient } from '@tanstack/svelte-query';
+import { QueryClient, type MutationOptions } from '@tanstack/svelte-query';
 
 export const queryClient = new QueryClient({
 	defaultOptions: {
@@ -8,3 +8,26 @@ export const queryClient = new QueryClient({
 		},
 	},
 });
+
+export function executeMutation<TData, TError, TVariables, TContext>(
+	options: MutationOptions<TData, TError, TVariables, TContext>,
+	variables: TVariables,
+) {
+	const mutation = queryClient.getMutationCache().build(queryClient, options);
+	return mutation.execute(variables);
+}
+
+export function defineMutation<TData, TError, TVariables, TContext>(
+	options: MutationOptions<TData, TError, TVariables, TContext> &
+		Required<
+			Pick<MutationOptions<TData, TError, TVariables, TContext>, 'mutationFn'>
+		>,
+) {
+	return {
+		options,
+		async execute(...args: Parameters<typeof options.mutationFn>) {
+			const result = await executeMutation(options, ...args);
+			return result;
+		},
+	};
+}
