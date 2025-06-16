0503af1aa78947af810d718937e3bc32e6187189
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-06-15 22:42:54 -0700

    fix(toast): remove ! in classes for toast component layout
    Former-commit-id: 3fefb24b6e57a2dc326fc28d628d4537348d7c79
    Former-commit-id: ebb4a7449946ce0bc168f1fcd1d83594285628ff

--- apps/app/src/routes/+layout/AppShell.svelte
+++ apps/app/src/routes/+layout/AppShell.svelte
@@ -52,7 +52,7 @@
 		toastOptions: {
 			classes: {
 				toast:
-					'group !flex !flex-wrap [&>[data-icon]]:!flex-shrink-0 [&>[data-content]]:!flex-1 [&>[data-button]]:!w-full [&>[data-button]]:!ml-0 [&>[data-button]]:!mt-3 [&>[data-button]]:!inline-flex [&>[data-button]]:!justify-center',
+					'group flex flex-wrap [&>[data-icon]]:flex-shrink-0 [&>[data-content]]:flex-1 [&>[data-button]]:w-full [&>[data-button]]:ml-0 [&>[data-button]]:mt-3 [&>[data-button]]:!inline-flex [&>[data-button]]:justify-center',
 			},
 		},
 	} satisfies ToasterProps;
