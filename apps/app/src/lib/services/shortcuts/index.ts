3d2e4b8791b9aa2c9652ea63d201ab963e8898fe
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-06-19 14:36:12 -0700

    refactor(shortcuts): remove unused shortcut conversion functions to streamline codebase
    Former-commit-id: ebe7c7d06984483263fe4951270493cccc31dd4d
    Former-commit-id: 90b20bfc8a1768a3471047b7493741291e5b6388

--- apps/app/src/lib/services/shortcuts/index.ts
+++
@@ -1,9 +0,0 @@
-export { createLocalShortcutManager } from './createLocalShortcutManager.svelte';
-export { createGlobalShortcutManager } from './createGlobalShortcutManager';
-export {
-	shortcutStringToArray,
-	arrayToShortcutString,
-	shortcutStringToTauriAccelerator,
-	normalizeKey,
-	formatKeysForDisplay,
-} from './formatConverters';
