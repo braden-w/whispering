604f817ab9e788e3ef017ebee6977854bdab1337
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-06-20 20:23:10 -0700

    chore(config): update Svelte and Vite configurations for Tauri development
    Former-commit-id: da9aad77928a2d06d10e7e999d7f88a7563f6182
    Former-commit-id: 4f297aa4cf85964e31268de69e4cfa46f47d5f72

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -1,7 +1,32 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
+const host = process.env.TAURI_DEV_HOST;
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
 	plugins: [sveltekit(), tailwindcss()],
-});
+
+	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+	//
+	// 1. prevent vite from obscuring rust errors
+	clearScreen: false,
+	// 2. tauri expects a fixed port, fail if that port is not available
+	server: {
+		port: 1420,
+		strictPort: true,
+		host: host || false,
+		hmr: host
+			? {
+					protocol: 'ws',
+					host,
+					port: 1421,
+				}
+			: undefined,
+		watch: {
+			// 3. tell vite to ignore watching `src-tauri`
+			ignored: ['**/src-tauri/**'],
+		},
+	},
+}));
