9c51024d4844639a3d5e1690c5810f36bab44e7f
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-06-20 20:23:58 -0700

    chore(dependencies): remove unused @tauri-apps/plugin-window-state from package.json and lib.rs
    Former-commit-id: e0dfd98cea1bfcf9e8869dbe9e46d386b2d18b83
    Former-commit-id: 456836f3255b45cc4b8af0bfa252d2a06235681d

--- apps/app/src-tauri/src/lib.rs
+++ apps/app/src-tauri/src/lib.rs
@@ -25,7 +25,6 @@ pub fn run() {
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
-        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .manage(AppData::new());
 
