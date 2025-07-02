eaa1e3771b98fc4087e5f01d0415a899a6f5fb11
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-07-02 08:24:22 -0700

    fix(dialog): adjust z-index for dialog and overlay components to ensure alert-dialogs are displayed on top
    Former-commit-id: cf7b5e3d5dc3ebd5d259ebb2f8e74f9e78244568
    Former-commit-id: c574f4279af5ec540f11bb1a329e197dc2615215

--- apps/app/src/lib/components/ui/dialog/dialog-content.svelte
+++ apps/app/src/lib/components/ui/dialog/dialog-content.svelte
@@ -25,6 +25,8 @@
 		class={cn(
 			'bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed left-[50%] top-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg',
 			'overflow-y-auto max-h-[calc(100vh-2rem)]',
+			// Override to z-40 to ensure that alert-dialogs (which are at z-50) are always on top of dialogs
+			'z-40',
 			className,
 		)}
 		{...restProps}
