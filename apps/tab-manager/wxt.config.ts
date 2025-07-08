95c61956ca07d9d16153d9dc255f8b4b94c3e29c
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-07-08 16:56:06 -0700

    chore(deps): tailwindcss

--- apps/tab-manager/wxt.config.ts
+++ apps/tab-manager/wxt.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'wxt';
+import path from 'node:path';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
 	srcDir: 'src',
 	modules: ['@wxt-dev/module-svelte'],
+	vite: () => ({
+		resolve: {
+			alias: {
+				$lib: path.resolve('./src/lib'),
+			},
+		},
+	}),
 });
