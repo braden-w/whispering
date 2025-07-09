649ef9d293ee0700afc02d1a4b63248fc5a49ba6
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-07-08 17:09:47 -0700

    chore(deps): integrate Tailwind CSS

--- apps/tab-manager/wxt.config.ts
+++ apps/tab-manager/wxt.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'wxt';
 import path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
 	srcDir: 'src',
 	modules: ['@wxt-dev/module-svelte'],
 	vite: () => ({
+		plugins: [tailwindcss()],
 		resolve: {
 			alias: {
 				$lib: path.resolve('./src/lib'),
