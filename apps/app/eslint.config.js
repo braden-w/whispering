f3a7c7b24c44a9773d212809e14be6164a98cfbe
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-07-09 22:05:17 -0700

    refactor(eslint): simplify ESLint configuration by removing unused dependencies and streamlining setup
    Former-commit-id: f574ee5a92e6ea5e788a4adb14b308609a3794d1
    Former-commit-id: b4943323e384faf040a6da1b05b00322bd09005b

--- apps/app/eslint.config.js
+++ apps/app/eslint.config.js
@@ -1,36 +1,4 @@
-import { fileURLToPath } from 'node:url';
-import { includeIgnoreFile } from '@eslint/compat';
-import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import svelte from 'eslint-plugin-svelte';
-import globals from 'globals';
-import ts from 'typescript-eslint';
-import svelteConfig from './svelte.config.js';
 
-const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
-
-export default ts.config(
-	includeIgnoreFile(gitignorePath),
-	js.configs.recommended,
-	...ts.configs.recommended,
-	...svelte.configs.recommended,
-	prettier,
-	...svelte.configs.prettier,
-	{
-		languageOptions: {
-			globals: { ...globals.browser, ...globals.node },
-		},
-		rules: { 'no-undef': 'off' },
-	},
-	{
-		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
-		languageOptions: {
-			parserOptions: {
-				projectService: true,
-				extraFileExtensions: ['.svelte'],
-				parser: ts.parser,
-				svelteConfig,
-			},
-		},
-	},
-);
+export default [prettier, ...svelte.configs.prettier];
