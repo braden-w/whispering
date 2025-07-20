db7c68de4178c3b0716ad6ca570575570c3383ee
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-07-20 09:36:15 -0700

    refactor: remove messages module in query in favor of store

--- apps/sh/src/lib/query/index.ts
+++ apps/sh/src/lib/query/index.ts
@@ -1,12 +1,3 @@
 // Re-export all queries and mutations as a namespace
 export * as sessions from './sessions';
-export * as messages from './messages';
 export * as models from './models';
-
-// Re-export helper functions
-export {
-	isMessageProcessing,
-	getLatestAssistantMessage,
-	isSessionProcessing,
-	formatMessageTime,
-} from './messages';
