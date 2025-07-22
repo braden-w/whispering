045d3e653e0860245c191012b3b17fb51d5a866e
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-07-22 09:44:27 -0700

    style:format

--- packages/ui/src/drawer/drawer-overlay.svelte
+++ packages/ui/src/drawer/drawer-overlay.svelte
@@ -18,8 +18,8 @@
 	data-slot="drawer-overlay"
 	class={cn(
 		'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/50',
-			// Override to z-40 to ensure that alert-dialogs (which are at z-50) are always on top of drawers
-			'z-40',
+		// Override to z-40 to ensure that alert-dialogs (which are at z-50) are always on top of drawers
+		'z-40',
 		className,
 	)}
 	{...restProps}
