0c91bef8b3a8a02782afccd3041d47b50affac0d
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-07-24 08:55:45 -0700

    refactor(auth): move db to db/index.ts

--- apps/auth/src/lib/db/index.ts
+++ apps/auth/src/lib/db/index.ts
@@ -1,6 +1,6 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-import { env } from './env';
+import { env } from '../env';
 
 const sql = neon(env.DATABASE_URL);
 export const db = drizzle({ client: sql });
