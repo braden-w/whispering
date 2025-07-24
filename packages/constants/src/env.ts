09193866c5efd013ecab618949c0ec1d3ed20534
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-07-24 11:35:27 -0700

    feat(constants): add validateEnv function to enhance environment validation and error handling

--- packages/constants/src/env.ts
+++ packages/constants/src/env.ts
@@ -12,4 +12,10 @@ export const envSchema = type({
 	GITHUB_CLIENT_SECRET: 'string',
 });
 
+export function validateEnv(env: unknown) {
+	const result = envSchema(env);
+	if (result instanceof type.errors) throw new Error(result.summary);
+	return result;
+}
+
 export type Env = typeof envSchema.infer;
