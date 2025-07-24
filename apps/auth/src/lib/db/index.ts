748941d1740148d740d8c8737ed19149992e8586
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-07-24 10:55:55 -0700

    fix(auth): return drizzle instance in db function for better clarity

--- apps/auth/src/lib/db/index.ts
+++ apps/auth/src/lib/db/index.ts
@@ -1,8 +1,8 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-import type { Env } from '../env';
+import type { Env } from '../env-schema';
 
 export const db = (env: Env) => {
 	const sql = neon(env.DATABASE_URL);
-	drizzle({ client: sql });
+	return drizzle({ client: sql });
 };
