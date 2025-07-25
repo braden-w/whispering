5da76e52388511d40f2f492f63e3f88fd46f0953
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-07-24 20:04:55 -0700

    feat(auth): implement load function to retrieve user session data using queryClient

--- apps/sh/src/routes/+layout.ts
+++ apps/sh/src/routes/+layout.ts
@@ -1,1 +1,11 @@
+import { queryClient } from '$lib/query/_client.js';
+import * as rpc from '$lib/query';
+
 export const ssr = false;
+
+export const load = async () => {
+	const session = await queryClient.ensureQueryData(
+		rpc.auth.getSession.options(),
+	);
+	return { session };
+};
