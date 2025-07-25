5b185ea1b1ac2dfeafee562dd4db252b63364b6a
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-07-24 17:54:19 -0700

    refactor(auth): integrate schema into drizzle initialization for improved database configuration

--- apps/auth/src/lib/db/index.ts
+++ apps/auth/src/lib/db/index.ts
@@ -1,8 +1,9 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import type { Env } from '@repo/constants/env-schema';
+import * as schema from './schema/auth';
 
 export const db = (env: Env) => {
 	const sql = neon(env.DATABASE_URL);
-	return drizzle({ client: sql });
+	return drizzle({ client: sql, schema });
 };
